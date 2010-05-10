1f965f7ea6351601813a8bc8350f76346ab32e87
annlynnworth <annlynnworth@88ca0d87-9d1d-4ddd-a6d8-5c97b49fb611>
2010-05-10 07:40:24 +0000

    Utility to adjust test results by stripping out changing sections

---
+++ trunk/webhubdemos/Source/SeleniumTests/DelphiDriver/TestFileCleanup/WHIgnoreChanging.dpr
@@ -0,0 +1,42 @@
+program WHIgnoreChanging;
+
+uses
+  Forms,
+  IOUtils,
+  Classes,
+  SysUtils,
+  ucString,
+  ucLogFil in 'k:\webhub\tpack\ucLogFil.pas',
+  uSeleniumIgnoreChanging in '..\uSeleniumIgnoreChanging.pas';
+
+{$R *.res}
+
+var
+  Filespec : string;
+  BaseFolder: string;
+  A: AnsiString;
+  S8Before, S8After: UTF8String;
+
+begin
+  if (ParamCount = 1) then
+  begin
+    BaseFolder := ParamStr(1);
+
+    if TDirectory.Exists(BaseFolder) then
+    begin
+      for Filespec in TDirectory.GetFiles(BaseFolder, 'test*.txt') do
+      begin
+        Writeln(Filespec);
+        S8Before := UTF8StringLoadFromFile(Filespec);
+        S8After := StripChanging(S8Before);
+        if (S8Before <> S8After) then
+          UTF8StringWriteToFile(Filespec, S8After);
+        //if Counter > 5 then break;
+      end;
+    end
+    else
+      writeln(Format('Directory [%s] does not exist.', [BaseFolder]));
+  end
+  else
+    Writeln('usage: pass in the name of the folder containing files to clean');
+end.
---
+++ trunk/webhubdemos/Source/SeleniumTests/DelphiDriver/TestFileCleanup/run-testfilecleanupD14.bat
@@ -0,0 +1,3 @@
+WHIgnoreChanging.exe D:\Projects\webhubdemos\TestResults\2010-05-10\ShowcaseD14
+
+pause
