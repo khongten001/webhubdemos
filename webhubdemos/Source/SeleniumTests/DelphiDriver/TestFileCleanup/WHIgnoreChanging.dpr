1587c10891b095fc3b5aaff762fca68e034b2f55
annlynnworth <annlynnworth@88ca0d87-9d1d-4ddd-a6d8-5c97b49fb611>
2010-06-10 20:59:30 +0000

    Support 2nd parameter for filespec

--- webhubdemos/Source/SeleniumTests/DelphiDriver/TestFileCleanup/WHIgnoreChanging.dpr
+++ webhubdemos/Source/SeleniumTests/DelphiDriver/TestFileCleanup/WHIgnoreChanging.dpr
@@ -6,6 +6,7 @@ uses
   Classes,
   SysUtils,
   ucPos in 'k:\webhub\tpack\ucPos.pas',
+  ucVers,
   ucString in 'k:\webhub\tpack\ucString.pas',
   ucLogFil in 'k:\webhub\tpack\ucLogFil.pas',
   uSeleniumIgnoreChanging in '..\uSeleniumIgnoreChanging.pas';
@@ -17,15 +18,20 @@ var
   BaseFolder: string;
   A: AnsiString;
   S8Before, S8After: UTF8String;
+  FilterSpec: string;
 
 begin
-  if (ParamCount = 1) then
+  if (ParamCount >= 1) then
   begin
     BaseFolder := ParamStr(1);
+    if ParamCount = 2 then
+      FilterSpec := ParamStr(2)
+    else
+      FilterSpec := 'test*.txt';
 
     if TDirectory.Exists(BaseFolder) then
     begin
-      for Filespec in TDirectory.GetFiles(BaseFolder, 'test*.txt') do
+      for Filespec in TDirectory.GetFiles(BaseFolder, FilterSpec) do
       begin
         Writeln(Filespec);
         S8Before := UTF8StringLoadFromFile(Filespec);
@@ -39,5 +45,14 @@ begin
       writeln(Format('Directory [%s] does not exist.', [BaseFolder]));
   end
   else
-    Writeln('usage: pass in the name of the folder containing files to clean');
+  begin
+    Writeln('WHIgnoreChanging v' + GetVersionDigits(False));
+    Writeln('Open Source; Creative Commons License');
+    Writeln('Copyright (c) 2010 HREF Tools Corp.');
+    Writeln('');
+
+    Writeln('usage: pass in the name of the folder containing files to clean ' +
+     'plus optionally the filespec');
+    Writeln('Example: WHIgnoreChanging.exe .\test1\ test*.txt');
+  end;
 end.
--- webhubdemos/Source/SeleniumTests/DelphiDriver/TestFileCleanup/test-ignore.bat
+++ webhubdemos/Source/SeleniumTests/DelphiDriver/TestFileCleanup/test-ignore.bat
@@ -1,3 +1,3 @@
 copy sample-before-changes.txt test123.txt
-whignorechanging.exe .\
+whignorechanging.exe .\ test1*.txt
 notepad test123.txt
