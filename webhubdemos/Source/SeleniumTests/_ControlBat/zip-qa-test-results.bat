2c8f66682065cd18071a5788b6c20105840d50e5
annlynnworth <annlynnworth@88ca0d87-9d1d-4ddd-a6d8-5c97b49fb611>
2010-06-24 06:19:38 +0000

    Tested on APATE server

--- webhubdemos/Source/SeleniumTests/_ControlBat/zip-qa-test-results.bat
+++ webhubdemos/Source/SeleniumTests/_ControlBat/zip-qa-test-results.bat
@@ -1,11 +1,13 @@
 :: param 1 = yyyymmdd
 
-CD /D D:\Projects\webhubdemos\Live\WebRoot\webhub\echoqa
+setlocal
+set qahome=D:\Projects\webhubdemos\Live\WebRoot\webhub\echoqa
+CD /D %qahome%
 
 d:\Apps\Utilities\7Zip\7z.exe a -r %1_uncleaned.7z %1
 pause
 
-D:\Projects\webhubdemos\Live\WebHub\Apps\WHIgnoreChanging.exe %1\showcaseD07\ t*.txt :1204
+D:\Projects\webhubdemos\Live\WebHub\Apps\WHIgnoreChanging.exe %qahome%\%1\ t*.txt :1204 /s
 pause
 
 d:\Apps\Utilities\7Zip\7z.exe a -r %1_clean.7z %1
