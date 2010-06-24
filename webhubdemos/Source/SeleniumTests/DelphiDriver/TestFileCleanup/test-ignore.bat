2d3ca363d336f3b29c14477428ad9f8317d418d3
annlynnworth <annlynnworth@88ca0d87-9d1d-4ddd-a6d8-5c97b49fb611>
2010-06-24 06:20:26 +0000

    Tested on APATE server

--- webhubdemos/Source/SeleniumTests/DelphiDriver/TestFileCleanup/test-ignore.bat
+++ webhubdemos/Source/SeleniumTests/DelphiDriver/TestFileCleanup/test-ignore.bat
@@ -1,3 +1,3 @@
 copy sample-before-changes.txt test123.txt
-whignorechanging.exe .\ test1*.txt 12345
+D:\Projects\webhubdemos\Live\WebHub\Apps\whignorechanging.exe .\ test1*.txt :12345 /s
 notepad test123.txt
