fec2ef1bf5d9f395a006b1241126deb48bfce7b2
annlynnworth <annlynnworth@88ca0d87-9d1d-4ddd-a6d8-5c97b49fb611>
2010-07-12 11:31:43 +0000

    showcase demo does not run as a service

--- trunk/webhubdemos/Live/WebHub/Apps/sample-start-cserver.bat
+++ trunk/webhubdemos/Live/WebHub/Apps/sample-start-cserver.bat
@@ -1,7 +1,7 @@
 :: Note: the ServiceName property of cserver.exe is 'webhubsample'
 :: The default AppID is 'appvers'
 
-net start webhubsample1 /AppID=showcase
+net start webhubsample1 /AppID=fast
 net start webhubsample2 
 net start webhubsample3 
 
