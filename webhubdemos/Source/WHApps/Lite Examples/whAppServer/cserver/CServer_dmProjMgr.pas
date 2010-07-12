67b71ea0d2305edb33d6a61cf2e9752b767762c3
annlynnworth <annlynnworth@88ca0d87-9d1d-4ddd-a6d8-5c97b49fb611>
2010-07-12 07:43:06 +0000

    inherits from webhub demo datamodule containing TtpProject

---
+++ webhubdemos/Source/WHApps/Lite Examples/whAppServer/cserver/CServer_dmProjMgr.pas
@@ -0,0 +1,42 @@
+unit CServer_dmProjMgr;
+
+interface
+
+uses
+  Windows, Messages, SysUtils, Variants, Classes, Graphics, Controls, Forms,
+  Dialogs, whdemo_DMProjMgr, tpProj;
+
+type
+  TDMForWHDemoC = class(TDMForWHDemo)
+    procedure ProjMgrGUICreate(Sender: TtpProject;
+      const ShouldEnableGUI: Boolean; var ErrorText: string;
+      var Continue: Boolean);
+  private
+    { Private declarations }
+  public
+    { Public declarations }
+  end;
+
+var
+  DMForWHDemoC: TDMForWHDemoC;
+
+implementation
+
+{$R *.dfm}
+
+uses
+  ucLogFil,
+  cfmwhCustom;
+
+procedure TDMForWHDemoC.ProjMgrGUICreate(Sender: TtpProject;
+  const ShouldEnableGUI: Boolean; var ErrorText: string; var Continue: Boolean);
+begin
+  inherited;
+  if ShouldEnableGUI then
+  begin
+    HREFTestLog('info', 'ShouldEnableGUI', 'True');
+    Application.CreateForm(TfmAppCustomPanel, fmAppCustomPanel);
+  end;
+end;
+
+end.
