c5a4516ed833f22d03fef0d67149df85dda396ac
annlynnworth <annlynnworth@88ca0d87-9d1d-4ddd-a6d8-5c97b49fb611>
2010-07-12 07:43:37 +0000

    Use drive H: not D:

--- webhubdemos/Source/WHApps/Lite Examples/whAppServer/cserver/cserver.dpr
+++ webhubdemos/Source/WHApps/Lite Examples/whAppServer/cserver/cserver.dpr
@@ -1,48 +1,35 @@
 program cserver;  // Created 29-Jun-2006 by the WebHub New Project Wizard
 
 uses
-  Forms,
-  webApp,
-  uCode,
-  ucString,
-  whMacroAffixes,
-  utPanFrm in '..\..\..\..\..\..\..\Apps\HREFTools\WebHub\lib\utPanFrm.pas' {utParentForm},
-  utMainFm in '..\..\..\..\..\..\..\Apps\HREFTools\WebHub\lib\utMainFm.pas' {fmMainForm},
-  utTrayFm in '..\..\..\..\..\..\..\Apps\HREFTools\WebHub\lib\utTrayFm.pas' {fmTrayForm},
+  MultiTypeApp in 'h:\MultiTypeApp.pas',
+  tpProj in 'h:\tpProj.pas',
+  utPanFrm in 'H:\utPanFrm.pas' {utParentForm},
+  utMainFm in 'H:\utMainFm.pas' {fmMainForm},
+  utTrayFm in 'H:\utTrayFm.pas' {fmTrayForm},
+  uAutoDataModules in 'H:\uAutoDataModules.pas',
+  uAutoPanels in 'H:\uAutoPanels.pas',
+  CServer_dmProjMgr in 'CServer_dmProjMgr.pas' {DMForWHDemoC: TDataModule},
+  cfmwhCustom in 'cfmwhCustom.pas' {fmAppCustomPanel},
   whsample_EvtHandlers in 'h:\whsample_EvtHandlers.pas' {whdmCommonEventHandlers: TDataModule},
-  whMain in '..\..\..\..\..\..\..\Apps\HREFTools\WebHub\lib\whMain.pas' {fmWebHubMainForm},
-  dmWHApp in '..\..\..\..\..\..\..\Apps\HREFTools\WebHub\lib\dmWHApp.pas' {dmWebHubApp: TDataModule},
-  whHTML in '..\..\..\..\..\..\..\Apps\HREFTools\WebHub\lib\whHtml.pas' {fmAppHTML},
-  whdw_RemotePages in '..\..\..\..\..\..\..\Apps\HREFTools\WebHub\lib\whdw_RemotePages.pas' {DataModuleDreamWeaver: TDataModule},
-  whpanel_RemotePages in '..\..\..\..\..\..\..\Apps\HREFTools\WebHub\lib\whpanel_RemotePages.pas' {fmWhDreamweaver},
-  whsample_DWSecurity in '..\..\..\..\..\..\..\Apps\HREFTools\WebHub\lib\whsample_DWSecurity.pas' {dmDWSecurity: TDataModule},
-  cfmwhCustom in 'cfmwhCustom.pas' {fmAppCustomPanel};
+  whdemo_Extensions in '..\..\..\Common\whdemo_Extensions.pas' {DemoExtensions: TDataModule},
+  whdemo_DMProjMgr in '..\..\..\Common\whdemo_DMProjMgr.pas' {DMForWHDemo: TDataModule},
+  whdemo_ViewSource in '..\..\..\Common\whdemo_ViewSource.pas' {DemoViewSource: TDataModule},
+  whdemo_Initialize in '..\..\..\Common\whdemo_Initialize.pas',
+  whdemo_Refresh in '..\..\..\Common\whdemo_Refresh.pas' {dmWhRefresh: TDataModule},
+  whdemo_About in '..\..\..\Common\whdemo_About.pas' {fmAppAboutPanel};
 
 {$R *.res}
-//{$R WHAPPICO.RES}
-//{$R HTICONS.RES}
-//{$R HTGLYPHS.RES}
+{$R WHAPPICO.RES}
+{$R HTICONS.RES}
+{$R HTGLYPHS.RES}
 
 begin
   Application.Initialize;
-  Application.CreateForm(TfmWebHubMainForm, fmWebHubMainForm);
-  CreateCoreWebHubDataModule;
-  with pWebApp do
-  begin
-    AppID := DefaultsTo(Lowercase(ParamString('ID')), 'hreftest');
-    Refresh;
-  end;
-  // create additional forms AFTER refresh
-  Application.CreateForm(TfmAppHTML, fmAppHTML);
-  Application.CreateForm(TDataModuleDreamWeaver, DataModuleDreamWeaver);
-  Application.CreateForm(TfmWhDreamweaver, fmWhDreamweaver);
-  Application.CreateForm(TdmDWSecurity, dmDWSecurity);
-  Application.CreateForm(TfmAppCustomPanel, fmAppCustomPanel);
-  InitCoreWebHubDataModule;
-  DataModuleDreamWeaver.Init;
-  dmDWSecurity.Init;
-  whDemoInit;
+  Application.CreateForm(TDMForWHDemoC, DMForWHDemoC);
+  DMForWHDemoC.SetDemoFacts('appvers', 'Lite Examples\whAppServer\cserver',
+    True);
+  DMForWHDemoC.ProjMgr.ManageStartup;
   Application.Run;
 end.
- 
+
 
