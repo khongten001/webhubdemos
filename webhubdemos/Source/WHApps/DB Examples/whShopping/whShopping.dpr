f3d604562c0572e86bbacfb1040fa0194f951513
vseslavsuvorov <vseslavsuvorov@88ca0d87-9d1d-4ddd-a6d8-5c97b49fb611>
2011-05-19 07:30:31 +0000

    Formatted source using Delphi XE

--- webhubdemos/Source/WHApps/DB Examples/whShopping/whShopping.dpr
+++ webhubdemos/Source/WHApps/DB Examples/whShopping/whShopping.dpr
@@ -1,24 +1,24 @@
 program whShopping;     { Shopping Cart - Simplest Example }
 (*
-Copyright (c) 1998 HREF Tools Corp.
+  Copyright (c) 1998-2011 HREF Tools Corp.
 
-Permission is hereby granted, on 04-Jun-2004, free of charge, to any person
-obtaining a copy of this file (the "Software"), to deal in the Software
-without restriction, including without limitation the rights to use, copy,
-modify, merge, publish, distribute, sublicense, and/or sell copies of the
-Software, and to permit persons to whom the Software is furnished to do so,
-subject to the following conditions:
+  Permission is hereby granted, on 04-Jun-2004, free of charge, to any person
+  obtaining a copy of this file (the "Software"), to deal in the Software
+  without restriction, including without limitation the rights to use, copy,
+  modify, merge, publish, distribute, sublicense, and/or sell copies of the
+  Software, and to permit persons to whom the Software is furnished to do so,
+  subject to the following conditions:
 
-The above copyright notice and this permission notice shall be included in
-all copies or substantial portions of the Software.
+  The above copyright notice and this permission notice shall be included in
+  all copies or substantial portions of the Software.
 
-THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
-THE SOFTWARE.
+  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+  THE SOFTWARE.
 *)
 
 (* See "How to Work with WebHub Demos.rtf" in the webhubdemos\source\docs folder 
