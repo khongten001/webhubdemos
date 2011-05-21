342c5d4869bd77aec329d83ee39e581060c48c37
vseslavsuvorov <vseslavsuvorov@88ca0d87-9d1d-4ddd-a6d8-5c97b49fb611>
2011-05-21 21:28:56 +0000

    precompiled .class files are required for Java applets

--- webhubdemos/Live/WebRoot/webhub/demos/whShowcase/java/JavaVersionDisplayApplet.java
+++ webhubdemos/Live/WebRoot/webhub/demos/whShowcase/java/JavaVersionDisplayApplet.java
@@ -7,8 +7,10 @@ import java.applet.*;
  public class JavaVersionDisplayApplet extends Applet
  { private Label m_labVersionVendor; 
    public JavaVersionDisplayApplet() //constructor
-   { Color colFrameBackground = Color.pink;
+   { Color colFrameBackground = Color.blue;
+     Color colFrameForeground = Color.white;
      this.setBackground(colFrameBackground);
+     this.setForeground(colFrameForeground);
      m_labVersionVendor = new Label (" Java Version: " +
                                     System.getProperty("java.version")+
                            " from "+System.getProperty("java.vendor"));
