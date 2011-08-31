59b6acab0a4b04c2de5c1780b4046fce97e33fcb
annlynnworth <annlynnworth@88ca0d87-9d1d-4ddd-a6d8-5c97b49fb611>
2011-08-31 06:34:13 +0000

    D07 obsolete

--- trunk/webhubdemos/Live/WebHub/Apps/run-FORM-D07.bat
+++
@@ -1,5 +0,0 @@
-del c:\temp\whSessions\1204.var
-del d:\temp\whSessions\1204.var
-
-cd /d %~dp0
-start DServerD07nopackages.exe /ID=form 
--- trunk/webhubdemos/Live/WebHub/Apps/run-SHOWCASE-D07.bat
+++
@@ -1,9 +0,0 @@
-del c:\temp\whSessions\1204.var
-del d:\temp\whSessions\1204.var
-
-::change to folder containing this bat file
-cd %~dp0
-
-::start the WebHub showcase demo 
-start dserverD07nopackages.exe /ID=showcase 
-
