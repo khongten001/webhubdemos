4214b85d9f7300ad2522b9576282bf5b60e5491f
annlynnworth <annlynnworth@88ca0d87-9d1d-4ddd-a6d8-5c97b49fb611>
2011-09-04 23:27:48 +0000

    use ZM to find compiler

--- trunk/webhubdemos/Source/_Control/compile-1demoD15nopackages.bat
+++ trunk/webhubdemos/Source/_Control/compile-1demoD15nopackages.bat
@@ -1,6 +1,11 @@
 @echo off
 setlocal
-set dcc=D:\Apps\Embarcadero\RADStudio\8.0\bin\dcc32.exe
+
+:: use ZaphodsMap to find compiler
+:: zmset.bat and ZMLookup.exe are FREE from HREF Tools Corp. via www.zaphodsmap.com
+call %ZaphodsMap%zmset.bat dcc UsingKey2Folder "HREFTools\Production\cv001 Delphi D15"
+set dcc=%dcc%bin\dcc32.exe
+if not exist %dcc% pause
 
 set raizelib=K:\Vendors\Raize\CodeSite4\Source\VCL\Indy;K:\Vendors\Raize\CodeSite4\Source\VCL
 set libsearchpath=h:\;h:\dcu_d15_win32;k:\Rubicon\source;K:\Vendors\CPS\IBObjects\v4.9.11
@@ -13,7 +18,7 @@ ren %1.cfg %1.off
 
 
 @echo on
-%dcc%  -w -h -b %1.dpr  -nd:\temp\DelphiTempDCU -E%outputroot% -D%compilerflags% -LU%pkg% -u%libsearchpath%;%raizelib% -R%libsearchpath% -I%includepath% -O%objpath% /$D- /$L- /$Y- /$Q- /$R
+%dcc%  -w -h -b %1.dpr  -nd:\temp\DelphiTempDCU -E%outputroot% -D%compilerflags% -LU%pkg% -u%libsearchpath%;%raizelib% -R%libsearchpath% -I%includepath% /$D- /$L- /$Y- /$Q- /$R
 if errorlevel 1 pause
 
 @echo off
