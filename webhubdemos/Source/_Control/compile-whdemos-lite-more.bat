08d583f509dc6c33da8628aa950098995814365d
annlynnworth <annlynnworth@88ca0d87-9d1d-4ddd-a6d8-5c97b49fb611>
2011-09-05 05:45:03 +0000

    Compile DServer with D15 as-service

--- webhubdemos/Source/_Control/compile-whdemos-lite-more.bat
+++ webhubdemos/Source/_Control/compile-whdemos-lite-more.bat
@@ -4,7 +4,6 @@ d:
 
 
 DEL D:\Projects\webhubdemos\Live\WebHub\Apps\DServerD15*.exe /q
-DEL D:\Projects\webhubdemos\Live\WebHub\Apps\DServerD07*.exe /q
 
 cd %droot%\Lite Examples\whAppServer\whLite
 call %cbat% whLite
@@ -13,6 +12,13 @@ cd %droot%\Lite Examples\whAppServer\aserver
 call %cbat% aserver
 
 cd %droot%\Lite Examples\whAppServer\dserver
+
+:: as-service
+set cbat=D:\Projects\webhubdemos\Source\_Control\compile-1demoD15svc.bat
+call %cbat% dserver
+REN D:\Projects\webhubdemos\Live\WebHub\Apps\dserver.exe DServerD15svc.exe
+
+set cbat=D:\Projects\webhubdemos\Source\_Control\compile-1demoD15.bat
 call %cbat% dserver
 REN D:\Projects\webhubdemos\Live\WebHub\Apps\dserver.exe DServerD15.exe
 call D:\Projects\webhubdemos\Source\_Control\compile-1demoD15nopackages.bat dserver
@@ -46,7 +52,6 @@ cd %droot%\More Examples\whSendmail
 call %cbat% whSendmail
 
 cd %droot%\More Examples\whStopspam
-::call d:\projects\compile-1demosvcD07.bat whStopspam
 call d:\projects\compile-1demosvcD15.bat whStopspam
 
 cd %droot%\More Examples\whText2Table
