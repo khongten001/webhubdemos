72b1b0833a36c78f625b3fabc7df122cfd90f0b2
annlynnworth <annlynnworth@88ca0d87-9d1d-4ddd-a6d8-5c97b49fb611>
2011-09-06 06:14:41 +0000

    compiles WebHub v2.148 d16_win32 lite, more, db

--- webhubdemos/Source/_Control/compile-1demo_d16_win32.bat
+++ webhubdemos/Source/_Control/compile-1demo_d16_win32.bat
@@ -3,22 +3,26 @@ setlocal
 
 :: use ZaphodsMap to find compiler
 :: zmset.bat and ZMLookup.exe are FREE from HREF Tools Corp. via www.zaphodsmap.com
-call %ZaphodsMap%zmset.bat dcc UsingKey2Folder "HREFTools\Production\cv001 Delphi D15"
-set dcc=%dcc%bin\dcc32.exe
+call %ZaphodsMap%zmset.bat d16 UsingKey2Folder "HREFTools\Production\cv001 Delphi D16"
+set dcc=%d16%bin\dcc32.exe
 if not exist %dcc% pause
 
 set ibopath=K:\Vendors\CPS\IBObjects\v5.x\source\common;K:\Vendors\CPS\IBObjects\v5.x\source\tdataset;K:\Vendors\CPS\IBObjects\v5.x\source\tools;K:\Vendors\CPS\IBObjects\v5.x\source\core;K:\Vendors\CPS\IBObjects\v5.x\source\access
-set libsearchpath=h:\;h:\dcu_d15_win32;h:\pkg_d15_win32;k:\Rubicon\source;%ibopath%;
+set libsearchpath=h:\;h:\dcu_d16_win32;h:\pkg_d16_win32;k:\Rubicon\source;%ibopath%;%d16%\lib\win32\release;
 set outputroot="d:\Projects\WebHubDemos\Live\WebHub\Apps"
 set pkg="vcl;vclx;vcldb;vcldbx;soaprtl;xmlrtl;inet;ldiRegExLib;ZaphodsMapLib;WebHub;WebHubDB"
 set compilerflags=PREVENTSVCMGR;use_IBO
 set includepath=h:\;k:\Rubicon\source\inc;K:\Vendors\CPS\IBObjects\v5.x\source\common;
 
+:: extra parameters for Delphi XE2
+set dccflags=--no-config -M -Q -AGenerics.Collections=System.Generics.Collections;Generics.Defaults=System.Generics.Defaults;WinTypes=Windows;WinProcs=Windows;DbiTypes=BDE;DbiProcs=BDE;DbiErrs=BDE
+set dccns=-NSSystem;Xml;Data;Datasnap;Web;Soap;Winapi;System.Win;Data.Win;Datasnap.Win;Web.Win;Soap.Win;Xml.Win;Bde;Vcl;Vcl.Imaging;Vcl.Touch;Vcl.Samples;Vcl.Shell
+
 ren %1.cfg %1.off
 
-
+echo 1demo d16_win32 %1
 @echo on
-%dcc%  -w -h -b %1.dpr  -nd:\temp\DelphiTempDCU -E%outputroot% -D%compilerflags% -LU%pkg% -u%libsearchpath% -R%libsearchpath% -I%includepath% /$D- /$L- /$Y- /$Q- /$R
+%dcc%  -w -h -b %1.dpr  -nd:\temp\DelphiTempDCU -E%outputroot% -D%compilerflags% -LU%pkg% -u%libsearchpath% -R%libsearchpath% -I%includepath% /$D- /$L- /$Y- /$Q- /$R %dccflags% %dccns%
 if errorlevel 1 pause
 
 @echo off
--- webhubdemos/Source/_Control/compile-1demo_d16_win32_nopackag.bat
+++ webhubdemos/Source/_Control/compile-1demo_d16_win32_nopackag.bat
@@ -3,22 +3,28 @@ setlocal
 
 :: use ZaphodsMap to find compiler
 :: zmset.bat and ZMLookup.exe are FREE from HREF Tools Corp. via www.zaphodsmap.com
-call %ZaphodsMap%zmset.bat dcc UsingKey2Folder "HREFTools\Production\cv001 Delphi D15"
-set dcc=%dcc%bin\dcc32.exe
+call %ZaphodsMap%zmset.bat d16 UsingKey2Folder "HREFTools\Production\cv001 Delphi D16"
+set dcc=%d16%bin\dcc32.exe
 if not exist %dcc% pause
 
 set raizelib=K:\Vendors\Raize\CodeSite4\Source\VCL\Indy;K:\Vendors\Raize\CodeSite4\Source\VCL
-set libsearchpath=h:\;h:\dcu_d15_win32;k:\Rubicon\source;K:\Vendors\CPS\IBObjects\v4.9.11
+set libsearchpath=h:\;h:\dcu_d16_win32;k:\Rubicon\source;K:\Vendors\CPS\IBObjects\v4.9.11;%d16%\lib\win32\release;
 set outputroot="d:\Projects\WebHubDemos\Live\WebHub\Apps"
 set pkg=
 set compilerflags=PREVENTSVCMGR;use_IBO;
 set includepath=h:\;k:\Rubicon\source\inc;K:\Vendors\CPS\IBObjects\v4.9.11
 
+:: extra parameters for Delphi XE2
+set dccflags=--no-config -M -Q -AGenerics.Collections=System.Generics.Collections;Generics.Defaults=System.Generics.Defaults;WinTypes=Windows;WinProcs=Windows;DbiTypes=BDE;DbiProcs=BDE;DbiErrs=BDE
+set dccns=-NSSystem;Xml;Data;Datasnap;Web;Soap;Winapi;System.Win;Data.Win;Datasnap.Win;Web.Win;Soap.Win;Xml.Win;Bde;Vcl;Vcl.Imaging;Vcl.Touch;Vcl.Samples;Vcl.Shell
+
+
 ren %1.cfg %1.off
 
+echo 1demo no-packages d16_win32 %1
 
 @echo on
-%dcc%  -w -h -b %1.dpr  -nd:\temp\DelphiTempDCU -E%outputroot% -D%compilerflags% -LU%pkg% -u%libsearchpath%;%raizelib% -R%libsearchpath% -I%includepath% /$D- /$L- /$Y- /$Q- /$R
+%dcc%  -w -h -b %1.dpr  -nd:\temp\DelphiTempDCU -E%outputroot% -D%compilerflags% -LU%pkg% -u%libsearchpath%;%raizelib% -R%libsearchpath% -I%includepath% /$D- /$L- /$Y- /$Q- /$R %dccflags% %dccns%
 if errorlevel 1 pause
 
 @echo off
