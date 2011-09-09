12cd4960037564e3e6ec76cc42aef4b8a60e60d5
annlynnworth <annlynnworth@88ca0d87-9d1d-4ddd-a6d8-5c97b49fb611>
2011-09-09 09:28:18 +0000

    Starting on Delphi XE2, 64-bit WebHub demos

--- trunk/webhubdemos/Source/_Control/compile-1demo_d16_win64.bat
+++ trunk/webhubdemos/Source/_Control/compile-1demo_d16_win64.bat
@@ -4,13 +4,13 @@ setlocal
 :: use ZaphodsMap to find compiler
 :: zmset.bat and ZMLookup.exe are FREE from HREF Tools Corp. via www.zaphodsmap.com
 call %ZaphodsMap%zmset.bat d16 UsingKey2Folder "HREFTools\Production\cv001 Delphi D16"
-set dcc=%d16%bin\dcc32.exe
+set dcc=%d16%bin\dcc64.exe
 if not exist %dcc% pause
 
 set ibopath=K:\Vendors\CPS\IBObjects\v5.x\source\common;K:\Vendors\CPS\IBObjects\v5.x\source\tdataset;K:\Vendors\CPS\IBObjects\v5.x\source\tools;K:\Vendors\CPS\IBObjects\v5.x\source\core;K:\Vendors\CPS\IBObjects\v5.x\source\access
-set libsearchpath="h:\;h:\dcu_d16_win32;h:\pkg_d16_win32;k:\Rubicon\source;%ibopath%;%d16%\lib\win32\release;"
+set libsearchpath="h:\;h:\dcu_d16_win64;h:\pkg_d16_win64;k:\Rubicon\source;%ibopath%;%d16%\lib\win64\release;"
 set outputroot="d:\Projects\WebHubDemos\Live\WebHub\Apps"
-set pkg="vcl;vclx;vcldb;vcldbx;soaprtl;xmlrtl;inet;ldiRegExLib;ZaphodsMapLib;WebHub;WebHubDB"
+set pkg="vcl;vclx;vcldb;soaprtl;xmlrtl;inet;ldiRegExLib;ZaphodsMapLib;WebHub;WebHubDB"
 set compilerflags=PREVENTSVCMGR;use_IBO
 set includepath=h:\;k:\Rubicon\source\inc;K:\Vendors\CPS\IBObjects\v5.x\source\common;
 
@@ -20,7 +20,7 @@ set dccns=-NSSystem;Xml;Data;Datasnap;Web;Soap;Winapi;System.Win;Data.Win;Datasn
 
 ren %1.cfg %1.off
 
-echo 1demo d16_win32 %1
+echo 1demo d16_win64 %1
 @echo on
 "%dcc%"  -w -h -b %1.dpr  -nd:\temp\DelphiTempDCU -E%outputroot% -D%compilerflags% -LU%pkg% -u%libsearchpath% -R%libsearchpath% -I%includepath% /$D- /$L- /$Y- /$Q- /$R %dccflags% %dccns%
 if errorlevel 1 pause
--- trunk/webhubdemos/Source/_Control/compile-whdemos-lite-more.bat
+++ trunk/webhubdemos/Source/_Control/compile-whdemos-lite-more.bat
@@ -1,8 +1,13 @@
 @echo off
 set /P comp3=Enter Pascal Compiler Code in UPPERCASE (eg. D15 or D16) :   
 if %comp3%=="" goto end
+if %comp3%==D15 set bits=32
+if %comp3%==D15 goto DServerContinue00
+set /P bits=Enter 32 or 64 for win32 or win64 :   
+if %bits%=="" goto end
 
-set cbat=D:\Projects\webhubdemos\Source\_Control\compile-1demo_%comp3%_win32.bat
+:DServerContinue00
+set cbat=D:\Projects\webhubdemos\Source\_Control\compile-1demo_%comp3%_win%bits%.bat
 set droot=\projects\WebHubDemos\Source\WHApps
 d:
 
@@ -17,16 +22,16 @@ call %cbat% aserver
 cd %droot%\Lite Examples\whAppServer\dserver
 
 :: as-service
-set cbat=D:\Projects\webhubdemos\Source\_Control\compile-1demo_%comp3%_win32_svc.bat
+set cbat=D:\Projects\webhubdemos\Source\_Control\compile-1demo_%comp3%_win%bits%_svc.bat
 call %cbat% dserver
 REN D:\Projects\webhubdemos\Live\WebHub\Apps\dserver.exe DServer%comp3%svc.exe
 
 :DServerContinue01
-set cbat=D:\Projects\webhubdemos\Source\_Control\compile-1demo_%comp3%_win32.bat
+set cbat=D:\Projects\webhubdemos\Source\_Control\compile-1demo_%comp3%_win%bits%.bat
 call %cbat% dserver
 REN D:\Projects\webhubdemos\Live\WebHub\Apps\dserver.exe DServer%comp3%.exe
-call D:\Projects\webhubdemos\Source\_Control\compile-1demo_%comp3%_win32_nopackages.bat dserver
-REN D:\Projects\webhubdemos\Live\WebHub\Apps\dserver.exe DServer%comp3%_win32_NoPackages.exe
+call D:\Projects\webhubdemos\Source\_Control\compile-1demo_%comp3%_win%bits%_nopackages.bat dserver
+REN D:\Projects\webhubdemos\Live\WebHub\Apps\dserver.exe DServer%comp3%_win%bits%_NoPackages.exe
 if errorlevel 1 pause
 
 cd %droot%\More Examples\whCOM
@@ -57,7 +62,7 @@ cd %droot%\More Examples\whSendmail
 call %cbat% whSendmail
 
 cd %droot%\More Examples\whStopspam
-call d:\projects\compile-1demo_%comp3%_win32_svc.bat whStopspam
+call d:\projects\compile-1demo_%comp3%_win%bits%_svc.bat whStopspam
 
 cd %droot%\More Examples\whText2Table
 call %cbat% whText2Table
