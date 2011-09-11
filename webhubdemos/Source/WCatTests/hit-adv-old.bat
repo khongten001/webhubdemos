4cd7adc17a0967222b267cb2d1e6333df59f77ff
annlynnworth <annlynnworth@88ca0d87-9d1d-4ddd-a6d8-5c97b49fb611>
2011-09-11 09:41:16 +0000

    minor edits

--- webhubdemos/Source/WCatTests/hit-adv-old.bat
+++ webhubdemos/Source/WCatTests/hit-adv-old.bat
@@ -4,12 +4,12 @@ net stop w3svc
 net stop hub
 
 del D:\temp\whSessions\*.var
-del C:\temp\whTemp\ipc\*.*
+del C:\temp\whTemp\ipc\*.* /q
 
 net start w3svc
 net start hub
 
-echo Start DServer X instances now
+echo Start DServer instances now
 pause
 
 cd /d D:\Apps\Microsoft\wcat
