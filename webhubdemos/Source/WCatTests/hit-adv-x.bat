e867220300d29224d2bf2a24f966774e1b6a5e6a
annlynnworth <annlynnworth@88ca0d87-9d1d-4ddd-a6d8-5c97b49fb611>
2011-09-11 09:53:32 +0000

    separate preparation from hit

--- webhubdemos/Source/WCatTests/hit-adv-x.bat
+++ webhubdemos/Source/WCatTests/hit-adv-x.bat
@@ -1,15 +1,4 @@
-D:\Apps\HREFTools\WebHub\bin\ShutdownWHApps.exe
-
-net stop w3svc
-net stop hub
-
-del D:\temp\whSessions\*.var
-del C:\temp\whTemp\ipc\*.* /q
-
-net start w3svc
-net start hub
-
-echo Start DServer X instances now
+REM Make sure server is ready
 pause
 
 cd /d D:\Apps\Microsoft\wcat
