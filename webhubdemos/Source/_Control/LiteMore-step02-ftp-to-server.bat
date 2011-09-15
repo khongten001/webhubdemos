c68d9f3c5e64dd5bbdea2b81fb114fa3fdace058
annlynnworth <annlynnworth@88ca0d87-9d1d-4ddd-a6d8-5c97b49fb611>
2011-09-15 03:49:35 +0000

    Demonstrate how to update production server

---
+++ webhubdemos/Source/_Control/LiteMore-step02-ftp-to-server.bat
@@ -0,0 +1,26 @@
+:: use the ncftp utility to post the files to the server using ftp
+:: ncftp is freely available yet its author would appreciate a donation
+
+:: use zmset.bat to pull credentials from a local config file
+:: zmset and ZMLookup are freely available from HREF Tools
+:: Pascal source is included with ZaphodsMap 
+:: www.zaphodsmap.com
+
+setlocal
+
+:: switch to the location of this BAT file
+cd %~dp0
+:: switch to the location of the 7z files
+cd ..\..\Live\WebHub\Apps
+
+set h=lite.demos.href.com
+
+call %ZaphodsMap%zmset.bat u UsingKey2Value "HREFTools\FileTransfer FTP webhubdemos-lite-user"
+call %ZaphodsMap%zmset.bat p UsingKey2Value "HREFTools\FileTransfer FTP webhubdemos-lite-pass"
+
+D:\Apps\Utilities\NcFTP\ncftpput.exe -u %u% -p %p% %h% /Live/WebHub/Apps LiteMore-bin.7z
+if errorlevel 1 pause
+D:\Apps\Utilities\NcFTP\ncftpput.exe -u %u% -p %p% %h% /Live/Library LiteMore-Library-bin.7z
+if errorlevel 1 pause
+
+endlocal
