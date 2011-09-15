eafef3a88e5c887b250e09319e7872c074cf5160
annlynnworth <annlynnworth@88ca0d87-9d1d-4ddd-a6d8-5c97b49fb611>
2011-09-15 05:02:50 +0000

    Delete var 1204

--- webhubdemos/Live/WebHub/Apps/run-SHOWCASE.bat
+++ webhubdemos/Live/WebHub/Apps/run-SHOWCASE.bat
@@ -1,5 +1,10 @@
 ::change to folder containing this bat file
 cd %~dp0
 
+
+::delete the session data for the quality-assurance test run
+del c:\temp\whSessions\1204.var
+del d:\temp\whSessions\1204.var
+
 ::start the WebHub showcase demo
 start whLite.exe /ID=showcase 
