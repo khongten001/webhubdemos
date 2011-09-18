120c0329fc1b67cf92fe08209c0dc60f0a6ca0ae
annlynnworth <annlynnworth@88ca0d87-9d1d-4ddd-a6d8-5c97b49fb611>
2011-09-18 01:45:21 +0000

    quickly toggle ipc on db.demos.href.com

---
+++ webhubdemos/Live/_Control/ipc-database-swtich-to-new.bat
@@ -0,0 +1,32 @@
+cd /d d:\Apps\HREFTools\WebHub\bin
+
+ShutdownWHApps.exe
+net stop w3svc
+
+rem wait until all WebHub EXEs have closed
+pause
+
+net stop hub
+
+copy hub_d15_win32_debug_newipc.exe hub.exe
+copy webhubadmin_newipc.exe WebHubAdmin.exe
+
+cd whRunner
+copy runisa_newipc.dll runisa.dll
+
+net start hub
+net start w3svc
+
+cd /d %~dp0
+cd ..\WebHub\Apps
+
+d:\Apps\Utilities\7Zip\7z.exe x Database-bin-newipc.7z
+if errorlevel 1 pause
+
+cd ..\..\Library
+d:\Apps\Utilities\7Zip\7z.exe x Database-Library-bin-newipc.7z
+if errorlevel 1 pause
+
+cd /d D:\AppsData\ZaphodsMap\HREFTools\Install
+copy ZMKeybox-newipc.xml ZMKeybox.xml
+
---
+++ webhubdemos/Live/_Control/ipc-database-swtich-to-old.bat
@@ -0,0 +1,32 @@
+cd /d d:\Apps\HREFTools\WebHub\bin
+
+ShutdownWHApps.exe
+net stop w3svc
+
+rem wait until all WebHub EXEs have closed
+pause
+
+net stop hub
+
+copy hub_oldipc.exe hub.exe
+copy webhubadmin_oldipc.exe WebHubAdmin.exe
+
+cd whRunner
+copy runisa_oldipc.dll runisa.dll
+
+net start hub
+net start w3svc
+
+cd /d %~dp0
+cd ..\WebHub\Apps
+
+d:\Apps\Utilities\7Zip\7z.exe x Database-bin-oldipc.7z
+if errorlevel 1 pause
+
+cd ..\..\Library
+d:\Apps\Utilities\7Zip\7z.exe x Database-Library-bin-oldipc.7z
+if errorlevel 1 pause
+
+cd /d D:\AppsData\ZaphodsMap\HREFTools\Install
+copy ZMKeybox-oldipc.xml ZMKeybox.xml
+
