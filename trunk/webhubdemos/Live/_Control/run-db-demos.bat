87f58fe7203b685a9cf0e1c12d087f9ad9d297f4
annlynnworth <annlynnworth@88ca0d87-9d1d-4ddd-a6d8-5c97b49fb611>
2011-09-18 00:17:21 +0000

    Further cover pages

--- trunk/webhubdemos/Live/_Control/run-db-demos.bat
+++ trunk/webhubdemos/Live/_Control/run-db-demos.bat
@@ -10,11 +10,16 @@ echo flagdemosdb is %flagdemosdb%
 if NOT %flagdemosdb%==yes goto end
 
 set covermin=90
-set coverreason="stress testing selected database demos"
+set coverreason="testing selected database demos; if you need to see this one, please contact techsupport"
 
+set demodbhtml=yes
 set demodsp=yes
-set demojpeg=no
+set demofire=no
 set demohtcl=no
+set demohtfm=no
+set demohtfs=yes
+set demojpeg=no
+set demoshop1=no
 
 d:
 cd \projects\WebHubDemos\Live\WebHub\Apps\
@@ -26,7 +31,7 @@ if %demodsp%==yes start whDSP.exe
 if %demodsp%==yes d:\Apps\HREFTools\miscutil\wait.exe 15
 if %demodsp%==no d:\Apps\HREFTools\WebHub\bin\WHCoverMgmt.exe /cover /appid=dsp /minutes=%covermin% /reason=%coverreason%
 
-rem CodeRage
+rem CodeRage from 2009
 REM  29-Dec-2010 start whSchedule.exe 
 REM d:\Apps\HREFTools\miscutil\wait.exe 15
 d:\Apps\HREFTools\WebHub\bin\WHCoverMgmt.exe /cover /appid=coderage /minutes=9325322 /reason="obsolete; was for CodeRage in 2009"
@@ -39,19 +44,21 @@ if %demohtcl%==yes start whClone.exe
 if %demohtcl%==yes d:\Apps\HREFTools\miscutil\wait.exe 12
 if %demohtcl%==no  d:\Apps\HREFTools\WebHub\bin\WHCoverMgmt.exe /cover /appid=htcl /minutes=%covermin% /reason=%coverreason%
 
-pause
+if %demohtfs%==yes start whFishStore.exe     
+if %demohtfs%==yes d:\Apps\HREFTools\miscutil\wait.exe 12
+if %demohtfs%==no  d:\Apps\HREFTools\WebHub\bin\WHCoverMgmt.exe /cover /appid=htfs /minutes=%covermin% /reason=%coverreason%
 
-start whFishStore.exe     
-d:\Apps\HREFTools\miscutil\wait.exe 12
+if %demofire%==yes start whFirebird.exe 
+if %demofire%==yes d:\Apps\HREFTools\miscutil\wait.exe 15
+if %demofire%==no  d:\Apps\HREFTools\WebHub\bin\WHCoverMgmt.exe /cover /appid=fire /minutes=%covermin% /reason=%coverreason%
 
-start whFirebird.exe 
-d:\Apps\HREFTools\miscutil\wait.exe 15
+if %demohtfm%==yes start whInstantForm.exe   
+if %demohtfm%==yes d:\Apps\HREFTools\miscutil\wait.exe 12
+if %demohtfm%==no  d:\Apps\HREFTools\WebHub\bin\WHCoverMgmt.exe /cover /appid=htfm /minutes=%covermin% /reason=%coverreason%
 
-start whInstantForm.exe   
-d:\Apps\HREFTools\miscutil\wait.exe 12
-
-start whLoadFromDB.exe  
-d:\Apps\HREFTools\miscutil\wait.exe 12
+if %demodbhtml%==yes start whLoadFromDB.exe  
+if %demodbhtml%==yes d:\Apps\HREFTools\miscutil\wait.exe 12
+if %demodbhtml%==no  d:\Apps\HREFTools\WebHub\bin\WHCoverMgmt.exe /cover /appid=dbhtml /minutes=%covermin% /reason=%coverreason%
 
 ::08Jan2011 OFF start whQuery1.exe    
 ::08Jan2011 OFF d:\Apps\HREFTools\miscutil\wait.exe 12
@@ -74,8 +81,9 @@ d:\Apps\HREFTools\WebHub\bin\WHCoverMgmt.exe /cover /appid=htq4 /minutes=9999999
 ::08Jan2011 OFF d:\Apps\HREFTools\miscutil\wait.exe 12
 d:\Apps\HREFTools\WebHub\bin\WHCoverMgmt.exe /cover /appid=scan /minutes=9999999 /reason="disabled; demo needs complete upgrade off BDE"
 
-start whShopping.exe  
-d:\Apps\HREFTools\miscutil\wait.exe 12
+if %demoshop1%==yes start whShopping.exe  
+if %demoshop1%==yes d:\Apps\HREFTools\miscutil\wait.exe 12
+if %demoshop1%==no  d:\Apps\HREFTools\WebHub\bin\WHCoverMgmt.exe /cover /appid=shop1 /minutes=%covermin% /reason=%coverreason%
 
 ::start whRubicon.exe 
 ::d:\Apps\HREFTools\miscutil\wait.exe 15
