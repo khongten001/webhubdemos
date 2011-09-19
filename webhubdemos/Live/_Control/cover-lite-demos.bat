bc2a910b0e222ff50bce2d10f7bee654a3defbb8
annlynnworth <annlynnworth@88ca0d87-9d1d-4ddd-a6d8-5c97b49fb611>
2011-09-19 01:56:26 +0000

    quotes in quotes

--- webhubdemos/Live/_Control/cover-lite-demos.bat
+++ webhubdemos/Live/_Control/cover-lite-demos.bat
@@ -1,9 +1,11 @@
 set covermin=999999
-set coverreason=(~APPID~) moved to <a href="http://lite.demos.href.com/">lite.demos.href.com</a>
+set url="http://lite.demos.href.com/"
+set coverreason="MOVED to <a href=%url%>lite.demos.href.com</a>"
 
 cd /d d:\Apps\HREFTools\WebHub\bin
 
 WHCoverMgmt.exe /cover /appid=adv /minutes=%covermin% /reason=%coverreason%
 WHCoverMgmt.exe /cover /appid=bw /minutes=%covermin% /reason=%coverreason%
 WHCoverMgmt.exe /cover /appid=joke /minutes=%covermin% /reason=%coverreason%
+WHCoverMgmt.exe /cover /appid=showcase /minutes=%covermin% /reason=%coverreason%
 
