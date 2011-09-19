ae99e35ab1315b9ad8aa99282ff74e86593022ba
annlynnworth <annlynnworth@88ca0d87-9d1d-4ddd-a6d8-5c97b49fb611>
2011-09-19 10:03:28 +0000

    cover pages for 2 disabled demos on new-ipc

--- trunk/webhubdemos/Live/_Control/run-more-demos.bat
+++ trunk/webhubdemos/Live/_Control/run-more-demos.bat
@@ -27,12 +27,13 @@ d:\Apps\HREFTools\miscutil\wait.exe 4
 start whConverter.exe 
 d:\Apps\HREFTools\miscutil\wait.exe 6
 
-start whASyncDemo.exe 
-d:\Apps\HREFTools\miscutil\wait.exe 6
+:: off 19-Sep-2011 start whASyncDemo.exe 
+:: off 19-Sep-2011 d:\Apps\HREFTools\miscutil\wait.exe 6
+d:\Apps\HREFTools\WebHub\bin\WHCoverMgmt.exe /cover /appid=htasync /minutes=999999 /reason="async feature is not yet ready for use with new-ipc"
 
 rem start whCOM.exe 
 rem d:\Apps\HREFTools\miscutil\wait.exe 6
-
+d:\Apps\HREFTools\WebHub\bin\WHCoverMgmt.exe /cover /appid=com /minutes=999999 /reason="COM demo not ready for new-ipc; may not be converted; talk to us if you need it"
 
 start f2hdemo.exe 
 d:\Apps\HREFTools\miscutil\wait.exe 6
