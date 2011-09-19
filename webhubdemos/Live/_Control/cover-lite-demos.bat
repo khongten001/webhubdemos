feb1b5da976daee4a1fc3a60d519dc3d7a9aecf0
annlynnworth <annlynnworth@88ca0d87-9d1d-4ddd-a6d8-5c97b49fb611>
2011-09-19 02:03:05 +0000

    Link in to a connecting page

--- webhubdemos/Live/_Control/cover-lite-demos.bat
+++ webhubdemos/Live/_Control/cover-lite-demos.bat
@@ -1,5 +1,5 @@
 set covermin=999999
-set url="http://lite.demos.href.com/"
+set url="http://lite.demos.href.com/demos:pgEnterLiteDEMOS"
 set coverreason="MOVED to <a href=%url%>lite.demos.href.com</a>"
 
 cd /d d:\Apps\HREFTools\WebHub\bin
