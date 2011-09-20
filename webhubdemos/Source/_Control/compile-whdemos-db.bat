69bcd8d65b50a60d2ea8a7838a4b1b64e22aebe3
annlynnworth <annlynnworth@88ca0d87-9d1d-4ddd-a6d8-5c97b49fb611>
2011-09-20 10:53:38 +0000

    Selective compilation of database demos, thus less to ftp to server for each test round

--- webhubdemos/Source/_Control/compile-whdemos-db.bat
+++ webhubdemos/Source/_Control/compile-whdemos-db.bat
@@ -12,52 +12,70 @@ d:
 cd %droot%\Lite Examples\whAppServer\whLite
 call %cbat% whLite
 
-cd %droot%\DB Examples\whQuery1
-call %cbat% whQuery1
+del %~dp0\..\..\Live\WebHub\Apps\whQuery1.exe /q
+if "%demohtq1%"=="" pause
+if NOT "%demohtq1%"=="no" cd %droot%\DB Examples\whQuery1
+if NOT "%demohtq1%"=="no" call %cbat% whQuery1
 
-cd %droot%\DB Examples\whQuery2
-call %cbat% whQuery2
+del %~dp0\..\..\Live\WebHub\Apps\whQuery2.exe /q
+if "%demohtq2%"=="" pause
+if NOT "%demohtq2%"=="no" cd %droot%\DB Examples\whQuery2
+if NOT "%demohtq2%"=="no" call %cbat% whQuery2
 
-cd %droot%\DB Examples\whQuery3
-call %cbat% whQuery3
+del %~dp0\..\..\Live\WebHub\Apps\whQuery3.exe /q
+if NOT "%demohtq3%"=="no" cd %droot%\DB Examples\whQuery3
+if NOT "%demohtq3%"=="no" call %cbat% whQuery3
 
-cd %droot%\DB Examples\whQuery4
-call %cbat% whQuery4
+del %~dp0\..\..\Live\WebHub\Apps\whQuery4.exe /q
+if NOT "%demohtq4%"=="no" cd %droot%\DB Examples\whQuery4
+if NOT "%demohtq4%"=="no" call %cbat% whQuery4
 
+:: democoderage
+del %~dp0\..\..\Live\WebHub\Apps\whSchedule.exe /q
 ::whSchedule uses IBObjects 
 ::cd %droot%\DB Examples\whSchedule
 ::::call %cbat% whSchedule
 
-cd %droot%\DB Examples\whFishStore
-call %cbat% whFishStore
+del %~dp0\..\..\Live\WebHub\Apps\whFishStore.exe /q
+if NOT "%demohtfs%"=="no" cd %droot%\DB Examples\whFishStore
+if NOT "%demohtfs%"=="no" call %cbat% whFishStore
 
-cd %droot%\DB Examples\whClone
-call %cbat% whClone
+del %~dp0\..\..\Live\WebHub\Apps\whClone.exe /q
+if NOT "%demohtcl%"=="no" cd %droot%\DB Examples\whClone
+if NOT "%demohtcl%"=="no" call %cbat% whClone
 
-cd %droot%\DB Examples\whLoadFromDB
-call %cbat% whLoadFromDB
+del %~dp0\..\..\Live\WebHub\Apps\whLoadFromDB.exe
+if NOT "%demodbhtml%"=="no" cd %droot%\DB Examples\whLoadFromDB
+if NOT "%demodbhtml%"=="no" call %cbat% whLoadFromDB
 
-cd %droot%\DB Examples\whShopping
-call %cbat% whShopping
+del %~dp0\..\..\Live\WebHub\Apps\whShopping.exe
+if NOT "%demoshop1%"=="no" cd %droot%\DB Examples\whShopping
+if NOT "%demoshop1%"=="no" call %cbat% whShopping
 
-cd %droot%\DB Examples\whInstantForm
-call %cbat% whInstantForm
+del %~dp0\..\..\Live\WebHub\Apps\whInstantForm.exe
+if NOT "%demohtfm%"=="no" cd %droot%\DB Examples\whInstantForm
+if NOT "%demohtfm%"=="no" call %cbat% whInstantForm
 
-cd %droot%\DB Examples\whScanTable
-call %cbat% whScanTable
+del %~dp0\..\..\Live\WebHub\Apps\whScanTable.exe
+if NOT "%demoscan%"=="no" cd %droot%\DB Examples\whScanTable
+if NOT "%demoscan%"=="no" call %cbat% whScanTable
 
-cd %droot%\More Examples\whDynamicJPEG
-call %cbat% whDynamicJPEG
+del %~dp0\..\..\Live\WebHub\Apps\whDynamicJPEG.exe
+if NOT "%demojpeg%"=="no" cd %droot%\More Examples\whDynamicJPEG
+if NOT "%demojpeg%"=="no" call %cbat% whDynamicJPEG
 
 ::whFirebird uses IBObjects 
-cd %droot%\DB Examples\whFirebird
-call %cbat% whFirebird
+del %~dp0\..\..\Live\WebHub\Apps\whFirebird.exe
+if NOT "%demofire%"=="no" cd %droot%\DB Examples\whFirebird
+if NOT "%demofire%"=="no" call %cbat% whFirebird
 
-cd %droot%\Third Party Examples\whRubicon
-call %cbat% whRubicon
+del %~dp0\..\..\Live\WebHub\Apps\whRubicon.exe
+if NOT "%demohtru%"=="no" cd %droot%\Third Party Examples\whRubicon
+if NOT "%demohtru%"=="no" call %cbat% whRubicon
 
-cd %droot%\Third Party Examples\whDSP
-call %cbat% whDSP
+del %~dp0\..\..\Live\WebHub\Apps\whDPP.exe
+if NOT "%demodsp%"=="no" cd %droot%\Third Party Examples\whDSP
+if NOT "%demodsp%"=="no" call %cbat% whDSP
 
 :END
 pause
