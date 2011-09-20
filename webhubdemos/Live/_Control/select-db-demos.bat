8cd4fd4c3f8a5e5b4cce532977251e322253002c
annlynnworth <annlynnworth@88ca0d87-9d1d-4ddd-a6d8-5c97b49fb611>
2011-09-20 10:53:06 +0000

    Flexibility on what to launch on different servers

---
+++ webhubdemos/Live/_Control/select-db-demos.bat
@@ -0,0 +1,50 @@
+:: select-db-demos.bat
+:: Copyright (c) 2011 HREF Tools Corp.
+:: www.href.com
+
+call %ZaphodsMap%\zmset.bat whipc UsingKey2Value "HREFTools\Install WebHub ipc old"
+
+echo .
+echo ***
+echo ipc is %whipc%
+echo ***
+echo .
+
+if "%whipc%"=="old" set demodbhtml=yes
+if "%whipc%"=="x"    set demodbhtml=no
+
+set demodsp=yes
+
+set democoderage=no
+
+if "%whipc%"=="old" set demofire=yes
+if "%whipc%"=="x"    set demofire=no
+
+if "%whipc%"=="old" set demohtcl=yes
+if "%whipc%"=="x"    set demohtcl=no
+
+if "%whipc%"=="old" set demohtfm=yes
+if "%whipc%"=="x"    set demohtfm=no
+
+set demohtfs=yes
+
+set demohtq1=no
+set demohtq2=no
+set demohtq3=no
+set demohtq4=no
+set demohtru=no
+
+:: not enough memory on APATE server for jpeg demo (BDE error)
+:: but it is fine on the stress test server
+:: 20-Sep-2011
+if "%whipc%"=="old" set demojpeg=no
+if "%whipc%"=="x"    set demojpeg=yes
+
+set demoscan=no
+
+if "%whipc%"=="old" set demoshop1=yes
+if "%whipc%"=="x"    set demoshop1=no
+
+set demostore000=no
+
+:end
