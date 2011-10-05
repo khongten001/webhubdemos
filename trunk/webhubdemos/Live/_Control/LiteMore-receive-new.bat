f65ee2f49d692764f6137f45cbebe9d44714f444
annlynnworth <annlynnworth@88ca0d87-9d1d-4ddd-a6d8-5c97b49fb611>
2011-10-05 09:22:02 +0000

    -aoa

--- trunk/webhubdemos/Live/_Control/LiteMore-receive-new.bat
+++ trunk/webhubdemos/Live/_Control/LiteMore-receive-new.bat
@@ -9,11 +9,11 @@ d:\Apps\HREFTools\WebHub\bin\WHCoverMgmt.exe /cover /appid=demos /minutes=10 "/r
 cd /d %~dp0
 cd ..\WebHub\Apps
 
-d:\Apps\Utilities\7Zip\7z.exe x LiteMore-bin.7z
+d:\Apps\Utilities\7Zip\7z.exe x LiteMore-bin.7z -aoa
 if errorlevel 1 pause
 
 cd ..\..\Library
-d:\Apps\Utilities\7Zip\7z.exe x LiteMore-Library-bin.7z
+d:\Apps\Utilities\7Zip\7z.exe x LiteMore-Library-bin.7z -aoa
 if errorlevel 1 pause
 
 :: now call startup procedure
