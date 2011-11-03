1c7f84899d1fef25e2e21ecdb4e96a910580e423
annlynnworth <annlynnworth@88ca0d87-9d1d-4ddd-a6d8-5c97b49fb611>
2011-11-03 02:51:05 +0000

    db, dsp

--- webhubdemos/Live/_Control/select-db-demos.bat
+++ webhubdemos/Live/_Control/select-db-demos.bat
@@ -10,15 +10,40 @@ echo ipc is %whipc%
 echo ***
 echo .
 
+set demodbhtml=no
+set demodsp=no
+set democoderage=no
+set demofire=no
+set demohtcl=no
+set demohtfm=no
+set demohtfs=no
+set demohtq1=no
+set demohtq2=no
+set demohtq3=no
+set demohtq4=no
+set demohtru=no
+set demojpeg=no
+set demoscan=no
+set demoshop1=no
+set demostore000=no
+
+
 if "%whipc%"=="old" set demodbhtml=yes
 if "%whipc%"=="x"    set demodbhtml=no
 
-set demodsp=yes
+call %ZaphodsMap%zmset.bat flagdemosdsp UsingKey2Value "HREFTools/WebHub/cv004 SystemStartup demosdsp"
+echo flagdemosdsp is %flagdemosdsp%
 
-set democoderage=no
+set demodsp=no
+if "%flagdemosdsp%"=="yes" set demodsp=yes
+
+call %ZaphodsMap%zmset.bat flagdemosdb UsingKey2Value "HREFTools/WebHub/cv004 SystemStartup demosdb"
+echo flagdemosdb is %flagdemosdb%
+
+if "%flagdemosdb%"=="no" goto end
 
 if "%whipc%"=="old" set demofire=yes
-if "%whipc%"=="x"    set demofire=no
+if "%whipc%"=="x"    set demofire=yes
 
 if "%whipc%"=="old" set demohtcl=yes
 if "%whipc%"=="x"    set demohtcl=no
@@ -27,23 +52,12 @@ if "%whipc%"=="old" set demohtfm=yes
 if "%whipc%"=="x"    set demohtfm=no
 
 if "%whipc%"=="old" set demohtfs=yes
-if "%whipc%"=="x"    set demohtfs=no
+if "%whipc%"=="x"    set demohtfs=yes
 
-
-set demohtq1=no
-set demohtq2=no
-set demohtq3=no
-set demohtq4=no
-set demohtru=no
-
-if "%whipc%"=="old" set demojpeg=no
-if "%whipc%"=="x"    set demojpeg=no
-
-set demoscan=no
+if "%whipc%"=="old" set demojpeg=yes
+if "%whipc%"=="x"    set demojpeg=yes
 
 if "%whipc%"=="old" set demoshop1=yes
 if "%whipc%"=="x"    set demoshop1=no
-
-set demostore000=no
 
 :end
