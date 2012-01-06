e15f82fe2668436746cbd5c27b06410e4adfe8dd
annlynnworth <annlynnworth@88ca0d87-9d1d-4ddd-a6d8-5c97b49fb611>
2012-01-06 07:35:20 +0000

    erase temp DCU files between compiles in case compiler options change moment to moment

--- trunk/webhubdemos/Source/_Control/compile-1demo_d16_win32_svc.bat
+++ trunk/webhubdemos/Source/_Control/compile-1demo_d16_win32_svc.bat
@@ -1,18 +1,22 @@
 @echo off
 setlocal
+
+del d:\temp\DelphiTempDCU\*.dcu
+
 :: use ZaphodsMap to find compiler
 :: zmset.bat and ZMLookup.exe are FREE from HREF Tools Corp. via www.zaphodsmap.com
 call %ZaphodsMap%zmset.bat d16 UsingKey2Folder "HREFTools\Production\cv001 Delphi D16"
 set dcc=%d16%bin\dcc32.exe
 if not exist %dcc% pause
 
-::set raizelib=K:\Vendors\Raize\CodeSite4\Source\VCL\Indy;K:\Vendors\Raize\CodeSite4\Source\VCL
-set libsearchpath="h:\;h:\dcu_d16_win32;h:\pkg_d16_win32;%raizelib%;%d16%\lib\win32\release;"
+set raizelib=K:\Vendors\Raize\CodeSite5\Source\VCL\Indy;K:\Vendors\Raize\CodeSite5\Source\VCL
+set libsearchpath="h:\;h:\dcu_d16_win32;h:\pkg_d16_win32;%raizelib%;%d16%\lib\win32\debug;"
 set outputroot="d:\Projects\WebHubDemos\Live\WebHub\Apps"
 set pkg="vcl;vclx;soaprtl;xmlrtl;inet;"
 set compilerflags=
 set includepath=h:\;
 set dcu=d:\temp\DelphiTempDCU
+set respath=%d16%lib\win32\release
 
 :: extra parameters for Delphi XE2
 set dccflags=--no-config -M -Q -AGenerics.Collections=System.Generics.Collections;Generics.Defaults=System.Generics.Defaults;WinTypes=Windows;WinProcs=Windows;DbiTypes=BDE;DbiProcs=BDE;DbiErrs=BDE
@@ -24,7 +28,7 @@ ren %1.cfg %1.off
 echo 1demo as-service d16_win32 %1
 
 @echo on
-"%dcc%"  -w -h -b %1.dpr  -n%dcu% -E%outputroot% -D%compilerflags% -LU%pkg% -u%libsearchpath% -R%libsearchpath% -I%includepath% /$D- /$L- /$Y- /$Q- /$R %dccflags% %dccns%
+"%dcc%"  -w -h -b %1.dpr  -n%dcu% -E%outputroot% -D%compilerflags% -LU%pkg% -u%libsearchpath% -R%respath%;%libsearchpath% -I%includepath% /$D- /$L- /$Y- /$Q- /$R %dccflags% %dccns%
 if errorlevel 1 pause
 
 @echo off
