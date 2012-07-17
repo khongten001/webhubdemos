51fd1d4cc599beb781b06ac4e53dc2a8cd65f897
annlynnworth <annlynnworth@88ca0d87-9d1d-4ddd-a6d8-5c97b49fb611>
2012-07-17 07:26:49 +0000

    copyright notice; added memo1 with some status info

--- trunk/webhubdemos/Source/WHApps/DB Examples/whSchedule/whSchedule_fmKeywordIndex.pas
+++ trunk/webhubdemos/Source/WHApps/DB Examples/whSchedule/whSchedule_fmKeywordIndex.pas
@@ -1,11 +1,12 @@
 unit whSchedule_fmKeywordIndex;
 
 { ---------------------------------------------------------------------------- }
-{ * Copyright (c) 1995-2011 HREF Tools Corp.  All Rights Reserved Worldwide. * }
+{ * Copyright (c) 2012 HREF Tools Corp.  All Rights Reserved Worldwide.      * }
 { *                                                                          * }
-{ * This source code file is part of WebHub v2.1x.  Please obtain a WebHub   * }
-{ * development license from HREF Tools Corp. before using this file, and    * }
-{ * refer friends and colleagues to http://www.href.com/webhub. Thanks!      * }
+{ * WebHub panel for creating the keyword index using Rubicon.               * }
+{ *                                                                          * }
+{ * Requires: Firebird SQL                                                   * }
+{ *           Rubicon components from HREF Tools Corp.                       * }
 { ---------------------------------------------------------------------------- }
 
 interface
@@ -27,6 +28,7 @@ type
     ActionList1: TActionList;
     tpToolButton1: TtpToolButton;
     ActionCreateIndex: TAction;
+    Memo1: TMemo;
     procedure ActionCreateIndexExecute(Sender: TObject);
     procedure FormCreate(Sender: TObject);
     procedure FormDestroy(Sender: TObject);
@@ -54,7 +56,10 @@ implementation
 {$R *.dfm}
 
 uses
-  webLink, tpFirebirdCredentials, uFirebird_Connect_CodeRageSchedule,
+  {$IFDEF CodeSite}CodeSiteLogging,{$ENDIF}
+  webLink,
+  tpFirebirdCredentials, uFirebird_Connect_CodeRageSchedule,
+  ucCodeSiteInterface,
   whdemo_DMIBObjCodeGen;
 
 { TfmRubiconIndex }
@@ -64,65 +69,82 @@ var
   DBName, DBUser, DBPass: string;
 begin
   inherited;
-  makewordslink := TrbMakeWordsIBOLink.Create(Self);
-  makewordslink.Name := 'makewordslink';
-  makewordslink.TableName := 'WORDS';
-
-  rbMake1 := TrbMake.Create(Self);
-  rbMake1.Name := 'rbMake1';
-  rbMake1.Ansi := False;  // UTF16
-
-  rbAccept1 := TrbAccept.Create(Self);
-  rbAccept1.Name := 'rbAccept1';
-  rbAccept1.Alpha := CNFInternationalAlphabet;
-  rbAccept1.NoLeadingNumbers := True;
-
-  rbMake1.WordDelims := CNFInternationalWordDelims;
-  rbMake1.MinWordLen := 4; // cCNFMinWordLen;
-
-  rbProgressDialog1 := TrbProgressDialog.Create(Self);
-  rbProgressDialog1.Name := 'rbProgressDialog1';
-  rbProgressDialog1.Engine := rbMake1;
-
-  rbCache1 := TrbCache.Create(Self);
-  rbCache1.Name := 'rbCache1';
-  rbMake1.Cache := rbCache1;
-
   ZMLookup_Firebird_Credentials(DMIBObjCodeGen.ProjectAbbreviationNoSpaces,
     DBName, DBUser, DBPass);
+  Memo1.Lines.Text := DBName;
+
   CreateIfNil(DBName, DBUser, DBPass);
+
   if NOT gCodeRageSchedule_Conn.Connected then
     gCodeRageSchedule_Conn.Connect;
 
-  QMake := TIBOQuery.Create(Self);
-  QMake.Name := 'QMake';
-  QMake.IB_Connection := gCodeRageSchedule_Conn;
-  QMake.IB_Transaction := gCodeRageschedule_Tr;
+  if NOT Assigned(makewordslink) then
+  begin
+    makewordslink := TrbMakeWordsIBOLink.Create(Self);
+    makewordslink.Name := 'makewordslink';
+    makewordslink.TableName := 'WORDS';
 
-  QData := TIBOQuery.Create(Self);
-  QData.Name := 'QData';
-  QData.IB_Connection := gCodeRageSchedule_Conn;
-  QData.IB_Transaction := gCodeRageschedule_Tr;
-  makewordslink.IBOQuery := QData;
+    rbMake1 := TrbMake.Create(Self);
+    rbMake1.Name := 'rbMake1';
+    rbMake1.Ansi := False;  // UTF16
 
-  rbMakeTextLink1 := TrbMakeTextIBOLink.Create(Self);
-  rbMakeTextLink1.Name := 'rbMakeTextLink1';
-  rbMakeTextLink1.TableName := 'Schedule';
-  rbMakeTextLink1.IndexFieldName := 'SchNo';
-  rbMakeTextLink1.FieldNames.Text :=
-    'SchTitle' + sLineBreak +
-    'SchPresenterFullName' + sLineBreak +
-    'SchPresenterOrg' + sLineBreak + 'SchBlurb';
-  rbMakeTextLink1.IBOQuery := QMake;
-  rbMake1.TextLink := rbMakeTextLink1;
-  rbMake1.WordsLink := makewordslink;
+    rbAccept1 := TrbAccept.Create(Self);
+    rbAccept1.Name := 'rbAccept1';
+    rbAccept1.Alpha := CNFInternationalAlphabet;
+    rbAccept1.NoLeadingNumbers := True;
 
-  rbMake1.Execute;
+    rbMake1.WordDelims := CNFInternationalWordDelims;
+    rbMake1.MinWordLen := 4; // cCNFMinWordLen;
+
+    rbProgressDialog1 := TrbProgressDialog.Create(Self);
+    rbProgressDialog1.Name := 'rbProgressDialog1';
+    rbProgressDialog1.Engine := rbMake1;
+
+    rbCache1 := TrbCache.Create(Self);
+    rbCache1.Name := 'rbCache1';
+    rbMake1.Cache := rbCache1;
+
+    QMake := TIBOQuery.Create(Self);
+    QMake.Name := 'QMake';
+    QMake.IB_Connection := gCodeRageSchedule_Conn;
+    QMake.IB_Transaction := gCodeRageschedule_Tr;
+
+    QData := TIBOQuery.Create(Self);
+    QData.Name := 'QData';
+    QData.IB_Connection := gCodeRageSchedule_Conn;
+    QData.IB_Transaction := gCodeRageschedule_Tr;
+    makewordslink.IBOQuery := QData;
+
+    rbMakeTextLink1 := TrbMakeTextIBOLink.Create(Self);
+    rbMakeTextLink1.Name := 'rbMakeTextLink1';
+    rbMakeTextLink1.TableName := 'Schedule';
+    rbMakeTextLink1.IndexFieldName := 'SchNo';
+    rbMakeTextLink1.FieldNames.Text :=
+      'SchTitle' + sLineBreak +
+      'SchPresenterFullName' + sLineBreak +
+      'SchPresenterOrg' + sLineBreak + 'SchBlurb';
+    rbMakeTextLink1.IBOQuery := QMake;
+    rbMake1.TextLink := rbMakeTextLink1;
+    rbMake1.WordsLink := makewordslink;
+  end;
+
+  Memo1.Lines.Add('Starting to use Rubicon to create the keyword index');
+  try
+    rbMake1.Execute;
+    Memo1.Lines.Add('Done. ' + FormatDateTime('dddd hh:nn:mm', Now));
+  except
+    on E: Exception do
+    begin
+      {$IFDEF CodeSite}CodeSite.SendException(E);{$ENDIF}
+      Memo1.Lines.Add(E.Message);
+    end;
+  end;
 end;
 
 procedure TfmRubiconIndex.FormCreate(Sender: TObject);
 begin
   inherited;
+  Memo1.Clear;
   makewordslink := nil;
   rbMake1 := nil;
   rbProgressDialog1 := nil;
