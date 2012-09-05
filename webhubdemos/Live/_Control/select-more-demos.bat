6ef1b1de5b802bb4ff543c468dae50701fad195e
annlynnworth <annlynnworth@88ca0d87-9d1d-4ddd-a6d8-5c97b49fb611>
2012-09-05 05:42:30 +0000

    f2h off

--- webhubdemos/Live/_Control/select-more-demos.bat
+++ webhubdemos/Live/_Control/select-more-demos.bat
@@ -21,7 +21,8 @@ if "%whipc%"=="x"    set demohtasync=no
 if "%whipc%"=="old" set democom=no
 if "%whipc%"=="x"    set democom=no
 
-set demodfm2html=yes
+:: 4-Sep-2012 Form2Html is just not the WebHub way of doing things
+set demodfm2html=no
 
 if "%whipc%"=="old" set demohtdr=yes
 if "%whipc%"=="x" set demohtdr=no
