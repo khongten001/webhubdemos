69eb69cc87fbfb732a64a8478fea435d6f5b0968
annlynnworth <annlynnworth@88ca0d87-9d1d-4ddd-a6d8-5c97b49fb611>
2012-09-21 05:23:34 +0000

    unit to create the listbox on the fly as needed for the code-gen GUI

---
+++ trunk/webhubdemos/Source/WHApps/Common/whdemo_IbObjCodeGenGUI.pas
@@ -0,0 +1,57 @@
+unit whdemo_IbObjCodeGenGUI;
+
+{ ---------------------------------------------------------------------------- }
+{ * Copyright (c) 2012 HREF Tools Corp.  All Rights Reserved Worldwide.      * }
+{ *                                                                          * }
+{ * This source code file is part of WebHub v2.1x.  Please obtain a WebHub   * }
+{ * development license from HREF Tools Corp. before using this file, and    * }
+{ * refer friends and colleagues to http://www.href.com/webhub. Thanks!      * }
+{ ---------------------------------------------------------------------------- }
+
+{ ---------------------------------------------------------------------------- }
+{ * Requires IBObjects from www.ibobjects.com                                * }
+{ * Requires Interbase or Firebird SQL Database                              * }
+{ * Requires WebHub v2.175+                                                  * }
+{ *                                                                          * }
+{ * EXTREMELY EXPERIMENTAL !                                                 * }
+{ *                                                                          * }
+{ ---------------------------------------------------------------------------- }
+
+interface
+
+uses
+  SysUtils, Classes, Controls, StdCtrls;
+
+procedure CreateCodeGenPatternListbox(var ListBox: TListBox;
+  InParent: TWinControl);
+
+implementation
+
+uses
+  Math;
+
+procedure CreateCodeGenPatternListbox(var ListBox: TListBox;
+  InParent: TWinControl);
+begin
+  if ListBox = nil then
+    ListBox := TListBox.Create(InParent);
+  ListBox.Visible := False;
+  ListBox.Parent := InParent;
+  ListBox.Top := 3;
+  ListBox.Left := 3;
+  ListBox.Height := InParent.Height - 6;
+  ListBox.Width := Min(370, InParent.Width - 3);
+  ListBox.Items.Clear;
+  ListBox.Items.Add('Whteko: Macros for Field Labels');
+  ListBox.Items.Add('Whteko: Macros for Primary Keys');
+  ListBox.Items.Add('Ini: Field List for IBObjects Import');
+  ListBox.Items.Add('Whteko: Droplets with Select SQL for each table');
+  ListBox.Items.Add('Whteko: Droplets with Update SQL for each table');
+  ListBox.Items.Add('Whteko: Readonly Form for each table');
+  ListBox.Items.Add('Whteko: Editable Form for each table');
+  ListBox.Items.Add('Whteko: Editable Form, labels above, for each table');
+  ListBox.Visible := True;
+  ListBox.Selected[0] := True;
+end;
+
+end.
