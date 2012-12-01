a0619ec00783fec5027fe59b4ee14d3894831977
annlynnworth <annlynnworth@88ca0d87-9d1d-4ddd-a6d8-5c97b49fb611>
2012-12-01 01:57:50 +0000

    manually corrected to support current IBObjects need for IB_Session

--- trunk/webhubdemos/Source/WHApps/DB Examples/whFirebird/uFirebird_Connect_Employee.pas
+++ trunk/webhubdemos/Source/WHApps/DB Examples/whFirebird/uFirebird_Connect_Employee.pas
@@ -1,6 +1,9 @@
+//unit uFirebird_Connect_Employee;
 unit uFirebird_Connect_Employee;
+// database engine: Firebird
 // project abbreviation: Employee
-// see Firebird_GenPAS_Connect
+// generated 01-Dec-2012 01:47
+// by IbAndFb_GenPAS_Connect
 
 interface 
 
@@ -19,8 +22,8 @@ var
   gEmployee_Sess: TIB_Session = nil;
 
 const
-  gUpdatedOnAtFieldname = 'UpdatedOnAt';
-  gUpdateCounterFieldname = 'UpdateCounter';
+  gUpdatedOnAtFieldname = '';
+  gUpdateCounterFieldname = '';
 
 implementation
 
@@ -29,22 +32,27 @@ procedure CreateIfNil(const FullDatabaseName: string;
 begin
   if gEmployee_Conn = nil then
   begin
-    gEmployee_Conn := TIB_Connection.Create(nil);
-    gEmployee_Conn.Name := 'gEmployee_Conn';
-    gEmployee_Conn.CharSet := 'UTF8';
-    gEmployee_Tr := TIB_Transaction.Create(nil);
-    gEmployee_Tr.Name := 'gEmployee_Tr';
     gEmployee_Sess := TIB_Session.Create(nil);
     gEmployee_Sess.Name := 'gEmployee_Sess';
+
+    gEmployee_Conn := TIB_Connection.Create(nil);
+    gEmployee_Conn.Name := 'gEmployee_Conn';
+
+    gEmployee_Tr := TIB_Transaction.Create(nil);
+    gEmployee_Tr.Name := 'gEmployee_Tr';
+
     with gEmployee_Conn do
     begin
       Database := FullDatabaseName;
       Username := InUsername;
       Password := InPassword;
+      IB_Session := gEmployee_Sess;
+      CharSet := 'UTF8';
     end;
     with gEmployee_Tr do
     begin
       IB_Connection := gEmployee_Conn;
+      IB_Session := gEmployee_Sess;
       Isolation := tiCommitted; // note the default is tiConcurrency
     end;
   end;
@@ -61,3 +69,4 @@ finalization
   end;
 
 end.
+
