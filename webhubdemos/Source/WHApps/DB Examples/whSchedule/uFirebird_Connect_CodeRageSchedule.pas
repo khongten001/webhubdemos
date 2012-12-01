7f7f20ebfc543e41f0b766ab13654b15cfc0196b
annlynnworth <annlynnworth@88ca0d87-9d1d-4ddd-a6d8-5c97b49fb611>
2012-12-01 02:07:12 +0000

    generated by corrected tpack unit

--- webhubdemos/Source/WHApps/DB Examples/whSchedule/uFirebird_Connect_CodeRageSchedule.pas
+++ webhubdemos/Source/WHApps/DB Examples/whSchedule/uFirebird_Connect_CodeRageSchedule.pas
@@ -1,8 +1,9 @@
 //unit uFirebird_Connect_CodeRageSchedule;
 unit uFirebird_Connect_CodeRageSchedule;
+// database engine: Firebird
 // project abbreviation: CodeRageSchedule
-// generated 19-Jul-2012 07:57
-// by Firebird_GenPAS_Connect
+// generated 01-Dec-2012 02:06
+// by IbAndFb_GenPAS_Connect
 
 interface 
 
@@ -31,22 +32,24 @@ procedure CreateIfNil(const FullDatabaseName: string;
 begin
   if gCodeRageSchedule_Conn = nil then
   begin
-    gCodeRageSchedule_Conn := TIB_Connection.Create(nil);
-    gCodeRageSchedule_Conn.Name := 'gCodeRageSchedule_Conn';
-    gCodeRageSchedule_Conn.CharSet := 'UTF8';
-    gCodeRageSchedule_Tr := TIB_Transaction.Create(nil);
-    gCodeRageSchedule_Tr.Name := 'gCodeRageSchedule_Tr';
     gCodeRageSchedule_Sess := TIB_Session.Create(nil);
     gCodeRageSchedule_Sess.Name := 'gCodeRageSchedule_Sess';
+    gCodeRageSchedule_Conn := TIB_Connection.Create(nil);
+    gCodeRageSchedule_Conn.Name := 'gCodeRageSchedule_Conn';
+    gCodeRageSchedule_Tr := TIB_Transaction.Create(nil);
+    gCodeRageSchedule_Tr.Name := 'gCodeRageSchedule_Tr';
     with gCodeRageSchedule_Conn do
     begin
+      IB_Session := gCodeRageSchedule_Sess;
       Database := FullDatabaseName;
       Username := InUsername;
       Password := InPassword;
+      CharSet := 'UTF8';
     end;
     with gCodeRageSchedule_Tr do
     begin
       IB_Connection := gCodeRageSchedule_Conn;
+      IB_Session := gCodeRageSchedule_Sess;
       Isolation := tiCommitted; // note the default is tiConcurrency
     end;
   end;
