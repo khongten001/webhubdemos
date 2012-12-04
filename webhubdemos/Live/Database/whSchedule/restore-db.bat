15984e0c9ed10df344a143458984d379143849d3
annlynnworth <annlynnworth@88ca0d87-9d1d-4ddd-a6d8-5c97b49fb611>
2012-12-04 22:18:22 +0000

    Obtain credentials from ZaphodsMap

--- webhubdemos/Live/Database/whSchedule/restore-db.bat
+++ webhubdemos/Live/Database/whSchedule/restore-db.bat
@@ -1,3 +1,20 @@
-d:\Apps\Firebird\Firebird_2_5\bin\gbak -v -z -recreate_database overwrite -user SYSDBA -password masterkey "D:\Projects\WebHubDemos\Live\Database\whSchedule\backup\coderage%1.fbk" 192.168.42.93:coderageschedule
+setlocal
+
+set thisdate=2012-12-04
+
+:: use ZaphodsMap to find credentials
+call %ZaphodsMap%zmset.bat dbname UsingKey2Value "FirebirdSQL Credentials-CodeRageSchedule database"
+call %ZaphodsMap%zmset.bat u UsingKey2Value "FirebirdSQL Credentials-CodeRageSchedule user"
+call %ZaphodsMap%zmset.bat p UsingKey2Value "FirebirdSQL Credentials-CodeRageSchedule pass"
+
+cls
+echo RESTORE backup dated %thisdate% to the following
+echo dbname %dbname%
+echo user %u%
+pause
+
+cd /d %~dp0
+
+d:\Apps\Firebird\Firebird_2_5\bin\gbak -v -z -recreate_database overwrite -user %u% -password %p% backup\coderage_%thisdate%.fbk %dbname%
 
 pause
