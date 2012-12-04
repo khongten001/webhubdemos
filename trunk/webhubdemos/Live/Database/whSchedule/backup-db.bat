00f9db8c35df66178d0704994231359a4f1a4c7a
annlynnworth <annlynnworth@88ca0d87-9d1d-4ddd-a6d8-5c97b49fb611>
2012-12-04 21:51:22 +0000

    Obtain credentials from ZaphodsMap

--- trunk/webhubdemos/Live/Database/whSchedule/backup-db.bat
+++ trunk/webhubdemos/Live/Database/whSchedule/backup-db.bat
@@ -1,5 +1,20 @@
+call D:\AppsData\WinNT\Bat\set-ymd.bat thisdate yyyy-mm-dd
+
+:: use ZaphodsMap to find credentials
+call %ZaphodsMap%zmset.bat dbname UsingKey2Value "FirebirdSQL Credentials-CodeRageSchedule database"
+call %ZaphodsMap%zmset.bat u UsingKey2Value "FirebirdSQL Credentials-CodeRageSchedule user"
+call %ZaphodsMap%zmset.bat p UsingKey2Value "FirebirdSQL Credentials-CodeRageSchedule pass"
+
+cls
+echo %thisdate%
+echo dbname %dbname%
+echo user %u%
+echo pass %p%
+pause
+
+cd /d %~dp0
 
 REM gbak <options> -user <username> -password <password> <source> <destination>
-D:\Apps\Firebird\Firebird_2_5\bin\gbak -v -z -user SYSDBA -password masterkey 192.168.42.93:coderageschedule "D:\Projects\WebHubDemos\Live\Database\whSchedule\backup\coderage%1.fbk" 
+D:\Apps\Firebird\Firebird_2_5\bin\gbak -v -z -user %u% -password %p% %dbname% backup\coderage_%thisdate%.fbk
 
 pause
