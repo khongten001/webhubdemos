838190546c26055af85c381bd2043a28fd81e5d1
annlynnworth <annlynnworth@88ca0d87-9d1d-4ddd-a6d8-5c97b49fb611>
2013-01-10 05:22:59 +0000

    f2h is gone. htoi is new.

--- trunk/webhubdemos/Source/_Control/LiteMore-step01-7z-bin.bat
+++ trunk/webhubdemos/Source/_Control/LiteMore-step01-7z-bin.bat
@@ -13,15 +13,14 @@ del LiteMore-Library-bin.7z
 d:\Apps\Utilities\7Zip\7z.exe a LiteMore-bin.7z whLite.exe
 if errorlevel 1 pause
 ::d:\Apps\Utilities\7Zip\7z.exe a LiteMore-bin.7z DServerD1?.exe
-d:\Apps\Utilities\7Zip\7z.exe a LiteMore-bin.7z whCom.exe
 d:\Apps\Utilities\7Zip\7z.exe a LiteMore-bin.7z whConverter.exe
 d:\Apps\Utilities\7Zip\7z.exe a LiteMore-bin.7z whDropdown.exe
 d:\Apps\Utilities\7Zip\7z.exe a LiteMore-bin.7z whObjectInspector.exe
 d:\Apps\Utilities\7Zip\7z.exe a LiteMore-bin.7z whOutline.exe
+d:\Apps\Utilities\7Zip\7z.exe a LiteMore-bin.7z whOpenID.exe
 d:\Apps\Utilities\7Zip\7z.exe a LiteMore-bin.7z whSendmail.exe
 d:\Apps\Utilities\7Zip\7z.exe a LiteMore-bin.7z whStopspam.exe
 d:\Apps\Utilities\7Zip\7z.exe a LiteMore-bin.7z whText2Table.exe
-d:\Apps\Utilities\7Zip\7z.exe a LiteMore-bin.7z f2hdemo.exe
 d:\Apps\Utilities\7Zip\7z.exe a LiteMore-bin.7z whAsyncDemo.exe
 
 set t=LiteMore-Library-bin.7z
