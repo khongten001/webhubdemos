58fed8c1e78af9e54e7b02ff58371a02ccae6be2
annlynnworth <annlynnworth@88ca0d87-9d1d-4ddd-a6d8-5c97b49fb611>
2013-01-29 22:05:24 +0000

    convert any paradox table

--- trunk/webhubdemos/Source/WHApps/DB Examples/whFishStore/Paradox2Xml/Paradox2Xml.dpr
+++ trunk/webhubdemos/Source/WHApps/DB Examples/whFishStore/Paradox2Xml/Paradox2Xml.dpr
@@ -2,13 +2,16 @@ program Paradox2Xml;
 
 uses
   Vcl.Forms,
-  fmGUI in 'fmGUI.pas' {Form3};
+  fmGUI in 'fmGUI.pas' {Form3},
+  Vcl.Themes,
+  Vcl.Styles;
 
 {$R *.res}
 
 begin
   Application.Initialize;
   Application.MainFormOnTaskbar := True;
+  TStyleManager.TrySetStyle('Turquoise Gray');
   Application.CreateForm(TForm3, Form3);
   Application.Run;
 end.
