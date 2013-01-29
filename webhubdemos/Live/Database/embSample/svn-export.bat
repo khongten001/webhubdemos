c386edffd4bee17e12438fc7d8d45136e1f054fa
annlynnworth <annlynnworth@88ca0d87-9d1d-4ddd-a6d8-5c97b49fb611>
2013-01-29 22:23:18 +0000

    employee and parts, also

--- webhubdemos/Live/Database/embSample/svn-export.bat
+++ webhubdemos/Live/Database/embSample/svn-export.bat
@@ -1,6 +1,15 @@
 :: Embarcadero sample files are in subversion and included when you install Delphi.
 :: http://docwiki.embarcadero.com/CodeExamples/XE3/en/Category:Sample
 
-D:\apps\Utilities\SVN\CollabNet\svn.exe export https://radstudiodemos.svn.sourceforge.net/svnroot/radstudiodemos/branches/RadStudio_XE3_Update/Data/biolife.xml ./biolife.xml
+setlocal
+set svnpath=D:\apps\Utilities\SVN\CollabNet
+set urlroot=https://radstudiodemos.svn.sourceforge.net/svnroot/radstudiodemos/branches/RadStudio_XE3_Update
+%svnpath%\svn.exe export %urlroot%/Data/employee.xml ./employee.xml
 if errorlevel 1 pause
+%svnpath%\svn.exe export %urlroot%/Data/parts.xml ./parts.xml
+if errorlevel 1 pause
+%svnpath%\svn.exe export %urlroot%/Data/biolife.xml ./biolife.xml
+if errorlevel 1 pause
+
+endlocal
 
