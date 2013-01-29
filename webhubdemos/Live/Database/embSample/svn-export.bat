a36b5b93bc46f012c9227a1c11a82bdbc6998b7c
annlynnworth <annlynnworth@88ca0d87-9d1d-4ddd-a6d8-5c97b49fb611>
2013-01-29 03:56:30 +0000

    export clean files at will

---
+++ webhubdemos/Live/Database/embSample/svn-export.bat
@@ -0,0 +1,6 @@
+:: Embarcadero sample files are in subversion and included when you install Delphi.
+:: http://docwiki.embarcadero.com/CodeExamples/XE3/en/Category:Sample
+
+D:\apps\Utilities\SVN\CollabNet\svn.exe export https://radstudiodemos.svn.sourceforge.net/svnroot/radstudiodemos/branches/RadStudio_XE3_Update/Data/biolife.xml ./biolife.xml
+if errorlevel 1 pause
+
