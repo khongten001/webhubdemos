fb3481085442e2a17fce1659f48253d0de5a5ab5
annlynnworth <annlynnworth@88ca0d87-9d1d-4ddd-a6d8-5c97b49fb611>
2013-02-01 11:28:44 +0000

    always take latest from Embarcadero

--- trunk/webhubdemos/Live/Database/embSample/svn-export.bat
+++ trunk/webhubdemos/Live/Database/embSample/svn-export.bat
@@ -4,22 +4,22 @@
 setlocal
 set svnpath=D:\apps\Utilities\SVN\CollabNet
 set urlroot=https://radstudiodemos.svn.sourceforge.net/svnroot/radstudiodemos/branches/RadStudio_XE3_Update
-%svnpath%\svn.exe export %urlroot%/Data/employee.xml ./employee.xml
+%svnpath%\svn.exe export %urlroot%/Data/employee.xml ./employee.xml --accept theirs-full
 if errorlevel 1 pause
-%svnpath%\svn.exe export %urlroot%/Data/parts.xml ./parts.xml
+%svnpath%\svn.exe export %urlroot%/Data/parts.xml ./parts.xml --accept theirs-full
 if errorlevel 1 pause
-%svnpath%\svn.exe export %urlroot%/Data/biolife.xml ./biolife.xml
+%svnpath%\svn.exe export %urlroot%/Data/biolife.xml ./biolife.xml --accept theirs-full
 if errorlevel 1 pause
 
 :: cd ..\whInstantForm
 :: del parts.*
-:: %svnpath%\svn.exe export %urlroot%/Data/parts.db ./parts.db
+:: %svnpath%\svn.exe export %urlroot%/Data/parts.db ./parts.db --accept theirs-full
 :: if errorlevel 1 pause
-:: %svnpath%\svn.exe export %urlroot%/Data/parts.px ./parts.px
-:: %svnpath%\svn.exe export %urlroot%/Data/parts.VAL ./parts.VAL
-:: %svnpath%\svn.exe export %urlroot%/Data/parts.x02 ./parts.x02
-:: %svnpath%\svn.exe export %urlroot%/Data/parts.y02 ./parts.y02
-:: %svnpath%\svn.exe export %urlroot%/Data/parts.xg0 ./parts.xg0
-:: %svnpath%\svn.exe export %urlroot%/Data/parts.yg0 ./parts.yg0
+:: %svnpath%\svn.exe export %urlroot%/Data/parts.px ./parts.px --accept theirs-full
+:: %svnpath%\svn.exe export %urlroot%/Data/parts.VAL ./parts.VAL --accept theirs-full
+:: %svnpath%\svn.exe export %urlroot%/Data/parts.x02 ./parts.x02 --accept theirs-full
+:: %svnpath%\svn.exe export %urlroot%/Data/parts.y02 ./parts.y02 --accept theirs-full
+:: %svnpath%\svn.exe export %urlroot%/Data/parts.xg0 ./parts.xg0 --accept theirs-full
+:: %svnpath%\svn.exe export %urlroot%/Data/parts.yg0 ./parts.yg0 --accept theirs-full
 
 endlocal
