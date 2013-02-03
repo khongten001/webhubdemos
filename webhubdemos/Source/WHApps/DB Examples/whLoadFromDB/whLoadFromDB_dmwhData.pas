bb46fc19c51ecac25bceaf53e4f1d03f9ffbef9f
annlynnworth <annlynnworth@88ca0d87-9d1d-4ddd-a6d8-5c97b49fb611>
2013-02-03 07:21:28 +0000

    DBGrid etc not active except upon-request with no surfers engaged

--- webhubdemos/Source/WHApps/DB Examples/whLoadFromDB/whLoadFromDB_dmwhData.pas
+++ webhubdemos/Source/WHApps/DB Examples/whLoadFromDB/whLoadFromDB_dmwhData.pas
@@ -36,14 +36,26 @@ type
   private
     { Private declarations }
     FlagInitDone: Boolean;
+    procedure AddPage(const defaultlingvo: String;
+      const pageid: String; const attributes: String; const pagecontent: String);
+    procedure AddMacros(const defaultlingvo: String;
+      const macrocontent: String);
     procedure WebAppUpdate(Sender: TObject);
   public
     { Public declarations }
     function Init(out ErrorText: string): Boolean;
+    function LoadWebHubTekerosFromDB: Boolean;
+    function LoadWebHubTekeroFromDB: Boolean;
+    procedure SendBufferedStringFromDB(const Value: string;
+      var Handled: Boolean);
   end;
 
 var
   DMContent: TDMContent;
+const
+  cIDField = 'Identifier';
+  cWhenToLoadField = 'WhenToLoad';
+  cTextField = 'Text';
 
 implementation
 
@@ -51,9 +63,83 @@ implementation
 
 uses
   {$IFDEF CodeSite}CodeSiteLogging,{$ENDIF}
-  webApp, htWebApp, ucCodeSiteInterface, whdemo_ViewSource;
+  Variants,
+  ucCodeSiteInterface,
+  webApp, webSplat, htbdeWApp, htWebApp, htmConst, webRead, webList,
+  whdemo_ViewSource;
+
+const
+  cTypeField = 'Type';
+  cPageIdentifier = whFolio;
+  cAttributesField = 'Attributes';
 
 { TDMContent }
+
+procedure TDMContent.AddMacros(const defaultlingvo, macrocontent: String);
+var
+  ms: TMemoryStream;
+  S: String;
+const
+  cEq = '=';
+begin
+  ms := nil;
+  try
+    ms := TMemoryStream.Create;
+
+    S := whTekoBegin + ' ' + clingvo + cEq + defaultlingvo + cEq + cGT + sLineBreak +
+      whMacrosTagBegin + cGT + sLineBreak +
+      macrocontent + sLineBreak +
+      whMacrosTagEnd + sLineBreak +
+      whTekoEnd;
+
+    ms.SetSize(Length(S));
+    ms.Seek(0, soFromBeginning);
+    ms.Write(S[1], Length(S));
+
+    WebRead.ReadChunksFromStream(pWebApp, ms,
+      'Expression #' + FormatDateTime('yyyymmddhhnnss', Now),
+       'Temp', '',
+       pWebApp.PageGeneration, pWebApp.Syntax0100,
+       pWebApp.Tekeros, pWebApp.Pages, pWebApp.Macros,
+       pWebApp.Debug.PageErrors, True, pWebApp.ProjectSyntax);
+  finally
+    FreeAndNil(ms);
+  end;
+end;
+
+procedure TDMContent.AddPage(const defaultlingvo, pageid, attributes,
+  pagecontent: String);
+var
+  ms: TMemoryStream;
+  S: String;
+const
+  cEq = '=';
+begin
+  ms := nil;
+  try
+    ms := TMemoryStream.Create;
+
+    S := whTekoBegin + ' ' + clingvo + cEq + defaultlingvo + cEq + cGT +
+      sLineBreak +
+      whFolioBegin + ' ' + cpageid + cEq + '"' + pageid + '" ' +
+      attributes + cGT + sLineBreak +
+      pagecontent + sLineBreak +
+      whFolioEnd + sLineBreak +
+      whTekoEnd;
+
+    ms.SetSize(Length(S));
+    ms.Seek(0, soFromBeginning);
+    ms.Write(S[1], Length(S));
+
+    WebRead.ReadChunksFromStream(pWebApp, ms,
+      'Expression #' + FormatDateTime('yyyymmddhhnnss', Now), 'Temp', '',
+       pWebApp.PageGeneration, pWebApp.Syntax0100,
+       pWebApp.Tekeros, pWebApp.Pages, pWebApp.Macros,
+       pWebApp.Debug.PageErrors, True, pWebApp.ProjectSyntax);
+  finally
+    FreeAndNil(ms);
+  end;
+end;
 
 procedure TDMContent.DataModuleCreate(Sender: TObject);
 begin
@@ -71,6 +157,22 @@ begin
 
     if Assigned(pWebApp) and pWebApp.IsUpdated then
     begin
+
+      if pWebApp is TwhApplication then
+      with TwhApplication(pWebApp) do
+      begin
+        OnSendBufferedString := SendBufferedStringFromDB;
+      end;
+
+      if pWebApp is TwhbdeApplication then
+      with TwhbdeApplication(pWebApp) do
+      begin
+        DynContentDataSource := DMContent.DataSource1;  // hook up the database
+        IDFieldName := cIDField;
+        DynContentFieldName := cTextField;
+        CacheDbContent := True;  {enable some caching}
+      end;
+
       ClientDataSet1.FileName := getHtDemoDataRoot + 'whLoadFromDB\' +
         'whContent.xml';
       try
@@ -85,8 +187,7 @@ begin
 
       if ErrorText = '' then
       begin
-        // helpful to know that WebAppUpdate will be called whenever the
-        // WebHub app is refreshed.
+        LoadWebHubTekerosFromDB;
         AddAppUpdateHandler(WebAppUpdate);
         FlagInitDone := True;
       end;
@@ -97,12 +198,188 @@ begin
   CodeSite.ExitMethod(Self, cFn);{$ENDIF}
 end;
 
+function TDMContent.LoadWebHubTekeroFromDB: Boolean;
+const cFn = 'LoadWebHubTekeroFromDB';
+var
+  aPageID: string;
+  aPageContent: string;
+  aPageDesc: string;
+  aTypeField: string;
+begin
+  {$IFDEF CodeSite}CodeSite.EnterMethod(Self, cFn);{$ENDIF}
+  Result := True;
+
+  {assume that we are positioned on the database record to load}
+  with ClientDataSet1 do
+  begin
+    aTypeField := FieldByName(cTypeField).asString;
+   // CSSend('aTypeField', aTypeField);
+    if (aTypeField = whMacrosTag) then
+    begin
+      AddMacros('eng', FieldByName(cTextField).AsString);
+    end
+    else
+    if (aTypeField = whFolio) then  // whFolio means whPage
+    begin
+      aPageID := FieldByName(cIDField).asString;
+      CSSend('aPageID', aPageID);
+      if FieldByName(cWhenToLoadField).asString = 'Preload' then
+      begin
+        aPageContent := FieldByName(cTextField).AsString;
+        AddPage('eng', aPageID, FieldByName(cAttributesField).asString,
+          aPageContent);
+      end
+      else
+      begin
+        aPageDesc := Copy(FieldByName(cAttributesField).asString, 7,
+          Length(FieldByName(cAttributesField).asString) - 7);
+        // add to the app's pages list.
+        pWebApp.Pages.Add(aPageID+'='+aPageID+',,,' + aPageDesc);
+      end;
+    end
+    else
+    begin
+      CSSendNote(Format('Not preloading "%s" of type %s.',
+        [FieldByName(cIDField).AsString, aTypeField]));
+      Result := False;
+    end;
+  end;
+  {$IFDEF CodeSite}CodeSite.ExitMethod(Self, cFn);{$ENDIF}
+end;
+
+function TDMContent.LoadWebHubTekerosFromDB: Boolean;
+const cFn = 'LoadWebHubTekerosFromDB';
+begin
+  {$IFDEF CodeSite}CodeSite.EnterMethod(Self, cFn);{$ENDIF}
+  Result := True;
+
+  (* excellent tutorial about TClientDataSet:
+  http://edn.embarcadero.com/article/29122
+  *)
+  if NOT ClientDataSet1.Active then
+  begin
+    try
+      DMContent.DataSource1.DataSet.Open;
+      // for some reason (bug) the TClientDataSet can have ReadOnly False
+      // yet need to have that property SET again to False.
+      // Confirmed in Delphi XE3 2-Feb-2013
+      if DMContent.ClientDataSet1.ReadOnly then
+      begin
+        // does not fire
+        //MsgWarningOk('ClientDataSet1.ReadOnly ... reversing');
+        DMContent.ClientDataSet1.ReadOnly := False;
+      end;
+      if NOT DMContent.ClientDataSet1.CanModify then
+      begin
+        //MsgWarningOk(DMContent.ClientDataSet1.Name + sLineBreak +
+        //  'CanModify = False' + sLineBreak + sLineBreak +
+        //  'Reversing so that you can do data entry now.');
+        DMContent.ClientDataSet1.ReadOnly := False;
+      end;
+    except
+      on E: Exception do
+      begin
+        LogSendException(E, cFn);
+        Result := False;
+      end;
+    end;
+  end;
+
+  if Result then
+  begin
+    ClientDataSet1.First;
+    while not ClientDataSet1.EOF do
+    begin
+      DMContent.LoadWebHubTekeroFromDB;
+      ClientDataSet1.Next;
+    end;
+  end;
+  WebMessage('');
+  {$IFDEF CodeSite}CodeSite.ExitMethod(Self, cFn);{$ENDIF}
+end;
+
+procedure TDMContent.SendBufferedStringFromDB(const Value: string;
+  var Handled: Boolean);
+{$IFDEF CodeSite}const cFn = 'SendBufferedStringFromDB';{$ENDIF}
+var
+  a1: String;
+  aWhenToLoad: String;
+  t: TwhList;
+begin
+{$IFDEF CodeSite}CodeSite.EnterMethod(Self, cFn);{$ENDIF}
+
+  {Warning! PageIDs will always be in UPPERCASE so the database should put
+   their identifiers in UPPERCASE as well. Droplets, on the other hand, may be
+   in lowercase or mixed case.  v2.037}
+
+  {Note! Implementing this event effectively bypasses the logic in the
+   SendBufferedString method in htbdeWApp.pas, which works only with TTable
+   objects and caches based purely on the CacheDbContent boolean property. The
+   approach shown here is much more flexible.}
+
+  with TwhbdeApplication(pWebApp) do
+  begin
+    {if we already know that this Value cannot be found in our database}
+    if (NOT CacheDbContent) or (pos('|'+uppercase(Value)+'|',CacheDbFail)=0)
+    then
+    begin
+      if NOT DMContent.DataSource1.DataSet.Active then
+      begin
+        CSSendWarning(DMContent.DataSource1.DataSet.Name + ' not active yet');
+        DMContent.DataSource1.DataSet.Open;
+      end;
+
+      {locate based on Value}
+      if DMContent.DataSource1.DataSet.Locate(IDFieldName, VarArrayOf([Value]),
+        [loCaseInsensitive]) then
+      begin
+        {we found our match; grab the page/droplet content now}
+        with DMContent.DataSource1.DataSet do
+        begin
+          a1 := FieldByName(cTextField).asString;
+          CSSend('FOUND a1', a1);
+          aWhenToLoad := FieldByname(cWhenToLoadField).asString;
+          CSSend('aWhenToLoad', aWhenToLoad);
+        end;
+
+        {output the content now, potentially recursively calling here}
+        // SendString expands any macros contained within a1
+        // SendStringImm does not.
+        SendString(a1);
+
+        {signal that we have handled this Value}
+        Handled := True;
+
+        {consider caching the content for next time}
+        if aWhenToLoad <> 'NoCache' then
+        begin
+          {Load the page or droplet content into memory. Both page and droplet
+           content are stored in the pWebApp.Droplets array.}
+          t := TwhList.create;
+          t.text := a1;
+          Tekeros.AddObject(Value + '=' + Value, t);
+          {Reminder: do NOT free t. WebHub will free it when appropriate.}
+        end;
+      end
+      else
+      begin
+        {add Value to the list so we don't search for it next time around.}
+        CSSendWarning('Not located: ' + Value);
+        CacheDbFail := CacheDbFail+uppercase(Value)+'|';
+        CSSend('CacheDbFail', CacheDbFail);
+        CacheDbFailCount := CacheDbFailCount + 1;
+        CSSend('CacheDbFailCount', S(CacheDbFailCount));
+      end;
+    end;
+  end;
+{$IFDEF CodeSite}CodeSite.ExitMethod(Self, cFn);{$ENDIF}
+end;
+
 procedure TDMContent.WebAppUpdate(Sender: TObject);
 const cFn = 'WebAppUpdate';
 begin
   {$IFDEF CodeSite}CodeSite.EnterMethod(Self, cFn);{$ENDIF}
-  // reserved for when the WebHub application object refreshes
-  // e.g. to make adjustments because the config changed.
+  LoadWebHubTekerosFromDB;
   {$IFDEF CodeSite}CodeSite.ExitMethod(Self, cFn);{$ENDIF}
 end;
 
--- webhubdemos/Source/WHApps/DB Examples/whLoadFromDB/whLoadFromDB_fmWhAppDBHTML.pas
+++ webhubdemos/Source/WHApps/DB Examples/whLoadFromDB/whLoadFromDB_fmWhAppDBHTML.pas
@@ -58,10 +58,10 @@ Notes updated 2-Feb-2013
 interface
 
 uses
-  Windows, Messages, SysUtils, Classes, Graphics, Controls, Forms, Dialogs,
+  Windows, Messages, SysUtils, Classes, Controls, Forms,
   DBCtrls, Grids, DBGrids, DB, ExtCtrls, StdCtrls, Buttons,
-  utPanFrm, updateOk, tpAction,
-  toolbar, {}tpCompPanel, restorer;
+  utPanFrm, updateOk, tpAction, toolbar, tpCompPanel,
+  webCall, webSend;
 
 type
   TfmAppDBHTML = class(TutParentForm)
@@ -71,7 +71,6 @@ type
     tpToolBar2: TtpToolBar;
     btnPostOnePage: TButton;
     DBMemo1: TDBMemo;
-    btnRefresh: TtpToolButton;
     Splitter1: TSplitter;
     DBGrid1: TDBGrid;
     DBNavigator1: TDBNavigator;
@@ -79,20 +78,15 @@ type
     BtnLoad: TtpToolButton;
     procedure BtnLoadClick(Sender: TObject);
     procedure btnPostOnePageClick(Sender: TObject);
-    procedure btnRefreshClick(Sender: TObject);
     procedure Table1AfterPost(DataSet: TDataSet);
   private
     { Private declarations }
-    procedure AddPage(const defaultlingvo: String;
-      const pageid: String; const attributes: String; const pagecontent: String);
-    procedure AddMacros(const defaultlingvo: String;
-      const macrocontent: String);
-    procedure DatabaseToWebHubStructure;
   public
     { Public declarations }
     function Init: Boolean; override;
-    procedure SendBufferedStringFromDB(const Value: string;
-      var Handled: Boolean);
+    procedure NoteBadPageID(Sender: TwhConnection;
+      const ADesiredPageID: string; var bContinue: Boolean);
+    function RestorerActiveHere: Boolean; override;
     end;
 
 var
@@ -103,126 +97,65 @@ implementation
 {$R *.DFM}
 
 uses
+  {$IFDEF CodeSite}CodeSiteLogging,{$ENDIF}
   Variants,
-  webApp, htbdeWApp, webInfou, webPHub, whdemo_ViewSource, webRead, htmConst,
-  webList, webSplat,
-  ucDlgs, whLoadFromDB_dmwhData;
+  ucDlgs, ucCodeSiteInterface,
+  webApp, whLoadFromDB_dmwhData;
 
 //------------------------------------------------------------------------------
-
-const
-  cIDField = 'Identifier';
-  cTextField = 'Text';
-  cTypeField = 'Type';
-  cPageIdentifier = whFolio;
-  cWhenToLoadField = 'WhenToLoad';
-  cAttributesField = 'Attributes';
 
 function TfmAppDBHTML.Init: Boolean;
 begin
   Result:= inherited Init;
   if Result then
   begin
-    with TwhbdeApplication(pWebApp) do
-    begin
-      OnSendBufferedString := SendBufferedStringFromDB;
-      DynContentDataSource := DMContent.DataSource1;  // hook up the database
-      IDFieldName := cIDField;
-      DynContentFieldName := cTextField;
-      CacheDbContent := True;  {enable some caching}
-    end;
-    BtnLoadClick(nil);  // automatically load the database
-    DMContent.ClientDataSet1.AfterPost := Table1AfterPost;
+    pConnection.OnBadPageID := NoteBadPageID;
+    (* Vital! Do not have the DB controls active while surfers are causing the
+       underlying TClientDataSet to change records, etc. That causes
+       synchronization errors with the main thread and the EXE will hang. *)
+    DBGrid1.Enabled := False;
+    DBGrid1.DataSource := nil;
+    DBMemo1.DataSource := nil;
+    DBNavigator1.DataSource := nil;
   end;
+end;
+
+function TfmAppDBHTML.RestorerActiveHere: Boolean;
+begin
+  Result := False;
 end;
 
 //------------------------------------------------------------------------------
 
-procedure TfmAppDBHTML.DatabaseToWebHubStructure;
-var
-  aPageID: string;
-  aPageContent: string;
-  aPageDesc: string;
-  aTypeField: string;
-begin
-  {assume that we are positioned on the database record to load}
-  with DMContent.DataSource1.DataSet do
-  begin
-    aTypeField := FieldByName(cTypeField).asString;
-    if (aTypeField = whMacrosTag) then
-    begin
-      AddMacros('eng', FieldByName(cTextField).AsString);
-    end
-    else
-    if (aTypeField = whFolio) then  // whFolio means whPage
-    begin
-      aPageID := FieldByName(cIDField).asString;
-      if FieldByName(cWhenToLoadField).asString = 'Preload' then
-      begin
-        aPageContent := FieldByName(cTextField).AsString;
-        AddPage('eng', aPageID, FieldByName(cAttributesField).asString,
-          aPageContent);
-      end
-      else
-      begin
-        aPageDesc := Copy(FieldByName(cAttributesField).asString, 7,
-          Length(FieldByName(cAttributesField).asString) - 7);
-        // add to the app's pages list.
-        pWebApp.Pages.Add(aPageID+'='+aPageID+',,,' + aPageDesc);
-      end;
-    end;
-  end;
-end;
-
 procedure TfmAppDBHTML.BtnLoadClick(Sender: TObject);
+const cFn = 'BtnLoadClick';
 begin
+  {$IFDEF CodeSite}CodeSite.EnterMethod(Self, cFn);{$ENDIF}
   inherited;
-  (* excellent tutorial about TClientDataSet:
-  http://edn.embarcadero.com/article/29122
-  *)
-
-  if DMContent.DataSource1.DataSet.Active then
+  if Assigned(DBGrid1.DataSource) then
   begin
-    DMContent.DataSource1.DataSet.Close;
+    DBGrid1.DataSource := nil;
+    DBMemo1.DataSource := nil;
+    DBNavigator1.DataSource := nil;
+    DMContent.ClientDataSet1.AfterPost := nil;
   end
   else
   begin
-    try
-      DMContent.DataSource1.DataSet.Open;
-      // for some reason (bug) the TClientDataSet can have ReadOnly False
-      // yet need to have that property SET again to False.
-      // Confirmed in Delphi XE3 2-Feb-2013
-      if DMContent.ClientDataSet1.ReadOnly then
-      begin
-        // does not fire
-        MsgWarningOk('ClientDataSet1.ReadOnly ... reversing');
-        DMContent.ClientDataSet1.ReadOnly := False;
-      end;
-      if NOT DMContent.ClientDataSet1.CanModify then
-      begin
-        MsgWarningOk(DMContent.ClientDataSet1.Name + sLineBreak +
-          'CanModify = False' + sLineBreak + sLineBreak +
-          'Reversing so that you can do data entry now.');
-        DMContent.ClientDataSet1.ReadOnly := False;
-      end;
-    except
-      on E: Exception do
-      begin
-        MsgErrorOk(e.Message);
-      end;
-    end;
-
-    if DMContent.DataSource1.DataSet.Active then
+    MsgWarningOk(
+      'Please do not allow surfers to use the app while you are editing');
+    DBGrid1.Enabled := True;
+    DBGrid1.DataSource := DMContent.DataSource1;
+    DBMemo1.Enabled := True;
+    DBMemo1.DataSource := DMContent.DataSource1;
+    DBNavigator1.Enabled := True;
+    DBNavigator1.DataSource := DMContent.DataSource1;
+    DMContent.ClientDataSet1.AfterPost := Table1AfterPost;
+    if NOT DMContent.DataSource1.DataSet.Active then
     begin
-      DMContent.DataSource1.DataSet.First;
-      while not DMContent.DataSource1.DataSet.EOF do
-      begin
-        DatabaseToWebHubStructure;
-        DMContent.DataSource1.DataSet.Next;
-      end;
+      DMContent.DataSource1.DataSet.Open;
     end;
   end;
-  WebMessage('');
+  {$IFDEF CodeSite}CodeSite.ExitMethod(Self, cFn);{$ENDIF}
 end;
 
 // -----------------------------------------------------------------------------
@@ -235,177 +168,47 @@ end;
 
 // -----------------------------------------------------------------------------
 
-procedure TfmAppDBHTML.AddPage(const defaultlingvo: String;
-  const pageid: String; const attributes: String; const pagecontent: String);
-var
-  ms: TMemoryStream;
-  S: String;
-const
-  cEq = '=';
-begin
-  ms := nil;
-  try
-    ms := TMemoryStream.Create;
-
-    S := whTekoBegin + ' ' + clingvo + cEq + defaultlingvo + cEq + cGT +
-      sLineBreak +
-      whFolioBegin + ' ' + cpageid + cEq + '"' + pageid + '" ' +
-      attributes + cGT + sLineBreak +
-      pagecontent + sLineBreak +
-      whFolioEnd + sLineBreak +
-      whTekoEnd;
-
-    ms.SetSize(Length(S));
-    ms.Seek(0, soFromBeginning);
-    ms.Write(S[1], Length(S));
-
-    WebRead.ReadChunksFromStream(pWebApp, ms,
-      'Expression #' + IntToStr(GetTickCount), 'Temp', '',
-       pWebApp.PageGeneration, pWebApp.Syntax0100,
-       pWebApp.Tekeros, pWebApp.Pages, pWebApp.Macros,
-       pWebApp.Debug.PageErrors, True, pWebApp.ProjectSyntax);
-  finally
-    FreeAndNil(ms);
-  end;
-end;
-
-procedure TfmAppDBHTML.AddMacros(const defaultlingvo: String;
-  const macrocontent: String);
-var
-  ms: TMemoryStream;
-  S: String;
-const
-  cEq = '=';
-begin
-  ms := nil;
-  try
-    ms := TMemoryStream.Create;
-
-    S := whTekoBegin + ' ' + clingvo + cEq + defaultlingvo + cEq + cGT + sLineBreak +
-      whMacrosTagBegin + cGT + sLineBreak +
-      macrocontent + sLineBreak +
-      whMacrosTagEnd + sLineBreak +
-      whTekoEnd;
-
-    ms.SetSize(Length(S));
-    ms.Seek(0, soFromBeginning);
-    ms.Write(S[1], Length(S));
-
-    WebRead.ReadChunksFromStream(pWebApp, ms,
-      'Expression #' + IntToStr(GetTickCount), 'Temp', '',
-       pWebApp.PageGeneration, pWebApp.Syntax0100,
-       pWebApp.Tekeros, pWebApp.Pages, pWebApp.Macros,
-       pWebApp.Debug.PageErrors, True, pWebApp.ProjectSyntax);
-  finally
-    FreeAndNil(ms);
-  end;
-end;
-
 procedure TfmAppDBHTML.btnPostOnePageClick(Sender: TObject);
 var
-  aTekeroID: String;  // a part of a teko file (page or droplet)
-  aWhenToLoad: String;
+  aTekeroID: string;  // a part of a teko file (page or droplet)
+  aWhenToLoad: string;
 begin
   inherited;
-  if DMContent.DataSource1.DataSet.Active then
-  begin
-    DMContent.DataSource1.DataSet.Close;
-  end
-  else
-  begin
+  if NOT DMContent.DataSource1.DataSet.Active then
     DMContent.DataSource1.DataSet.Open;
 
-    aTekeroID := EditPageID.text;
-    with DMContent.DataSource1.DataSet do
+  { TO-DO: test this in isolateion }
+
+  aTekeroID := EditPageID.text;
+  with DMContent.DataSource1.DataSet do
+  begin
+    if Locate(cIDField, aTekeroID, [loCaseInsensitive]) then
     begin
-      if Locate(cIDField, aTekeroID, []) then
+      DMContent.LoadWebHubTekeroFromDB;
+      aWhenToLoad := FieldByName(cWhenToLoadField).asString;
+      if (aWhenToLoad <> 'NoCache') and (aWhenToLoad <> 'n/a') then
       begin
-        DatabaseToWebHubStructure;
-        aWhenToLoad := FieldByName(cWhenToLoadField).asString;
-        if (aWhenToLoad <> 'NoCache') and (aWhenToLoad <> 'n/a') then
-        begin
-          with pWebApp do
-            if (Tekero[aTekeroID]<>'') or // already cached
-               (FieldByName(cWhenToLoadField).asString = 'PreLoad') then
-            begin
-               Tekero[aTekeroID]:=FieldByName(cTextField).asString;
-            end;
-        end;
+        with pWebApp do
+          if (Tekero[aTekeroID]<>'') or // already cached
+             (FieldByName(cWhenToLoadField).asString = 'PreLoad') then
+          begin
+             Tekero[aTekeroID]:=FieldByName(cTextField).asString;
+          end;
       end;
     end;
   end;
+
 end;
 
-procedure TfmAppDBHTML.btnRefreshClick(Sender: TObject);
+procedure TfmAppDBHTML.NoteBadPageID(Sender: TwhConnection;
+  const ADesiredPageID: string; var bContinue: Boolean);
+const cFn = 'NoteBadPageID';
 begin
-  inherited;
-  with pWebApp do
-  begin
-    Refresh;  // reload whatever might come from INI or HTML
-    BtnLoadClick(Sender);
-  end;
-end;
-
-procedure TfmAppDBHTML.SendBufferedStringFromDB(const Value: string;
-  var Handled: Boolean);
-var
-  a1: String;
-  aWhenToLoad: String;
-  t: TwhList;
-begin
-
-  {Warning! PageIDs will always be in UPPERCASE so the database should put
-   their identifiers in UPPERCASE as well. Droplets, on the other hand, may be
-   in lowercase or mixed case.  v2.037}
-
-  {Note! Implementing this event effectively bypasses the logic in the
-   SendBufferedString method in htbdeWApp.pas, which works only with TTable
-   objects and caches based purely on the CacheDbContent boolean property. The
-   approach shown here is much more flexible.}
-
-  with TwhbdeApplication(pWebApp) do
-  begin
-    {if we already know that this Value cannot be found in our database}
-    if (NOT CacheDbContent) or (pos('|'+uppercase(Value)+'|',CacheDbFail)=0)
-    then
-    begin
-
-      {locate based on Value}
-      if DMContent.DataSource1.DataSet.Locate(IDFieldName, VarArrayOf([Value]), []) then
-      begin
-        {we found our match; grab the page/droplet content now}
-        with DMContent.DataSource1.DataSet do
-        begin
-          a1 := FieldByName(cTextField).asString;
-          aWhenToLoad := FieldByname(cWhenToLoadField).asString;
-        end;
-
-        {output the content now}
-        {Response.SendPChar(pchar(a1)); use SendPChar to ensure zero further expansion}
-        SendString(a1);         // SendString expands any macros contained within a1
-
-        {signal that we have handled this Value}
-        Handled := True;
-
-        {consider caching the content for next time}
-        if aWhenToLoad <> 'NoCache' then
-        begin
-          {Load the page or droplet content into memory. Both page and droplet
-           content are stored in the pWebApp.Droplets array.}
-          t:=TwhList.create;
-          t.text:=a1;
-          Tekeros.AddObject(Value+'='+Value,t);
-          {Reminder: do NOT free t. WebHub will free it when appropriate.}
-        end;
-      end
-      else
-      begin
-        {add Value to the list so we don't search for it next time around.}
-        CacheDbFail:=CacheDbFail+uppercase(Value)+'|';
-        CacheDbFailCount := CacheDbFailCount + 1;
-      end;
-    end;
-  end;
+{$IFDEF CodeSite}CodeSite.EnterMethod(Self, cFn);{$ENDIF}
+  // demonstrate how to catch an invalid PageID request.
+  LogSendWarning('ADesiredPageID = ' + ADesiredPageID, cFn);
+  bContinue := True;  // logged but not handled
+{$IFDEF CodeSite}CodeSite.ExitMethod(Self, cFn);{$ENDIF}
 end;
 
 end.
