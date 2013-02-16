6eecc275eaa4335bc12eb0bf612f36a2f90192cd
annlynnworth <annlynnworth@88ca0d87-9d1d-4ddd-a6d8-5c97b49fb611>
2013-02-16 00:21:34 +0000

    waMakeBadVars

--- webhubdemos/Source/WHApps/Lite Examples/whAppServer/dserver/dserver_whdmGeneral.pas
+++ webhubdemos/Source/WHApps/Lite Examples/whAppServer/dserver/dserver_whdmGeneral.pas
@@ -3,10 +3,12 @@ unit dserver_whdmGeneral;
 interface
 
 uses
-  SysUtils, Classes;
+  SysUtils, Classes, updateOK, tpAction, webTypes, webLink;
 
 type
   TdmwhGeneral = class(TDataModule)
+    waMakeBadVars: TwhWebAction;
+    procedure waMakeBadVarsExecute(Sender: TObject);
   private
     { Private declarations }
   public
@@ -24,6 +26,7 @@ implementation
 {$R *.dfm}
 
 uses
+  Math,
   webApp, webCall, htmConst, htWebApp,
   ucString, ucPos;
 
@@ -40,6 +43,42 @@ procedure TdmwhGeneral.LoadProjectSyntax(Sender: TObject);
 begin
   with TwhAppBase(Sender) do
     ProjectSyntax := cSyntaxStage;  {19-May-2004}
+end;
+
+procedure TdmwhGeneral.waMakeBadVarsExecute(Sender: TObject);
+var
+  dd: Double;
+  x: Integer;
+  s1: string;
+begin
+  if pWebApp.Session.StringVars.Count < 20 then
+  begin
+    x := Random(999);
+    dd := 1000 * 1000 / x;
+    pWebApp.StringVar['data' + IntToStr(x)] := FloatToStr(dd);
+  end;
+  s1 := pWebApp.StringVar['inVarName'];
+  if s1 = '' then
+  begin
+    pWebApp.StringVar['shtml'] := '<table>' + #10 + '<tr><td>abc</td></tr>' +
+      #10 + '</table>';
+    pWebApp.StringVar['withcrlf'] := '<table>' + sLineBreak +
+      '<tr><td>def</td></tr>' +
+      sLineBreak + '</table>';
+    pWebApp.StringVar['hexer'] := '1.0000' +
+      #12 +  // $0C            }
+      #251 + // $FB            } invalid string - becomes 0C C3 BB 31 20 20
+      #49 +  // $29            }
+      #32#32; // $20 $20;      }
+  end
+  else
+  begin
+    pWebApp.SendStringImm('<p>The value of StringVar[<b>' + s1 + '</b>] is ' +
+      pWebApp.StringVar[s1] + '.</p>');
+  end;
+  pWebApp.SendStringImm('<p>Now there are ' +
+    IntToStr(pWebApp.Session.StringVars.Count) + ' StringVars.</p>');
+
 end;
 
 procedure TdmwhGeneral.LoadProjectLingvo(Sender: TObject);
