1a5ef927410750f54fd4c02519b9ac3bff360033
annlynnworth <annlynnworth@88ca0d87-9d1d-4ddd-a6d8-5c97b49fb611>
2013-03-17 03:43:31 +0000

    alternate must be in separate folder to avoid confusing the Delphi compiler

--- trunk/webhubdemos/Source/StandaloneDemos/TtpSharedData/alternate/CodeSiteLogging.pas
+++ trunk/webhubdemos/Source/StandaloneDemos/TtpSharedData/alternate/CodeSiteLogging.pas
@@ -40,6 +40,27 @@ const
   csmGreen: Integer = 1;
   csmYellow: Integer = 1;
 
+function InitCodeSiteTransfer: Boolean;
+
+type
+  TCodeSiteDestination = class;
+
+  TCodeSiteLogFile = class
+  public
+    FilePath: string;
+    FileName: string;
+    Active: Boolean;
+  end;
+
+  TCodeSiteDestination = class
+  strict private
+    FLogFIle: TCodeSiteLogFile;
+  public
+    property LogFile: TCodeSiteLogFile read FLogFile write FLogFile;
+    constructor Create( AOwner: TComponent );
+    destructor Destroy; override;
+  end;
+
 type
   TDestinationRec = class
   public
@@ -52,10 +73,13 @@ type
   private
     FCategory: string;
     FCategoryFontColor: Integer;
+    FDestination: TCodeSiteDestination;
     FUseCodeSiteManagerConnection: Boolean;
     {$IFDEF XFER2CodeSite}
     FSharedBuf: TtpSharedBuf;
     {$ENDIF}
+    procedure ClearBuf;
+    procedure SetDestination(Value: TCodeSiteDestination);
   public
     constructor Create;
     destructor Destroy; override;
@@ -78,6 +102,11 @@ type
     property Category: string read FCategory write FCategory;
     property CategoryFontColor: Integer read FCategoryFontColor
       write FCategoryFontColor;
+    property Destination: TCodeSiteDestination read FDestination write
+      SetDestination;
+    {$IFDEF XFER2CodeSite}
+    property SharedBuf: TtpSharedBuf read FSharedBuf;
+    {$ENDIF}
     procedure UseCodeSiteManagerConnection;
   end;
 
@@ -85,12 +114,15 @@ type
   TCodeSiteManagerFake = class
   private
     FDefaultDestination: TDestinationRec;
+    FEnabled: Boolean;
+    procedure SetEnabled(Value: Boolean);
   public
     constructor Create;
     destructor Destroy; override;
     procedure ConnectUsingTcp; overload;
     procedure ConnectUsingTcp(const a: string; const i: Integer); overload;
     property DefaultDestination: TDestinationRec read FDefaultDestination;
+    property Enabled: Boolean read FEnabled write SetEnabled;
   end;
 
 var
@@ -117,7 +149,8 @@ end;
 
 function ABC(const a: Integer; const b, c: string): AnsiString;
 begin
-  Result := AnsiString(UTF8Encode(IntToStr(a) + '^^' + b + '^^' + c));
+  Result := AnsiString(UTF8Encode(Copy(IntToStr(a) + '^^' + b + '^^' + c, 1,
+    2048)));
 end;
 
 procedure TCodeSiteFake.EnterMethod(c: TObject; const S: string);
@@ -125,6 +158,7 @@ begin
   if Assigned(c) and (c is TComponent) then
   begin
     {$IFDEF XFER2CodeSite}
+    ClearBuf;
     FSharedBuf.GlobalAnsiString := ABC(6, TComponent(c).ClassName + '.' +
       TComponent(c).Name, S);
     {$ELSE}
@@ -134,6 +168,7 @@ begin
   else
   begin
     {$IFDEF XFER2CodeSite}
+    ClearBuf;
     FSharedBuf.GlobalAnsiString := ABC(6, S, '');
     {$ELSE}
     HREFTestLog('>>', '', S);
@@ -144,6 +179,7 @@ end;
 procedure TCodeSiteFake.EnterMethod(const S: string);
 begin
   {$IFDEF XFER2CodeSite}
+  ClearBuf;
   FSharedBuf.GlobalAnsiString := ABC(6, S, '');
   {$ELSE}
   HREFTestLog('>>', '', S);
@@ -155,6 +191,7 @@ begin
   if Assigned(c) and (c is TComponent) then
   begin
     {$IFDEF XFER2CodeSite}
+    ClearBuf;
     FSharedBuf.GlobalAnsiString := ABC(7, TComponent(c).ClassName + '.' +
       TComponent(c).Name, S);
     {$ELSE}
@@ -164,11 +201,19 @@ begin
   else
   begin
     {$IFDEF XFER2CodeSite}
+    ClearBuf;
     FSharedBuf.GlobalAnsiString := ABC(7, S, '');
     {$ELSE}
     HREFTestLog('<<', '', S);
     {$ENDIF}
   end;
+end;
+
+procedure TCodeSiteFake.ClearBuf;
+begin
+  {$IFDEF XFER2CodeSite}
+  FSharedBuf.GlobalAnsiString := '';
+  {$ENDIF}
 end;
 
 constructor TCodeSiteFake.Create;
@@ -197,6 +242,7 @@ end;
 procedure TCodeSiteFake.ExitMethod(const S: string);
 begin
   {$IFDEF XFER2CodeSite}
+  ClearBuf;
   FSharedBuf.GlobalAnsiString := ABC(7, S, '');
   {$ELSE}
   HREFTestLog('<<', '', S);
@@ -206,7 +252,8 @@ end;
 procedure TCodeSiteFake.Send(const a1: string);
 begin
   {$IFDEF XFER2CodeSite}
-  FSharedBuf.GlobalAnsiString := ABC(7, a1, '');
+  ClearBuf;
+  FSharedBuf.GlobalAnsiString := ABC(1, a1, '');
   {$ELSE}
   HREFTestLog(1, a1, '');
   {$ENDIF}
@@ -215,7 +262,8 @@ end;
 procedure TCodeSiteFake.Send(const a1: string; const i2: Integer);
 begin
   {$IFDEF XFER2CodeSite}
-  FSharedBuf.GlobalAnsiString := ABC(7, a1, IntToStr(i2));
+  ClearBuf;
+  FSharedBuf.GlobalAnsiString := ABC(1, a1, IntToStr(i2));
   {$ELSE}
   HREFTestLog('info', a1, IntToStr(i2));
   {$ENDIF}
@@ -229,7 +277,8 @@ end;
 procedure TCodeSiteFake.Send(const a1, a2: string);
 begin
   {$IFDEF XFER2CodeSite}
-  FSharedBuf.GlobalAnsiString := ABC(7, a1, a2);
+  ClearBuf;
+  FSharedBuf.GlobalAnsiString := ABC(1, a1, a2);
   {$ELSE}
   HREFTestLog('info', a1, a2);
   {$ENDIF}
@@ -238,6 +287,7 @@ end;
 procedure TCodeSiteFake.SendError(const S: string);
 begin
   {$IFDEF XFER2CodeSite}
+  ClearBuf;
   FSharedBuf.GlobalAnsiString := ABC(3, S, '');
   {$ELSE}
   HREFTestLog('error', S, '');
@@ -247,6 +297,7 @@ end;
 procedure TCodeSiteFake.SendException(E: Exception);
 begin
   {$IFDEF XFER2CodeSite}
+  ClearBuf;
   FSharedBuf.GlobalAnsiString := ABC(5, E.Message, '');
   {$ELSE}
   HREFTestLog('exception', E.Message, '');
@@ -256,6 +307,7 @@ end;
 procedure TCodeSiteFake.SendNote(const S: string);
 begin
   {$IFDEF XFER2CodeSite}
+  ClearBuf;
   FSharedBuf.GlobalAnsiString := ABC(4, S, '');
   {$ELSE}
   HREFTestLog('note', S, '');
@@ -265,10 +317,31 @@ end;
 procedure TCodeSiteFake.SendWarning(const S: string);
 begin
   {$IFDEF XFER2CodeSite}
+  ClearBuf;
   FSharedBuf.GlobalAnsiString := ABC(2, S, '');
   {$ELSE}
   HREFTestLog('warning', S, '');
   {$ENDIF}
+end;
+
+procedure TCodeSiteFake.SetDestination(Value: TCodeSiteDestination);
+begin
+  if Self <> nil then
+  begin
+    FDestination := Value;
+    {$IFDEF XFER2CodeSite}
+    ClearBuf;
+    if (Value <> nil) and (Value.LogFile <> nil) then
+    begin
+      with Value do
+      begin
+        if (LogFile.FilePath <> '') and (LogFile.FileName <> '') then
+          FSharedBuf.GlobalAnsiString := ABC(8, LogFile.FilePath,
+            LogFile.FileName);
+      end;
+    end;
+    {$ENDIF}
+  end;
 end;
 
 procedure TCodeSiteFake.UseCodeSiteManagerConnection;
@@ -306,6 +379,19 @@ begin
   inherited;
 end;
 
+procedure TCodeSiteManagerFake.SetEnabled(Value: Boolean);
+begin
+  if Self <> nil then
+  begin
+    {$IFDEF XFER2CodeSite}
+    if Assigned(CodeSite) and Assigned(CodeSite.SharedBuf) then
+      CodeSite.ClearBuf;
+      CodeSite.SharedBuf.GlobalAnsiString := ABC(9, BoolToStr(Value, True), '');
+    {$ENDIF}
+    FEnabled := Value;
+  end;
+end;
+
 procedure TCodeSiteFake.Send(const a1: string; const b2: Boolean);
 begin
   Send(a1, BoolToStr(b2, True));
@@ -321,14 +407,35 @@ begin
   end;
 end;
 
-initialization
+{ TCodeSiteDestination }
 
-CodeSite := TCodeSiteFake.Create;
-CodeSiteManager := TCodeSiteManagerFake.Create;
+constructor TCodeSiteDestination.Create(AOwner: TComponent);
+begin
+  FLogFile := TCodeSiteLogFile.Create;
+end;
+
+destructor TCodeSiteDestination.Destroy;
+begin
+  FreeAndNil(FLogFile);
+  inherited;
+end;
+
+
+function InitCodeSiteTransfer: Boolean;
+begin
+  if CodeSite = nil then
+  begin
+    CodeSite := TCodeSiteFake.Create;
+    CodeSiteManager := TCodeSiteManagerFake.Create;
+  end;
+  Result := True;
+end;
+
+initialization
+  InitCodeSiteTransfer;
 
 finalization
-
-FreeAndNil(CodeSite);
-FreeAndNil(CodeSiteManager);
+  FreeAndNil(CodeSite);
+  FreeAndNil(CodeSiteManager);
 
 end.
