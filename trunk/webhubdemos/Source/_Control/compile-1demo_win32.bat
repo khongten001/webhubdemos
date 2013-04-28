367fcfa734475acef68e7a8ae77b31710bc8e9ad
annlynnworth <annlynnworth@88ca0d87-9d1d-4ddd-a6d8-5c97b49fb611>
2013-04-28 03:40:38 +0000

    need to be able to use both D17 and D18 until Embarcadero and CPS and Raize and NexusDB ship required files for D18

--- trunk/webhubdemos/Source/_Control/compile-1demo_win32.bat
+++ trunk/webhubdemos/Source/_Control/compile-1demo_win32.bat
@@ -1,29 +1,32 @@
 @echo off
-setlocal
+if "%compilerdigits%"=="" set compilerdigits=18
 
+setlocal
 del d:\temp\DelphiTempDCU\*.dcu
 
 :: use ZaphodsMap to find compiler
 :: zmset.bat and ZMLookup.exe are FREE from HREF Tools Corp. via www.zaphodsmap.com
-call %ZaphodsMap%zmset.bat d18 UsingKey2Folder "HREFTools\Production\cv001 Delphi D18"
-set dcc=%d18%bin\dcc32.exe
+call %ZaphodsMap%zmset.bat droot UsingKey2Folder "HREFTools\Production\cv001 Delphi D%compilerdigits%"
+set dcc=%droot%bin\dcc32.exe
 if not exist %dcc% pause
 
-set raizepath=K:\Vendors\Raize\CodeSite5\Lib\RS-XE4\Win32
+if "%compilerdigits%"=="18" set raizepath=K:\Vendors\Raize\CodeSite5\Lib\RS-XE4\Win32
+if "%compilerdigits%"=="17" set raizepath=K:\Vendors\Raize\CodeSite5\Lib\RS-XE3\Win32
+if "%compilerdigits%"=="16" set raizepath=K:\Vendors\Raize\CodeSite5\Lib\RS-XE2\Win32
 set ibopath=K:\Vendors\CPS\IBObjects\v5.x\source\common;K:\Vendors\CPS\IBObjects\v5.x\source\tdataset;K:\Vendors\CPS\IBObjects\v5.x\source\tools;K:\Vendors\CPS\IBObjects\v5.x\source\core;K:\Vendors\CPS\IBObjects\v5.x\source\access
-set libsearchpath="h:\;h:\dcu_d18_win32;h:\pkg_d18_win32;k:\webhub\lib\whplus\rubi;k:\Rubicon\source;%ibopath%;%d18%\lib\win32\release;D:\vcl\NexusDB3;"
+set libsearchpath="h:\;h:\dcu_d%compilerdigits%_win32;h:\pkg_d%compilerdigits%_win32;k:\webhub\lib\whplus\rubi;k:\Rubicon\source;%ibopath%;%droot%lib\win32\release;D:\vcl\NexusDB3;"
 set outputroot="d:\Projects\WebHubDemos\Live\WebHub\Apps"
 set pkg="vcl;vclx;vcldb;vcldbx;soaprtl;xmlrtl;inet;ldiRegExLib;ZaphodsMapLib;WebHub;WebHubDB"
 set compilerflags=PREVENTSVCMGR;use_IBO;USE_TIBODataset;INHOUSE
 set includepath=h:\;k:\Rubicon\source\inc;K:\Vendors\CPS\IBObjects\v5.x\source\common;
 
-:: extra parameters for Delphi XE2
+:: extra parameters for Delphi XE2+
 set dccflags=--no-config -M -Q -AGenerics.Collections=System.Generics.Collections;Generics.Defaults=System.Generics.Defaults;WinTypes=Windows;WinProcs=Windows;DbiTypes=BDE;DbiProcs=BDE;DbiErrs=BDE
 set dccns=-NSSystem;Xml;Data;Datasnap;Web;Soap;Winapi;System.Win;Data.Win;Datasnap.Win;Web.Win;Soap.Win;Xml.Win;Bde;Vcl;Vcl.Imaging;Vcl.Touch;Vcl.Samples;Vcl.Shell
 
 ren %1.cfg %1.off
 
-echo 1demo d18_win32 %1
+echo 1demo d%compilerdigits%_win32 %1
 :: pending CS 5 for XE4 del %outputroot%\%1.exe %1.raize.bin
 :: pending CS 5 for XE4 @echo on
 :: pending CS 5 for XE4 "%dcc%"  %1.dpr -w -h -b -nd:\temp\DelphiTempDCU -E%outputroot% -DCodeSite;%compilerflags% -LUvcl;vclx;vcldb;vcldbx;soaprtl;xmlrtl;inet;ldiRegExLib -u%libsearchpath%;%raizepath% -R%libsearchpath% -I%includepath% /$D- /$L- /$Y- /$Q- /$R %dccflags% %dccns%
