21b6f242c882818b44a3e1e1939950a636ce3857
annlynnworth <annlynnworth@88ca0d87-9d1d-4ddd-a6d8-5c97b49fb611>
2013-05-03 02:29:20 +0000

    old ipc whlite

--- webhubdemos/Source/_Control/LiteMore-step00-compile-win32.bat
+++ webhubdemos/Source/_Control/LiteMore-step00-compile-win32.bat
@@ -5,6 +5,16 @@ cd /d %~dp0
 set comp3=D18
 set bits=32
 
+:: old ipc whLite.exe
+:: copy k:\webhub\lib\whvcl\WebHub_Comms.old.inc k:\webhub\lib\WebHub_Comms.inc
+:: copy k:\webhub\lib\whvcl\WebHub_Comms.old.inc h:\WebHub_Comms.inc
+:: if errorlevel 1 pause
+:: cd /d "D:\Projects\webhubdemos\Source\WHApps\Lite Examples\whAppServer\whLite"
+:: call %~dp0\compile-1demo_old_win32_source.bat whlite
+:: copy k:\webhub\lib\whvcl\WebHub_Comms.new.inc k:\webhub\lib\WebHub_Comms.inc
+:: copy k:\webhub\lib\whvcl\WebHub_Comms.new.inc h:\WebHub_Comms.inc
+
+cd /d %~dp0
 call compile-whdemos-lite-more.bat
 
 :END
