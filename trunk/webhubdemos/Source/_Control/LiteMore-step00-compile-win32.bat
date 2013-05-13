7434084abd9133577fd5bb489ad4505fac665a7d
annlynnworth <annlynnworth@88ca0d87-9d1d-4ddd-a6d8-5c97b49fb611>
2013-05-13 06:28:29 +0000

    make standalone whLite for old- and new-ipc, from full source, before building demos for deployment to production servers

--- trunk/webhubdemos/Source/_Control/LiteMore-step00-compile-win32.bat
+++ trunk/webhubdemos/Source/_Control/LiteMore-step00-compile-win32.bat
@@ -4,15 +4,22 @@ cd /d %~dp0
 
 set comp3=D18
 set bits=32
+:: goto newipcWhLite
 
 :: old ipc whLite.exe
-:: copy k:\webhub\lib\whvcl\WebHub_Comms.old.inc k:\webhub\lib\WebHub_Comms.inc
-:: copy k:\webhub\lib\whvcl\WebHub_Comms.old.inc h:\WebHub_Comms.inc
-:: if errorlevel 1 pause
-:: cd /d "D:\Projects\webhubdemos\Source\WHApps\Lite Examples\whAppServer\whLite"
-:: call %~dp0\compile-1demo_old_win32_source.bat whlite
-:: copy k:\webhub\lib\whvcl\WebHub_Comms.new.inc k:\webhub\lib\WebHub_Comms.inc
-:: copy k:\webhub\lib\whvcl\WebHub_Comms.new.inc h:\WebHub_Comms.inc
+if errorlevel 1 pause
+cd /d "D:\Projects\webhubdemos\Source\WHApps\Lite Examples\whAppServer\whLite"
+call "%~dp0\compile-1demo_old_win32_source.bat" whlite
+
+:: new ipc whLite.exe
+:newipcWhLite
+set compilerdigits=18
+copy k:\webhub\lib\whvcl\WebHub_Comms.new.inc k:\webhub\lib\WebHub_Comms.inc
+copy k:\webhub\lib\whvcl\WebHub_Comms.new.inc h:\WebHub_Comms.inc
+cd /d "D:\Projects\webhubdemos\Source\WHApps\Lite Examples\whAppServer\whLite"
+call %~dp0\compile-1demo_x_win32_source.bat whlite
+
+pause
 
 cd /d %~dp0
 call compile-whdemos-lite-more.bat
