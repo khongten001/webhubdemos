c94f9cc0247cce7e296e8563ef4232fcd0c5783b
annlynnworth <annlynnworth@88ca0d87-9d1d-4ddd-a6d8-5c97b49fb611>
2013-07-29 05:42:41 +0000

    nyx win64 testing

--- trunk/webhubdemos/Live/WebHub/Apps/run-DEMOS.bat
+++ trunk/webhubdemos/Live/WebHub/Apps/run-DEMOS.bat
@@ -1,5 +1,12 @@
+setlocal
+
+:: set zmcontext variable
+call d:\AppsData\WinNT\bat\set-zmcontext.bat
+
 ::change to folder containing this bat file
 cd %~dp0
 
 ::start the WebHub overview demo
-start whLite.exe /ID=demos 
+if     "%zmcontext%"=="NYX" start DServer_x_d18_win64.exe /ID=demos
+if NOT "%zmcontext%"=="NYX" start whLite.exe /ID=demos 
+
