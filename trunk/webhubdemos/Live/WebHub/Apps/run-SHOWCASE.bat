f6c6a0f7ec178db151518542fd7ac4752afb1af0
annlynnworth <annlynnworth@88ca0d87-9d1d-4ddd-a6d8-5c97b49fb611>
2013-07-29 09:09:26 +0000

    win64 testing

--- trunk/webhubdemos/Live/WebHub/Apps/run-SHOWCASE.bat
+++ trunk/webhubdemos/Live/WebHub/Apps/run-SHOWCASE.bat
@@ -1,10 +1,15 @@
+setlocal
+
+:: set zmcontext variable
+call d:\AppsData\WinNT\bat\set-zmcontext.bat
+
 ::change to folder containing this bat file
 cd %~dp0
-
 
 ::delete the session data for the quality-assurance test run
 del c:\temp\whSessions\1204.var
 del d:\temp\whSessions\1204.var
 
 ::start the WebHub showcase demo
-start whLite.exe /ID=showcase 
+if     "%zmcontext%"=="NYX" start DServer_x_d18_win64.exe /ID=showcase
+if NOT "%zmcontext%"=="NYX" start whLite.exe /ID=showcase 
