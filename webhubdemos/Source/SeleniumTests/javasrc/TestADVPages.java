48cbd14f4287165e751198fc2c7cf77809014fb6
annlynnworth <annlynnworth@88ca0d87-9d1d-4ddd-a6d8-5c97b49fb611>
2013-08-04 08:16:21 +0000

    Can run in parallel

--- webhubdemos/Source/SeleniumTests/javasrc/TestADVPages.java
+++ webhubdemos/Source/SeleniumTests/javasrc/TestADVPages.java
@@ -2,6 +2,9 @@ package advpackage;
 
 // useful http://www.guru99.com/introduction-to-selenium-grid.html
 // useful http://marakana.com/bookshelf/selenium_tutorial/selenium2.html#_api
+// very useful http://code.google.com/p/selenium/wiki/GettingStarted
+// nice test example using WordPress target  http://testng.org/doc/selenium.html
+
 
 //import org.testng.annotations.Test;
 import org.openqa.selenium.remote.DesiredCapabilities;
@@ -18,54 +21,65 @@ import org.testng.annotations.*;
 
 
 public class TestADVPages {
-	public String baseUrl, nodeURL; 
-	public WebDriver driver; // = new FirefoxDriver();
-	public Long IPort; 
+	public String baseUrl, selenHub; 
 	public DesiredCapabilities capability;
-	
-	
-  @BeforeClass
+		
+  @BeforeTest
   public void setUp() throws MalformedURLException {
 	  baseUrl = "http://w12.demos.href.com/scripts/runisa64.dll?adv:";
-	  nodeURL = "http://localhost:5555/wd/hub";
+	  System.out.println("setUp thread Id = " + String.valueOf(Thread.currentThread().getId()));
+
+	  selenHub = "db.demos.href.com:4444";
+	  //selenHub = "localhost:4444";
+	  
 	  capability = DesiredCapabilities.htmlUnit();   
 	  // as htmlUnit, the user agent goes through as Mozilla/4.0 (compatible; MSIE 8.0; Windows NT 6.0) 
 	  
 	  capability.setBrowserName(DesiredCapabilities.htmlUnit().getBrowserName());	  
-	  capability.setVersion(DesiredCapabilities.htmlUnit().getVersion());	  
-	  capability.setPlatform(org.openqa.selenium.Platform.WINDOWS);	  
-
-	  System.out.println("thread Id = " + String.valueOf(Thread.currentThread().getId()));
-
-	  IPort = 5554 + Thread.currentThread().getId();
-	  System.out.println("IPort = " + String.valueOf(IPort));
-  
-	  nodeURL = "http://localhost:" + String.valueOf(IPort) + "/wd/hub";
-	  System.out.println("nodeURL = " + nodeURL);
+	  //capability.setVersion(DesiredCapabilities.htmlUnit().getVersion());	  
+	  //capability.setPlatform(org.openqa.selenium.Platform.WINDOWS);	  
 	  
-	  driver = new RemoteWebDriver(new URL(nodeURL), capability);
-	  driver.manage().deleteAllCookies();
+	  //capability.setJavascriptEnabled(false);
+	  
+	  capability.setPlatform(org.openqa.selenium.Platform.ANY);	  
+	  
+	  
   
   }
 	
-  @AfterClass
+  @AfterTest
   public void afterTest() {
-	     driver.quit();	  
+//	     driver.quit();	  
   }
   
-  @Test(threadPoolSize = 3, invocationCount = 3,  timeOut = 10000)
-  public void verifyHomepageTitle() {
+  @Test(threadPoolSize = 4, invocationCount = 4,  timeOut = 45000)
+  public void verifyHomepageTitle() throws MalformedURLException {
+		Long IPort; 
+		WebDriver driver; // = new FirefoxDriver();
+		String gridURL;
 
+	  //IPort = 4444; // - 10 + Thread.currentThread().getId();
+	  //System.out.println("IPort = " + String.valueOf(IPort));
+  
+	  //nodeURL = "http://localhost:4444/wd/hub"; //" + String.valueOf(IPort) + "/wd/hub";
+	  gridURL = "http://" + selenHub + "/wd/hub";
+	  //System.out.println("gridURL = " + gridURL);
+
+	  driver = new RemoteWebDriver(new URL(gridURL), capability);
+	  //System.out.println("01");
+	  driver.manage().deleteAllCookies();
+	  //System.out.println("02");
 	  
-	  System.out.println("thread Id = " + String.valueOf(Thread.currentThread().getId()));
 	  driver.get(baseUrl);
+	  //System.out.println("03");
 	  String expectedTitle = "Page pgWelcome: Welcome Page for adv Demo (in the \"adv\" WebHub Demo)";
 	  String actualTitle = driver.getTitle();
+	  System.out.println(actualTitle);
 	  
 	  Assert.assertEquals(actualTitle, expectedTitle);
 	  
      driver.findElement(By.id("a-pgenteradv")).click();
-     if (1 == 3) {
+     if (1 == 1) {
      driver.findElement(By.linkText("Internal Workings")).click();
      driver.findElement(By.linkText("Cycle List Navigation Bar")).click();
      driver.findElement(By.linkText("How to Use it")).click();
@@ -81,6 +95,8 @@ public class TestADVPages {
      driver.findElement(By.linkText("Click to Show Next Advertisement")).click();
      driver.findElement(By.linkText("Click to Show Next Advertisement")).click();
      }
+     
+     driver.quit();
      
   }
 }
