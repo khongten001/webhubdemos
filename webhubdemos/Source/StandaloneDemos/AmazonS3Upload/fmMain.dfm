56e98e9b7ed7153c25260676bd3091013ad6b894
annlynnworth <annlynnworth@88ca0d87-9d1d-4ddd-a6d8-5c97b49fb611>
2013-08-22 11:37:12 +0000

    S3 upload in XE4

---
+++ webhubdemos/Source/StandaloneDemos/AmazonS3Upload/fmMain.dfm
@@ -0,0 +1,144 @@
+object Form2: TForm2
+  Left = 0
+  Top = 0
+  Caption = 'Form2'
+  ClientHeight = 473
+  ClientWidth = 768
+  Color = clBtnFace
+  Font.Charset = DEFAULT_CHARSET
+  Font.Color = clWindowText
+  Font.Height = -18
+  Font.Name = 'Segoe UI'
+  Font.Style = []
+  OldCreateOrder = False
+  OnCreate = FormCreate
+  PixelsPerInch = 120
+  TextHeight = 25
+  object Label1: TLabel
+    Left = 41
+    Top = 305
+    Width = 179
+    Height = 25
+    Caption = 'C:\Windows\system32'
+    Font.Charset = DEFAULT_CHARSET
+    Font.Color = clWindowText
+    Font.Height = -18
+    Font.Name = 'Segoe UI'
+    Font.Style = []
+    ParentFont = False
+  end
+  object Button1: TButton
+    Left = 584
+    Top = 317
+    Width = 137
+    Height = 125
+    Caption = 'Upload to S3'
+    Font.Charset = DEFAULT_CHARSET
+    Font.Color = clWindowText
+    Font.Height = -18
+    Font.Name = 'Segoe UI'
+    Font.Style = []
+    ParentFont = False
+    TabOrder = 0
+    OnClick = Button1Click
+  end
+  object LabeledEditBucket: TLabeledEdit
+    Left = 40
+    Top = 24
+    Width = 297
+    Height = 33
+    EditLabel.Width = 52
+    EditLabel.Height = 25
+    EditLabel.Caption = 'Bucket'
+    TabOrder = 1
+    Text = 'screenshots.href.com'
+  end
+  object LabeledEditAccessKey: TLabeledEdit
+    Left = 40
+    Top = 96
+    Width = 297
+    Height = 33
+    EditLabel.Width = 86
+    EditLabel.Height = 25
+    EditLabel.Caption = 'Access Key'
+    TabOrder = 2
+  end
+  object LabeledEditSecret: TLabeledEdit
+    Left = 40
+    Top = 168
+    Width = 577
+    Height = 33
+    EditLabel.Width = 48
+    EditLabel.Height = 25
+    EditLabel.Caption = 'Secret'
+    TabOrder = 3
+  end
+  object FileListBox1: TFileListBox
+    Left = 41
+    Top = 336
+    Width = 265
+    Height = 106
+    Font.Charset = DEFAULT_CHARSET
+    Font.Color = clWindowText
+    Font.Height = -18
+    Font.Name = 'Segoe UI'
+    Font.Style = []
+    ItemHeight = 25
+    ParentFont = False
+    TabOrder = 4
+  end
+  object DirectoryListBox1: TDirectoryListBox
+    Left = 312
+    Top = 345
+    Width = 241
+    Height = 97
+    DirLabel = Label1
+    FileList = FileListBox1
+    Font.Charset = DEFAULT_CHARSET
+    Font.Color = clWindowText
+    Font.Height = -18
+    Font.Name = 'Segoe UI'
+    Font.Style = []
+    ItemHeight = 25
+    ParentFont = False
+    TabOrder = 5
+  end
+  object DriveComboBox1: TDriveComboBox
+    Left = 312
+    Top = 308
+    Width = 145
+    Height = 31
+    DirList = DirectoryListBox1
+    Font.Charset = DEFAULT_CHARSET
+    Font.Color = clWindowText
+    Font.Height = -18
+    Font.Name = 'Segoe UI'
+    Font.Style = []
+    ParentFont = False
+    TabOrder = 6
+  end
+  object LabeledEditTargetPath: TLabeledEdit
+    Left = 40
+    Top = 233
+    Width = 577
+    Height = 33
+    EditLabel.Width = 328
+    EditLabel.Height = 25
+    EditLabel.Caption = 'Upload Target Path ( blank or end with / )'
+    Font.Charset = DEFAULT_CHARSET
+    Font.Color = clWindowText
+    Font.Height = -18
+    Font.Name = 'Segoe UI'
+    Font.Style = []
+    ParentFont = False
+    TabOrder = 7
+  end
+  object AmazonConnectionInfo1: TAmazonConnectionInfo
+    Protocol = 'https'
+    TableEndpoint = 'sdb.amazonaws.com'
+    QueueEndpoint = 'queue.amazonaws.com'
+    StorageEndpoint = 's3.amazonaws.com'
+    Left = 568
+    Top = 64
+  end
+end
---
+++ webhubdemos/Source/StandaloneDemos/AmazonS3Upload/fmMain.pas
@@ -0,0 +1,112 @@
+unit fmMain;
+
+(*
+Copyright (c) 2013 HREF Tools Corp.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+THE SOFTWARE.
+*)
+
+interface
+
+uses
+  Winapi.Windows, Winapi.Messages, System.SysUtils, System.Variants, System.Classes, Vcl.Graphics,
+  Vcl.Controls, Vcl.Forms, Vcl.Dialogs, IPPeerClient, Vcl.StdCtrls,
+  Vcl.FileCtrl, Vcl.ExtCtrls, Data.Cloud.CloudAPI, Data.Cloud.AmazonAPI;
+
+type
+  TForm2 = class(TForm)
+    AmazonConnectionInfo1: TAmazonConnectionInfo;
+    Button1: TButton;
+    LabeledEditBucket: TLabeledEdit;
+    LabeledEditAccessKey: TLabeledEdit;
+    LabeledEditSecret: TLabeledEdit;
+    FileListBox1: TFileListBox;
+    DirectoryListBox1: TDirectoryListBox;
+    Label1: TLabel;
+    DriveComboBox1: TDriveComboBox;
+    LabeledEditTargetPath: TLabeledEdit;
+    procedure FormCreate(Sender: TObject);
+    procedure Button1Click(Sender: TObject);
+  private
+    { Private declarations }
+  public
+    { Public declarations }
+  end;
+
+var
+  Form2: TForm2;
+
+implementation
+
+{$R *.dfm}
+
+//const
+//  SMDPath = 'originalfilepath';
+//  SMDFrom = 'uploadfrom';
+
+procedure TForm2.Button1Click(Sender: TObject);
+var
+  ResponseInfo: TCloudResponseInfo;
+  //Metadata: TStringList;
+  Filespec: string;
+  stream: TBytesStream;
+  StorageService: TAmazonStorageService;
+begin
+  AmazonConnectionInfo1.AccountName := LabeledEditAccessKey.Text;
+  AmazonConnectionInfo1.AccountKey := LabeledEditSecret.Text;
+  AmazonConnectionInfo1.Protocol := 'https';
+
+  Filespec := Filelistbox1.FileName;
+  if FileExists(Filespec) then
+  begin
+    StorageService := TAmazonStorageService.Create(AmazonConnectionInfo1);
+
+    ResponseInfo := TCloudResponseInfo.Create;
+    stream := TBytesStream.Create;
+    stream.LoadFromFile(Filespec);
+
+    //Metadata not required on Amazon S3
+    //Metadata := TStringList.Create;
+    //Metadata.Values[SMDPath] := ExtractFilePath(Filespec);
+    //Metadata.Values[SMDFrom] := GetComputerandUserName;
+
+    StorageService.UploadObject(
+      LabeledEditBucket.Text,  // target bucket e.g. screenshots.href.com
+
+      { LabeledEditTargetPath.Text must be blank or end in / }
+      LabeledEditTargetPath.Text + ExtractFileName(Filespec),
+
+      Stream.Bytes,
+      False, nil, nil,
+      Data.Cloud.AmazonAPI.amzbaPublicRead,  // permissions - public
+      ResponseInfo);
+
+    {status 200 means that it worked
+     if you use a bad Access Key or Secret Access Key, status 403 will be in
+     headers}
+    ShowMessage(Format('statuscode %d, message %s, headers %s',
+      [ResponseInfo.StatusCode, ResponseInfo.StatusMessage,
+      ResponseInfo.Headers.Text]));
+
+    //FreeAndNil(metadata);
+    FreeAndNil(stream);
+  end;
+end;
+
+procedure TForm2.FormCreate(Sender: TObject);
+var
+  SampleDirectory: string;
+begin
+  SampleDirectory :=
+  'D:\Apps\Embarcadero\RADStudio\11.0\Samples\Delphi\DataSnap\connectors\WindowsPhone7Clients\CompanyTweetClient\CompanyTweetClient\icons';
+  if Directoryexists(SampleDirectory) then
+    DirectoryListBox1.Directory := SampleDirectory;
+end;
+
+end.
