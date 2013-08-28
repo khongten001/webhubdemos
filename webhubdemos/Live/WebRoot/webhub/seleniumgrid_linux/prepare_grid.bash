060b6ceddaba8a6ec29c58c04cae8c266cdcd457
annlynnworth <annlynnworth@88ca0d87-9d1d-4ddd-a6d8-5c97b49fb611>
2013-08-28 08:04:53 +0000

    testing

---
+++ webhubdemos/Live/WebRoot/webhub/seleniumgrid_linux/prepare_grid.bash
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+# established 28-Aug-2013
+# written by HREF Tools Corp.
+# http://db.demos.href.com/webhub/seleniumgrid_linux/prepare_grid.bash
+
+# yum install java-1.7.0-openjdk
+
+cd /usr
+mkdir selenium
+cd selenium
+
+wget https://selenium.googlecode.com/files/selenium-server-standalone-2.33.0.jar
+
