d6800ff0a17120ac022020bbdedbf8c03e51d1e8
annlynnworth <annlynnworth@88ca0d87-9d1d-4ddd-a6d8-5c97b49fb611>
2013-08-29 00:58:50 +0000

    port

--- webhubdemos/Live/WebRoot/webhub/seleniumgrid_linux/start_zombie.bash
+++ webhubdemos/Live/WebRoot/webhub/seleniumgrid_linux/start_zombie.bash
@@ -4,13 +4,20 @@
 # written by HREF Tools Corp.
 # http://db.demos.href.com/webhub/seleniumgrid_linux/start_zombie.bash
 
+echo selenium grid call-back port will be $1
+
 # host has to be set to the current public dns entry
 # let Amazon tell us our current public hostname
+if [-e public-hostname];
+then
+cat public-hostname
+else
 wget http://169.254.169.254/latest/meta-data/public-hostname
+fi
 
-# save relevant portion to an environment variable
+# pipe relevant portion into an environment variable
 ec2publichost=`grep ^ec2*. public-hostname`
 
 # start zombie
-java -jar selenium-server-standalone-2.33.0.jar -role webdriver -hub http://db.demos.href.com:4444/grid/register -browser "browserName=htmlunit" -port 5555 -host $ec2publichost
+java -jar selenium-server-standalone-2.33.0.jar -role webdriver -hub http://db.demos.href.com:4444/grid/register -browser "browserName=htmlunit" -port $1 -host $ec2publichost
 
