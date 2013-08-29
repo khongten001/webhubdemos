fcab5b62442c9f0fc9c39051b9e9a0b55cbd99a9
annlynnworth <annlynnworth@88ca0d87-9d1d-4ddd-a6d8-5c97b49fb611>
2013-08-29 00:44:36 +0000

    public-hostname

--- webhubdemos/Live/WebRoot/webhub/seleniumgrid_linux/start_zombie.bash
+++ webhubdemos/Live/WebRoot/webhub/seleniumgrid_linux/start_zombie.bash
@@ -5,5 +5,12 @@
 # http://db.demos.href.com/webhub/seleniumgrid_linux/start_zombie.bash
 
 # host has to be set to the current public dns entry
-java -jar selenium-server-standalone-2.33.0.jar -role webdriver -hub http://db.demos.href.com:4444/grid/register -browser "browserName=htmlunit" -port 5555 -host ec2-54-213-191-149.us-west-2.compute.amazonaws.com
+# let Amazon tell us our current public hostname
+wget http://169.254.169.254/latest/meta-data/public-hostname
+
+# save relevant portion to an environment variable
+ec2publichost=`grep ^ec2*. public-hostname`
+
+# start zombie
+java -jar selenium-server-standalone-2.33.0.jar -role webdriver -hub http://db.demos.href.com:4444/grid/register -browser "browserName=htmlunit" -port 5555 -host $ec2publichost
 
