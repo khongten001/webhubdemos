df913a96c7f4433a480936d3f252acdaebc3302b
annlynnworth <annlynnworth@88ca0d87-9d1d-4ddd-a6d8-5c97b49fb611>
2013-12-29 06:01:28 +0000

    sample project for sending secure email using Indy components

---
+++ webhubdemos/Source/StandaloneDemos/EMail_with_StartTLS/Test_Indy_Smtp.dpr
@@ -0,0 +1,40 @@
+program Test_Indy_Smtp;
+
+(*
+Copyright (c) 2013 HREF Tools Corp.
+
+Permission is hereby granted, on 29-Dec-2013, free of charge, to any person
+obtaining a copy of this file (the "Software"), to deal in the Software
+without restriction, including without limitation the rights to use, copy,
+modify, merge, publish, distribute, sublicense, and/or sell copies of the
+Software, and to permit persons to whom the Software is furnished to do so,
+subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+THE SOFTWARE.
+*)
+
+uses
+  ucCodeSiteInterface,
+  Vcl.Forms,
+  fmIndyEMailSSL in 'fmIndyEMailSSL.pas' {Form3},
+  Vcl.Themes,
+  Vcl.Styles;
+
+{$R *.res}
+
+begin
+  Application.Initialize;
+  Application.MainFormOnTaskbar := True;
+  TStyleManager.TrySetStyle('Ruby Graphite');
+  Application.CreateForm(TForm3, Form3);
+  Application.Run;
+end.
