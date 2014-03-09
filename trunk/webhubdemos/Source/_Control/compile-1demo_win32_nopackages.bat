538108ec84b7f4ceef9ac6d296f19422d0cc8966
annlynnworth <annlynnworth@88ca0d87-9d1d-4ddd-a6d8-5c97b49fb611>
2014-03-09 14:38:33 +0000

    Test UTF-8 Russian content when sending emails

--- trunk/webhubdemos/Source/_Control/compile-1demo_win32_nopackages.bat
+++ trunk/webhubdemos/Source/_Control/compile-1demo_win32_nopackages.bat
@@ -19,7 +19,7 @@ if "%compilerdigits%"=="19" set raizelib=K:\Vendors\Raize\CodeSite5\Lib\RS-XE5\W
 set libsearchpath="h:\;h:\dcu_d%compilerdigits%_win32;k:\Rubicon\source;%droot%lib\win32\release;"
 set outputroot="d:\Projects\WebHubDemos\Live\WebHub\Apps"
 set pkg=
-set compilerflags=PREVENTSVCMGR;use_IBO;USE_TIBODataset;INHOUSE
+set compilerflags=PREVENTSVCMGR;use_IBO;USE_TIBODataset;INHOUSE%compilerflagsplus%
 set includepath=h:\;k:\Rubicon\source\inc;
 
 :: extra parameters for Delphi XE2+
