d22e5972a31030cc80b44b32f67738ba5a73dd7c
annlynnworth <annlynnworth@88ca0d87-9d1d-4ddd-a6d8-5c97b49fb611>
2014-06-16 02:33:19 +0000

    Error message in case of bad directories

--- trunk/webhubdemos/Source/WhtekoSyntaxColoring/EditPadPro_v7/ManageEditPadProV7INI/BackupEditPadProColors_fmMain.pas
+++ trunk/webhubdemos/Source/WhtekoSyntaxColoring/EditPadPro_v7/ManageEditPadProV7INI/BackupEditPadProColors_fmMain.pas
@@ -12,8 +12,11 @@ type
     Button1: TButton;
     Memo1: TMemo;
     Button2: TButton;
+    Label1: TLabel;
+    Label2: TLabel;
     procedure Button1Click(Sender: TObject);
     procedure Button2Click(Sender: TObject);
+    procedure FormCreate(Sender: TObject);
   private
     { Private declarations }
   public
@@ -31,13 +34,38 @@ uses
   IniFiles, BackupEditPadProColors_uHREFTools_Inhouse;
 
 procedure TForm1.Button1Click(Sender: TObject);
+var
+  ErrorText: string;
 begin
-  Memo1.Lines.Text := LoadHREFToolsColors;
+  Label1.Text := '';
+  Memo1.Lines.Text := LoadHREFToolsColors(ErrorText);
+  if ErrorText = '' then
+    Label1.Text := 'Loaded ' + FormatDateTime('dddd hh:nn:ss', Now)
+  else
+  begin
+    Label1.Text := ErrorText;
+    Memo1.Lines.Add(ErrorText);
+  end;
 end;
 
 procedure TForm1.Button2Click(Sender: TObject);
+var
+  ErrorText: string;
 begin
-  PreserveHREFToolsColors;
+  PreserveHREFToolsColors(ErrorText);
+  if ErrorText = '' then
+    Label2.Text := 'Saved ' + FormatDateTime('dddd hh:nn:ss', Now)
+  else
+  begin
+    Label2.Text := ErrorText;
+    Memo1.Lines.Add(ErrorText);
+  end;
+end;
+
+procedure TForm1.FormCreate(Sender: TObject);
+begin
+  Label1.Text := '';
+  Label2.Text := '';
 end;
 
 end.
