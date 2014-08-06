765f3d91569c74d5fd470a5acc2ae64508988e0a
annlynnworth <annlynnworth@88ca0d87-9d1d-4ddd-a6d8-5c97b49fb611>
2014-08-06 03:13:03 +0000

    no pauses during compilation

--- trunk/webhubdemos/Source/_Control/LiteMore-step00-compile-win32.bat
+++ trunk/webhubdemos/Source/_Control/LiteMore-step00-compile-win32.bat
@@ -21,8 +21,8 @@ copy k:\webhub\lib\whvcl\WebHub_Comms.new.inc h:\WebHub_Comms.inc
 cd /d "D:\Projects\webhubdemos\Source\WHApps\Lite Examples\whAppServer\whLite"
 call %~dp0\compile-1demo_x_source.bat whlite
 
-%CSSend% "intentional pause... before compile-whdemos-lite-more.bat"
-pause
+:: %CSSend% "intentional pause... before compile-whdemos-lite-more.bat"
+:: pause
 
 cd /d %~dp0
 call compile-whdemos-lite-more.bat
