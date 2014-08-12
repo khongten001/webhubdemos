5b4ff62fdde8250047cd94aeaf78399547712d01
annlynnworth <annlynnworth@88ca0d87-9d1d-4ddd-a6d8-5c97b49fb611>
2014-08-12 03:12:40 +0000

    more control when upload speed is super slow

--- trunk/webhubdemos/Source/_Control/Database-step02-ftp-to-dsp.href.com.bat
+++ trunk/webhubdemos/Source/_Control/Database-step02-ftp-to-dsp.href.com.bat
@@ -8,6 +8,14 @@
 
 setlocal
 
+@echo off
+set /P z1=Do you want to FTP the first zip file? y/n/a: 
+set /P z2=Do you want to FTP the second zip file? y/n: 
+set /P z3=Do you want to FTP the third zip file? y/n: 
+
+echo on
+
+
 :: switch to the location of this BAT file
 cd %~dp0
 :: switch to the location of the 7z files
@@ -17,11 +25,19 @@ set h=dsp.href.com
 call %ZaphodsMap%zmset.bat u UsingKey2Value "HREFTools\FileTransfer FTP webhubdemos-database-user"
 call %ZaphodsMap%zmset.bat p UsingKey2Value "HREFTools\FileTransfer FTP webhubdemos-database-pass"
 
+if "%z1%"=="n" goto continue01
 D:\Apps\Utilities\NcFTP\ncftpput.exe -u %u% -p %p% %h% /Live/WebHub/Apps DriveH-source.7z
 if errorlevel 1 pause
+
+:continue01
+if "%z2%"=="n" goto continue02
 D:\Apps\Utilities\NcFTP\ncftpput.exe -u %u% -p %p% %h% /Live/WebHub/Apps Database-dsp.7z
 if errorlevel 1 pause
+
+:continue02
+if "%z3%"=="n" goto TheEnd
 D:\Apps\Utilities\NcFTP\ncftpput.exe -u %u% -p %p% %h% /Live/Library Database-Library-bin.7z
 if errorlevel 1 pause
 
+:TheEnd
 endlocal
