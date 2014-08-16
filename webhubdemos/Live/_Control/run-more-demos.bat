a6826e7a95bc092c578fea40f4d53132dde63c8b
annlynnworth <annlynnworth@88ca0d87-9d1d-4ddd-a6d8-5c97b49fb611>
2014-08-16 04:08:20 +0000

    longer delays v3.218

--- webhubdemos/Live/_Control/run-more-demos.bat
+++ webhubdemos/Live/_Control/run-more-demos.bat
@@ -1,5 +1,5 @@
 :: run-more-demos.bat
-:: Copyright (c) 2011 HREF Tools Corp.
+:: Copyright (c) 2011-2014 HREF Tools Corp.
 :: www.href.com
 
 @echo off
@@ -24,39 +24,39 @@ cd ..\WebHub\Apps
 :More Examples
 
 if "%demohtun%"=="yes" start whStopSpam.exe 
-if "%demohtun%"=="yes" d:\Apps\HREFTools\miscutil\wait.exe 4
+if "%demohtun%"=="yes" d:\Apps\HREFTools\miscutil\wait.exe 8
 if "%demohtun%"=="no" d:\Apps\HREFTools\WebHub\bin\WHCoverMgmt.exe /cover /appid=htun /minutes=%covermin% /reason=%coverreason% 
 
 if "%demohtcv%"=="yes" start whConverter.exe 
-if "%demohtcv%"=="yes" d:\Apps\HREFTools\miscutil\wait.exe 6
+if "%demohtcv%"=="yes" d:\Apps\HREFTools\miscutil\wait.exe 9
 if "%demohtcv%"=="no" d:\Apps\HREFTools\WebHub\bin\WHCoverMgmt.exe /cover /appid=htcv /minutes=%covermin% /reason=%coverreason% 
 
 if "%demohtasync%"=="yes" start whASyncDemo.exe 
-if "%demohtasync%"=="yes" d:\Apps\HREFTools\miscutil\wait.exe 6
+if "%demohtasync%"=="yes" d:\Apps\HREFTools\miscutil\wait.exe 9
 if "%demohtasync%"=="no" d:\Apps\HREFTools\WebHub\bin\WHCoverMgmt.exe /cover /appid=htasync /minutes=999999 "/reason=async feature is not yet ready for use with new-ipc"
 
 if "%demohtdr%"=="yes" start whDropdown.exe 
-if "%demohtdr%"=="yes" d:\Apps\HREFTools\miscutil\wait.exe 6
+if "%demohtdr%"=="yes" d:\Apps\HREFTools\miscutil\wait.exe 9
 if "%demohtdr%"=="no" d:\Apps\HREFTools\WebHub\bin\WHCoverMgmt.exe /cover /appid=htdr /minutes=%covermin% /reason=%coverreason% 
 
 if "%demohtoi%"=="yes" start whOpenID.exe 
-if "%demohtoi%"=="yes" d:\Apps\HREFTools\miscutil\wait.exe 6
+if "%demohtoi%"=="yes" d:\Apps\HREFTools\miscutil\wait.exe 9
 if "%demohtoi%"=="no" d:\Apps\HREFTools\WebHub\bin\WHCoverMgmt.exe /cover /appid=htoi /minutes=%covermin% /reason=%coverreason% 
 
 if "%demohtob%"=="yes" start whObjectInspector.exe 
-if "%demohtob%"=="yes" d:\Apps\HREFTools\miscutil\wait.exe 6
+if "%demohtob%"=="yes" d:\Apps\HREFTools\miscutil\wait.exe 9
 if "%demohtob%"=="no" d:\Apps\HREFTools\WebHub\bin\WHCoverMgmt.exe /cover /appid=htob /minutes=%covermin% /reason=%coverreason% 
 
 if "%demohtol%"=="yes" start whOutline.exe 
-if "%demohtol%"=="yes" d:\Apps\HREFTools\miscutil\wait.exe 4
+if "%demohtol%"=="yes" d:\Apps\HREFTools\miscutil\wait.exe 8
 if "%demohtol%"=="no" d:\Apps\HREFTools\WebHub\bin\WHCoverMgmt.exe /cover /appid=htol /minutes=%covermin% /reason=%coverreason% 
 
 if "%demohtem%"=="yes" start whSendMail.exe   
-if "%demohtem%"=="yes" d:\Apps\HREFTools\miscutil\wait.exe 4
+if "%demohtem%"=="yes" d:\Apps\HREFTools\miscutil\wait.exe 8
 if "%demohtem%"=="no" d:\Apps\HREFTools\WebHub\bin\WHCoverMgmt.exe /cover /appid=htem /minutes=%covermin% /reason=%coverreason% 
 
 if "%demohtgr%"=="yes" start whText2Table.exe 
-if "%demohtgr%"=="yes" d:\Apps\HREFTools\miscutil\wait.exe 4
+if "%demohtgr%"=="yes" d:\Apps\HREFTools\miscutil\wait.exe 8
 if "%demohtgr%"=="no" d:\Apps\HREFTools\WebHub\bin\WHCoverMgmt.exe /cover /appid=htgr /minutes=%covermin% /reason=%coverreason% 
 
 :end
