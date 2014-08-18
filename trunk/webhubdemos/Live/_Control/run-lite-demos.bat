5014ef7f444c9d856b92afe46643ba9da26c871e
annlynnworth <annlynnworth@88ca0d87-9d1d-4ddd-a6d8-5c97b49fb611>
2014-08-18 02:08:08 +0000

    Fast 6 second load times

--- trunk/webhubdemos/Live/_Control/run-lite-demos.bat
+++ trunk/webhubdemos/Live/_Control/run-lite-demos.bat
@@ -8,6 +8,9 @@ setlocal
 call %ZaphodsMap%zmset.bat flagdemoslite UsingKey2Value "HREFTools/WebHub/cv004 SystemStartup demoslite"
 echo flagdemoslite is %flagdemoslite%
 
+set fastseconds=6
+set slowseconds=12
+
 :: change to the location of this bat file
 cd /d %~dp0
 call .\select-lite-demos.bat
@@ -23,44 +26,44 @@ cd ..\WebHub\Apps
 
 :Lite Examples
 if "%demodemos%"=="yes" start whLite.exe /ID=demos  
-if "%demodemos%"=="yes" d:\Apps\HREFTools\miscutil\wait.exe 20
+if "%demodemos%"=="yes" d:\Apps\HREFTools\miscutil\wait.exe %fastseconds%
 if "%demodemos%"=="no" d:\Apps\HREFTools\WebHub\bin\WHCoverMgmt.exe /cover /appid=demos /minutes=%covermin% /reason=%coverreason% 
 if NOT %flagdemoslite%==yes goto end
 
 if "%demoadv%"=="yes" start whLite.exe /ID=adv 
-if "%demoadv%"=="yes" d:\Apps\HREFTools\miscutil\wait.exe 15
+if "%demoadv%"=="yes" d:\Apps\HREFTools\miscutil\wait.exe %fastseconds%
 if "%demoadv%"=="no" d:\Apps\HREFTools\WebHub\bin\WHCoverMgmt.exe /cover /appid=adv /minutes=%covermin% /reason=%coverreason% 
 
 if "%demoshowcase%"=="yes" start whLite.exe /ID:showcase    
-if "%demoshowcase%"=="yes" d:\Apps\HREFTools\miscutil\wait.exe 25
+if "%demoshowcase%"=="yes" d:\Apps\HREFTools\miscutil\wait.exe %slowseconds%
 if "%demoshowcase%"=="no" d:\Apps\HREFTools\WebHub\bin\WHCoverMgmt.exe /cover /appid=showcase /minutes=%covermin% /reason=%coverreason% 
 
 if "%demohtaj%"=="yes" start whLite.exe /ID:htaj    
-if "%demohtaj%"=="yes" d:\Apps\HREFTools\miscutil\wait.exe 15
+if "%demohtaj%"=="yes" d:\Apps\HREFTools\miscutil\wait.exe %fastseconds%
 if "%demohtaj%"=="no" d:\Apps\HREFTools\WebHub\bin\WHCoverMgmt.exe /cover /appid=htaj /minutes=%covermin% /reason=%coverreason% 
 
 if "%demohtfd%"=="yes" start whLite.exe /ID:htfd   
-if "%demohtfd%"=="yes" d:\Apps\HREFTools\miscutil\wait.exe 15
+if "%demohtfd%"=="yes" d:\Apps\HREFTools\miscutil\wait.exe %fastseconds%
 if "%demohtfd%"=="no" d:\Apps\HREFTools\WebHub\bin\WHCoverMgmt.exe /cover /appid=htfd /minutes=%covermin% /reason=%coverreason% 
 
 if "%demoform%"=="yes" start whLite.exe /ID:form    
-if "%demoform%"=="yes" d:\Apps\HREFTools\miscutil\wait.exe 15
+if "%demoform%"=="yes" d:\Apps\HREFTools\miscutil\wait.exe %fastseconds%
 if "%demoform%"=="no" d:\Apps\HREFTools\WebHub\bin\WHCoverMgmt.exe /cover /appid=form /minutes=%covermin% /reason=%coverreason% 
 
 if "%demofast%"=="yes" start whLite.exe /ID:fast    
-if "%demofast%"=="yes" d:\Apps\HREFTools\miscutil\wait.exe 10
+if "%demofast%"=="yes" d:\Apps\HREFTools\miscutil\wait.exe %fastseconds%
 if "%demofast%"=="no" d:\Apps\HREFTools\WebHub\bin\WHCoverMgmt.exe /cover /appid=fast /minutes=%covermin% /reason=%coverreason% 
 
 if "%demobw%"=="yes" start whLite.exe /ID:bw    
-if "%demobw%"=="yes"  d:\Apps\HREFTools\miscutil\wait.exe 14
+if "%demobw%"=="yes"  d:\Apps\HREFTools\miscutil\wait.exe %fastseconds%
 if "%demobw%"=="no" d:\Apps\HREFTools\WebHub\bin\WHCoverMgmt.exe /cover /appid=bw /minutes=%covermin% /reason=%coverreason% 
 
 if "%demohtsc%"=="yes" start whLite.exe /ID:htsc   
-if "%demohtsc%"=="yes" d:\Apps\HREFTools\miscutil\wait.exe 14
+if "%demohtsc%"=="yes" d:\Apps\HREFTools\miscutil\wait.exe %fastseconds%
 if "%demohtsc%"=="no" d:\Apps\HREFTools\WebHub\bin\WHCoverMgmt.exe /cover /appid=htsc /minutes=%covermin% /reason=%coverreason% 
 
 if "%demojoke%"=="yes" start whLite.exe /ID:joke   
-if "%demojoke%"=="yes" d:\Apps\HREFTools\miscutil\wait.exe 14
+if "%demojoke%"=="yes" d:\Apps\HREFTools\miscutil\wait.exe %fastseconds%
 if "%demojoke%"=="no" d:\Apps\HREFTools\WebHub\bin\WHCoverMgmt.exe /cover /appid=joke /minutes=%covermin% /reason=%coverreason% 
 
 :end
