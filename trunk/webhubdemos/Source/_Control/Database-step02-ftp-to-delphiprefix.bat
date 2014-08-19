eb8ce80ddec91ef6c8cb9daaf703f5843328bab8
annlynnworth <annlynnworth@88ca0d87-9d1d-4ddd-a6d8-5c97b49fb611>
2014-08-19 06:46:53 +0000

    manage DPR demo on separate production server

---
+++ trunk/webhubdemos/Source/_Control/Database-step02-ftp-to-delphiprefix.bat
@@ -0,0 +1,25 @@
+:: use the ncftp utility to post the files to the server using ftp
+:: ncftp is freely available yet its author would appreciate a donation
+
+:: use zmset.bat to pull credentials from a local config file
+:: zmset and ZMLookup are freely available from HREF Tools
+:: Pascal source is included with ZaphodsMap 
+:: www.zaphodsmap.com
+
+setlocal
+
+:: switch to the location of this BAT file
+cd %~dp0
+:: switch to the location of the 7z files
+cd ..\..\Live\WebHub\Apps
+
+call %ZaphodsMap%zmset.bat h UsingKey2Value "HREFTools\FileTransfer FTP wesleywhdemos-host"
+call %ZaphodsMap%zmset.bat u UsingKey2Value "HREFTools\FileTransfer FTP wesleywhdemos-user"
+call %ZaphodsMap%zmset.bat p UsingKey2Value "HREFTools\FileTransfer FTP wesleywhdemos-pass"
+
+:continue01
+D:\Apps\Utilities\NcFTP\ncftpput.exe -u %u% -p %p% %h% /Live/WebHub/Apps Database-dpr.7z
+if errorlevel 1 pause
+
+:TheEnd
+endlocal
