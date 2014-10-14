5ba965f6d765b762fbf3935c65e2b04dd3fd8e11
annlynnworth <annlynnworth@88ca0d87-9d1d-4ddd-a6d8-5c97b49fb611>
2014-10-14 03:58:05 +0000

    testing DPR API client side

---
+++ trunk/webhubdemos/Source/WHApps/Third Party Examples/whDPrefix/FMXClient/Test_Client_fmMain.pas
@@ -0,0 +1,55 @@
+unit Test_Client_fmMain;
+
+interface
+
+uses
+  System.SysUtils, System.Types, System.UITypes, System.Classes, System.Variants,
+  FMX.Types, FMX.Controls, FMX.Forms, FMX.Graphics, FMX.Dialogs, FMX.StdCtrls;
+
+type
+  TForm1 = class(TForm)
+    Button1: TButton;
+    Label1: TLabel;
+    Label2: TLabel;
+    Label3: TLabel;
+    Label4: TLabel;
+    Label5: TLabel;
+    procedure Button1Click(Sender: TObject);
+  private
+    { Private declarations }
+  public
+    { Public declarations }
+  end;
+
+var
+  Form1: TForm1;
+
+implementation
+
+{$R *.fmx}
+
+uses DPrefix_Client_uInitialize;
+
+procedure TForm1.Button1Click(Sender: TObject);
+var
+  ErrorText: string;
+begin
+  if Client_Init(ErrorText) then
+  begin
+    Label1.Text := 'DPR_API_Versions_Rec.hdr.Version=' +
+      Format('%2.1f', [DPR_API_Versions_Rec.hdr.Version]);
+    Label2.Text := 'DPR_API_Versions_Rec.WebAppAPISpec_Version=' +
+      IntToStr(DPR_API_Versions_Rec.WebAppAPISpec_Version);
+    Label3.Text := 'DPR_API_Versions_Rec.ImageList_Version=' +
+      IntToStr(DPR_API_Versions_Rec.ImageList_Version);
+    Label4.Text := 'DPR_API_Versions_Rec.LingvoList_Version=' +
+      IntToStr(DPR_API_Versions_Rec.LingvoList_Version);
+    Label5.Text := 'DPR_API_Versions_Rec.TradukoList_Version=' +
+      IntToStr(DPR_API_Versions_Rec.TradukoList_Version);
+    ShowMessage('good');
+  end
+  else
+    ShowMessage(ErrorText);
+end;
+
+end.
