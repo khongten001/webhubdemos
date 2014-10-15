dec1aaac655d75edb32e6ae2a25600ef0dbfc001
annlynnworth <annlynnworth@88ca0d87-9d1d-4ddd-a6d8-5c97b49fb611>
2014-10-15 10:48:02 +0000

    works on Android - UTF8 traduko okay

--- webhubdemos/Source/WHApps/Third Party Examples/whDPrefix/FMXClient/DPrefix_Client.dpr
+++ webhubdemos/Source/WHApps/Third Party Examples/whDPrefix/FMXClient/DPrefix_Client.dpr
@@ -2,6 +2,7 @@ program DPrefix_Client;
 
 uses
   System.StartUpCopy,
+  System.ByteStrings,
   FMX.Forms,
   DPrefix_Client_uMain in 'DPrefix_Client_uMain.pas' {WebBrowserForm},
   ucHttps in 'K:\WebHub\tpack\ucHttps.pas',
@@ -13,10 +14,16 @@ uses
   {$IFNDEF FMACTIVE}
   ucJSONWrapper in 'K:\WebHub\tpack\ucJSONWrapper.pas',
   {$ENDIF}
+{$IFDEF FMACTIVE}
+  // This patch makes AnsiString etc available for use in FireMonkey apps.
+  // http://andy.jgknet.de/blog/2014/09/system-bytestrings-support-for-xe7/
+  System.ByteStrings,
+{$ENDIF}
 *)
 
 var
   ErrorText: string = '';
+  s8: UTF8String = '';
 
 begin
   Application.Initialize;
