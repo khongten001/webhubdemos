8cc01295ce54fa5bf26f0d86fd4ee9834b8f4530
annlynnworth <annlynnworth@88ca0d87-9d1d-4ddd-a6d8-5c97b49fb611>
2015-03-06 23:57:01 +0000

    assume new-ipc; stop running the sequence demo

--- webhubdemos/Live/_Control/select-lite-demos.bat
+++ webhubdemos/Live/_Control/select-lite-demos.bat
@@ -1,44 +1,27 @@
 :: select-more-demos.bat
-:: Copyright (c) 2011 HREF Tools Corp.
+:: Copyright (c) 2011-2015 HREF Tools Corp.
 :: www.href.com
 
-call %ZaphodsMap%\zmset.bat whipc UsingKey2Value "HREFTools\Install WebHub ipc old"
-
-:: default to new-ipc
-if "%whipc%"=="" set whipc=x
-
-echo .
-echo ***
-echo ipc is %whipc%
-echo ***
-echo .
+:: assume new-ipc WebHub v3
 
 set demodemos=yes
 
-if "%whipc%"=="old" set demoadv=yes
-if "%whipc%"=="x"    set demoadv=yes
+set demoadv=yes
 
-if "%whipc%"=="old" set demoshowcase=yes
-if "%whipc%"=="x" set demoshowcase=yes
+set demoshowcase=yes
 
 set demohtaj=yes
 
-if "%whipc%"=="old" set demohtfd=yes
-if "%whipc%"=="x" set demohtfd=yes
+set demohtfd=yes
 
-if "%whipc%"=="old" set demoform=yes
-if "%whipc%"=="x" set demoform=yes
+set demoform=yes
 
-if "%whipc%"=="old" set demofast=yes
-if "%whipc%"=="x" set demofast=yes
+set demofast=yes
 
-if "%whipc%"=="old" set demobw=no
-if "%whipc%"=="x" set demobw=yes
+set demobw=no
 
-if "%whipc%"=="old" set demohtsc=yes
-if "%whipc%"=="x" set demohtsc=yes
+set demohtsc=yes
 
-if "%whipc%"=="old" set demojoke=no
-if "%whipc%"=="x" set demojoke=yes
+set demojoke=no
 
 :end
