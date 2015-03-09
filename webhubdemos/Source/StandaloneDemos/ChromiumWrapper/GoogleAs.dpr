55899becc42f0128327d9842b65e175788bf1ea2
annlynnworth <annlynnworth@88ca0d87-9d1d-4ddd-a6d8-5c97b49fb611>
2015-03-09 00:37:21 +0000

    local shared memory is sufficient for this end-user program; uses clause in DPR

--- webhubdemos/Source/StandaloneDemos/ChromiumWrapper/GoogleAs.dpr
+++ webhubdemos/Source/StandaloneDemos/ChromiumWrapper/GoogleAs.dpr
@@ -8,13 +8,10 @@ uses
   ucShellProcessCntrl in 'k:\webhub\tpack\ucShellProcessCntrl.pas',
   uCode in 'k:\webhub\tpack\uCode.pas',
   System.UITypes,
-  {$IFDEF CEF3}
   ceflib in 'Externals\CEF3\src\ceflib.pas',
+  cefgui in 'Externals\CEF3\src\cefgui.pas',
+  ceferr in 'Externals\CEF3\src\ceferr.pas',
   cefvcl in 'Externals\CEF3\src\cefvcl.pas',
-  {$ELSE}
-  ceflib in 'Externals\CEF1\src\ceflib.pas',
-  cefvcl in 'Externals\CEF1\src\cefvcl.pas',
-  {$ENDIF}
   GoogleAs_uCEF3_Init in 'GoogleAs_uCEF3_Init.pas',
   GoogleAs_fmChromium in 'GoogleAs_fmChromium.pas' {fmChromiumWrapper},
   Vcl.Themes,
@@ -26,16 +23,6 @@ var
   Flag: Boolean;
 
 begin
-  {$IFDEF CEF3}
   InitCEF_GoogleAs(Flag);
   ConditionalStartup(Flag);
-  {$ELSE}
-  Application.Initialize;
-  Application.MainFormOnTaskbar := True;
-
-  // must create Chromium wrapper form first -- main form
-  TStyleManager.TrySetStyle('Emerald Light Slate');
-  Application.CreateForm(TfmChromiumWrapper, fmChromiumWrapper);
-  Application.Run;
-  {$ENDIF}
 end.
