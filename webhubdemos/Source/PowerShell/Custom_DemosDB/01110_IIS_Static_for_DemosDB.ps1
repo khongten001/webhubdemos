5a9220c63d46276b58ef10dc42711e6150e2b70b
annlynnworth <annlynnworth@88ca0d87-9d1d-4ddd-a6d8-5c97b49fb611>
2015-03-18 02:50:39 +0000

    Sample PowerShell files for building up db.demos.href.com server

---
+++ webhubdemos/Source/PowerShell/Custom_DemosDB/01110_IIS_Static_for_DemosDB.ps1
@@ -0,0 +1,9 @@
+# IIS adjustments 
+
+# for use with the WebHub_Appliance_PS project
+# SVN checkout https://svn.code.sf.net/p/webhubapplianceps/code/trunk/PowerShell/WebHub_Appliance_PS
+
+if (! (Test-Path "D:\isites\demos.href.com")) { mkdir D:\isites\demos.href.com }
+
+# Let the site be created when the WebHub Demos are installed.
+
