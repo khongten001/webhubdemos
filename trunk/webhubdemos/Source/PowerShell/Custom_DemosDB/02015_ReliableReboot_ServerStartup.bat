c6adbef0822e3d0b0a6ae0e6f1a4e7fc72210a45
annlynnworth <annlynnworth@88ca0d87-9d1d-4ddd-a6d8-5c97b49fb611>
2015-03-18 03:43:51 +0000

    startup bat file

---
+++ trunk/webhubdemos/Source/PowerShell/Custom_DemosDB/02015_ReliableReboot_ServerStartup.bat
@@ -0,0 +1,5 @@
+:: http://www.howtogeek.com/204088/how-to-use-a-batch-file-to-make-powershell-scripts-easier-to-run/
+
+@ECHO OFF
+PowerShell.exe -Command "& '%~dpn0.ps1'"
+PAUSE
---
+++ trunk/webhubdemos/Source/PowerShell/Custom_DemosDB/02015_ReliableReboot_ServerStartup.ps1
@@ -0,0 +1,17 @@
+# ReliableReboot_ServerStartup
+# for HREF-Rack Server
+
+# To start Task Scheduler on a system with no obvious shortcut for it, run taskschd.msc   (works regardless of language of Windows)
+
+Invoke-Expression ($PSScriptRoot + '\..\WebHub_Appliance_PS\Initialize.ps1')  # loops back and uses custom values
+
+$Bat = "D:\ServerControl\start-hrefrack-server.bat"
+$A = New-ScheduledTaskAction –Execute $Bat
+$T = New-ScheduledTaskTrigger -AtLogon
+$S = New-ScheduledTaskSettingsSet -RunOnlyIfNetworkAvailable
+$D = New-ScheduledTask -Action $A -Trigger $T -Settings $S
+Register-ScheduledTask -TaskName "Server Startup" -InputObject $D
+
+$InfoMsg = ('"AutoStart" "server-startup.bat"')
+Start-Process $Global:CSConsole -ArgumentList $InfoMsg -NoNewWindow -Wait
+echo $InfoMsg
