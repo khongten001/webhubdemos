cf1e20bb249df760fc709809d4d16bb8115b27c3
annlynnworth <annlynnworth@88ca0d87-9d1d-4ddd-a6d8-5c97b49fb611>
2015-03-19 00:05:42 +0000

    virtual path for advanced selenium grid testing

---
+++ webhubdemos/Source/PowerShell/Custom_DemosDB/01251_WebHubRuntime_DemosDB_Post.ps1
@@ -0,0 +1,33 @@
+# After WebHubRuntime and WebHubDemos have been installed
+# Copyright 2015 HREF Tools Corp. 
+# Creative Commons license - keep credits intact.
+
+# call a separate script to init global variables.
+Invoke-Expression ($PSScriptRoot + '\..\WebHub_Appliance_PS\Initialize.ps1')  # loops back and uses custom values
+
+if (! (Test-Path ($env:USERPROFILE + '\Documents\WindowsPowerShell\Modules\Mod_IIS_Setup'))) { 
+	mkdir ($env:USERPROFILE + '\Documents\WindowsPowerShell\Modules\Mod_IIS_Setup')
+}
+copy ($PSScriptRoot + '\..\WebHub_Appliance_PS\Mod_IIS_Setup.psm1')   $env:USERPROFILE\Documents\WindowsPowerShell\Modules\Mod_IIS_Setup\Mod_IIS_Setup.psm1
+Import-Module Mod_IIS_Setup
+
+# Disable Data Execution Prevention (for Hub, runner plus all custom WebHub EXEs)
+Start-Process 'bcdedit.exe' -ArgumentList '/set nx OptIn'
+
+$AWebSite = "IIS:\Sites\WebHubDemos"
+$InfoMsg = 'VirtualDirectory "/seleniumgridoutput"'
+Start-Process $Global:CSConsole -ArgumentList $InfoMsg -NoNewWindow 
+echo $InfoMsg
+New-Item ($AWebSite +'\seleniumgridoutput') -physicalPath 'D:\Apps\Selenium\HREFTools\test-output' -type VirtualDirectory
+
+Start-Process 'iisreset' -NoNewWindow -Wait
+
+StartHttpRunnerEcho 'local-db.demos.href.com' 'scripts' $True $True  # Yes new ipc; Yes 32bit
+
+# Live\Library\*.bpl
+Invoke-Expression ($PSScriptRoot + "\01253_WebHubRuntime_DemosDB_LibraryPath.ps1")
+
+$InfoMsg = ('"WebHub Connection Layer has been reset; Done adjusting" ' + $Filespec)
+Start-Process $Global:CSConsole -ArgumentList $InfoMsg -Wait -NoNewWindow 
+echo $InfoMsg
+
