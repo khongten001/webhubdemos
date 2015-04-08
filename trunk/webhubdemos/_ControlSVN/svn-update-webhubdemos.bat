a9bbb136c926d9e780373037e0f4204a4926ca62
annlynnworth <annlynnworth@88ca0d87-9d1d-4ddd-a6d8-5c97b49fb611>
2015-04-08 03:12:42 +0000

    use CollabNet or Tortoise client

--- trunk/webhubdemos/_ControlSVN/svn-update-webhubdemos.bat
+++ trunk/webhubdemos/_ControlSVN/svn-update-webhubdemos.bat
@@ -8,7 +8,6 @@ setlocal
 set src=http://svn.href.com/svn-public/projects/project-whdemos/trunk
 if exist D:\Apps\Utilities\SVN\Collabnet\svn.exe       set svndir=D:\Apps\Utilities\SVN\Collabnet\
 if exist D:\Apps\Utilities\SVN\TortoiseSVN\bin\svn.exe set svndir=D:\Apps\Utilities\SVN\TortoiseSVN\bin\
-if exist D:\Apps\Utilities\SVN\Console\svn.exe         set svndir=d:\Apps\Utilities\SVN\Console\
 
 cd /d %~dp0
 cd ..
