a82687671cc6c1c235d787784b088d9a9f08dc3b
annlynnworth <annlynnworth@88ca0d87-9d1d-4ddd-a6d8-5c97b49fb611>
2015-04-08 04:29:47 +0000

    XE8 adjustments

--- webhubdemos/Source/_Control/Database-step01-7z-bin.bat
+++ webhubdemos/Source/_Control/Database-step01-7z-bin.bat
@@ -3,11 +3,13 @@
 
 setlocal
 
+set bits=32
+
 cd %~dp0
 cd ..\..\Live\WebHub\Apps
 
 del Database-bin.7z
-del Database-Library-bin.7z
+del Database-Library??-bin.7z
 
 del DriveH-source.7z
 
@@ -37,10 +39,12 @@ d:\Apps\Utilities\7Zip\7z.exe a %t% whFishStore.exe
 d:\Apps\Utilities\7Zip\7z.exe a %t% whScanTable.exe
 if errorlevel 1 pause
 
-set t=Database-Library-bin.7z
+set bits=32
+set compilerdigits=20
+set t=Database-Library%bits%-bin.7z
 
 :: XE6 compiler has bde support
-set sdir=h:\pkg_d20_win32
+set sdir=h:\pkg_d%compilerdigits%_win%bits%
 
 d:\Apps\Utilities\7Zip\7z.exe a %t% %sdir%\ldiRegExLib_d??_win32.bpl
 d:\Apps\Utilities\7Zip\7z.exe a %t% %sdir%\WebHub_d??_win32.bpl
@@ -48,15 +52,16 @@ d:\Apps\Utilities\7Zip\7z.exe a %t% %sdir%\WebHubDB_d??_win32.bpl
 d:\Apps\Utilities\7Zip\7z.exe a %t% %sdir%\WebHubBDE_d??_win32.bpl
 d:\Apps\Utilities\7Zip\7z.exe a %t% %sdir%\ZaphodsMapLib_d??_win32.bpl
 
-:: XE7 compiler has no bde support
-call %~dp0\default-compilerdigits.bat
-set sdir=h:\pkg_d%compilerdigits%_win32
+:: XE8 compiler has no bde support
+set bits=64
+set compilerdigits=22
+set sdir=h:\pkg_d%compilerdigits%_win%bits%
+set t=Database-Library%bits%-bin.7z
 
-d:\Apps\Utilities\7Zip\7z.exe a %t% %sdir%\ldiRegExLib_d%compilerdigits%_win32.bpl
-d:\Apps\Utilities\7Zip\7z.exe a %t% %sdir%\WebHub_d%compilerdigits%_win32.bpl
-d:\Apps\Utilities\7Zip\7z.exe a %t% %sdir%\WebHubDB_d%compilerdigits%_win32.bpl
-d:\Apps\Utilities\7Zip\7z.exe a %t% %sdir%\WebHubBDE_d%compilerdigits%_win32.bpl
-d:\Apps\Utilities\7Zip\7z.exe a %t% %sdir%\ZaphodsMapLib_d%compilerdigits%_win32.bpl
+d:\Apps\Utilities\7Zip\7z.exe a %t% %sdir%\ldiRegExLib_d%compilerdigits%_win??.bpl
+d:\Apps\Utilities\7Zip\7z.exe a %t% %sdir%\WebHub_d%compilerdigits%_win??.bpl
+d:\Apps\Utilities\7Zip\7z.exe a %t% %sdir%\WebHubDB_d??_win??.bpl
+d:\Apps\Utilities\7Zip\7z.exe a %t% %sdir%\ZaphodsMapLib_d%compilerdigits%_win??.bpl
 
 pause
 
