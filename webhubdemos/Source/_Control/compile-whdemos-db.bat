b40d128b631b57e0f28ea56cc04c2a573ab3a32b
annlynnworth <annlynnworth@88ca0d87-9d1d-4ddd-a6d8-5c97b49fb611>
2015-08-02 06:33:59 +0000

    D22 BDE shuffle

--- webhubdemos/Source/_Control/compile-whdemos-db.bat
+++ webhubdemos/Source/_Control/compile-whdemos-db.bat
@@ -5,7 +5,7 @@ set bdecomp=22
 set compilerdigits=%bdecomp%
 set comp3=D%compilerdigits%
 
-if NOT "%comp3%%bits%"=="" goto Continue00
+if NOT "%comp3%%bits%"=="D" goto Continue00
 
 set /P comp3=Enter Pascal Compiler Digits as Number (eg. 23 or 22) :   
 if %comp3%=="" goto end
@@ -13,10 +13,12 @@ set compilerdigits=%comp3%
 set comp3=D%compilerdigits%
 
 :Continue00
-:: first something for those that can compile with latest Delphi, win64
-set cbatwin64=d:\projects\webhubdemos\Source\_Control\compile-1demo_win64.bat
-:: continue...
 set droot=d:\projects\WebHubDemos\Source\WHApps
+:: something for those that can compile with latest Delphi, win64
+set cbatwin64=d:\projects\webhubdemos\Source\_Control\compile-1demo_win64.bat
+if NOT Exist %cbatwin64% %CSSend% /error "[001] %cbatwin64% NOT FOUND"
+if NOT Exist %cbatwin64% pause
+:: continue...
 if "%demonopackages%"=="yes" goto Continue01
 set cbat=d:\projects\webhubdemos\Source\_Control\compile-1demo_win32.bat
 if NOT Exist %cbat% %CSSend% /error "[001] %cbat% NOT FOUND"
@@ -30,6 +32,7 @@ if NOT Exist %cbat% pause
 
 
 :Continue02
+:: Latest Compiler, win64
 :: whLite /id=demos
 :: need DEMOS app even on DB server
 :: clearing compilerdigits makes it go to the default
@@ -38,66 +41,24 @@ call %~dp0\default-compilerdigits.bat
 cd /d %droot%\Lite Examples\whAppServer\whLite
 call %cbatwin64% whLite
 
-:: whQuery1 uses BDE
-set bits=32
-set cbat=d:\projects\webhubdemos\Source\_Control\compile-1demo_win32.bat
-del %~dp0\..\..\Live\WebHub\Apps\whQuery1.exe /q
-if "%compilehtq1%"=="" %CSSend% /error "compilehtq1 is blank. Start from Database-step00-compile-win32."
-if "%compilehtq1%"=="" pause
-if NOT "%compilehtq1%"=="no" cd %droot%\DB Examples\whQuery1
-if NOT "%compilehtq1%"=="no" set compilerdigits=%bdecomp%
-if NOT "%compilehtq1%"=="no" call %cbat% whQuery1
-
 :: whQuery2 uses IBObjects not BDE
-set compilerdigits=
-call %~dp0\default-compilerdigits.bat
 del %~dp0\..\..\Live\WebHub\Apps\whQuery2.exe /q
 if "%compilehtq2%"=="" %CSSend% /error "compilehtq2 is blank"
 if "%compilehtq2%"=="" pause
 if NOT "%compilehtq2%"=="no" cd %droot%\DB Examples\whQuery2
 if NOT "%compilehtq2%"=="no" call %cbatwin64% whQuery2
 
-:: whQuery3 uses BDE
-del %~dp0\..\..\Live\WebHub\Apps\whQuery3.exe /q
-if NOT "%compilehtq3%"=="no" cd %droot%\DB Examples\whQuery3
-if NOT "%compilehtq3%"=="no" set compilerdigits=%bdecomp%
-if NOT "%compilehtq3%"=="no" call %cbat% whQuery3
-
-:: whQuery4 uses BDE
-del %~dp0\..\..\Live\WebHub\Apps\whQuery4.exe /q
-if NOT "%compilehtq4%"=="no" cd %droot%\DB Examples\whQuery4
-if NOT "%compilehtq4%"=="no" set compilerdigits=%bdecomp%
-if NOT "%compilehtq4%"=="no" call %cbat% whQuery4
-
 :: democoderage
 :: whSchedule uses IBObjects and Rubicon, no BDE
 del %~dp0\..\..\Live\WebHub\Apps\whSchedule.exe /q
-if NOT "%compilecoderage%"=="no" set compilerdigits=
-if NOT "%compilecoderage%"=="no" call %~dp0\default-compilerdigits.bat
 if NOT "%compilecoderage%"=="no" cd %droot%\DB Examples\whSchedule
 if NOT "%compilecoderage%"=="no" call %cbatwin64% whSchedule
 
 :: Fish Store uses FireDAC September 2014
 del %~dp0\..\..\Live\WebHub\Apps\whFishStore.exe /q
-set compilerdigits=
 if NOT "%compilehtfs%"=="no" cd %droot%\DB Examples\whFishStore
-if NOT "%compilehtfs%"=="no" call %~dp0\default-compilerdigits.bat
-%CSSend% compilerdigits %compilerdigits%
 %CSSend% "cbatwin64 for Fish Store" %cbatwin64%
 if NOT "%compilehtfs%"=="no" call %cbatwin64% whFishStore
-
-:: whClone uses BDE
-del %~dp0\..\..\Live\WebHub\Apps\whClone.exe /q
-if NOT "%compilehtcl%"=="no" cd %droot%\DB Examples\whClone
-if NOT "%compilehtcl%"=="no" set compilerdigits=%bdecomp%
-if NOT "%compilehtcl%"=="no" call %cbat% whClone
-
-:: whLoadFromDB relies on some properties on the BDE webapp object
-del %~dp0\..\..\Live\WebHub\Apps\whLoadFromDB.exe
-if NOT "%compiledbhtml%"=="no" set compilerdigits=%bdecomp%
-if NOT "%compiledbhtml%"=="no" set bits=32
-if NOT "%compiledbhtml%"=="no" cd %droot%\DB Examples\whLoadFromDB
-if NOT "%compiledbhtml%"=="no" call %cbat% whLoadFromDB
 
 del %~dp0\..\..\Live\WebHub\Apps\whShopping.exe
 set compilerdigits=
@@ -105,23 +66,7 @@ call %~dp0\default-compilerdigits.bat
 if NOT "%compileshop1%"=="no" cd %droot%\DB Examples\whShopping
 if NOT "%compileshop1%"=="no" call %cbatwin64% whShopping
 
-:: whInstantForm requires BDE
-del %~dp0\..\..\Live\WebHub\Apps\whInstantForm.exe
-if NOT "%compilehtfm%"=="no" set compilerdigits=%bdecomp%
-if NOT "%compilehtfm%"=="no" set bits=32
-if NOT "%compilehtfm%"=="no" cd %droot%\DB Examples\whInstantForm
-if NOT "%compilehtfm%"=="no" call %cbat% whInstantForm
-
-:: whScanTable uses bde
-del %~dp0\..\..\Live\WebHub\Apps\whScanTable.exe
-if NOT "%compilescan%"=="no" set compilerdigits=%bdecomp%
-if NOT "%compilescan%"=="no" set bits=32
-if NOT "%compilescan%"=="no" cd %droot%\DB Examples\whScanTable
-if NOT "%compilescan%"=="no" call %cbat% whScanTable
-
 del %~dp0\..\..\Live\WebHub\Apps\whDynamicJPEG.exe
-set compilerdigits=
-call %~dp0\default-compilerdigits.bat
 if NOT "%compilejpeg%"=="no" cd %droot%\DB Examples\whDynamicJPEG
 if NOT "%compilejpeg%"=="no" call %cbatwin64% whDynamicJPEG
 
@@ -136,12 +81,6 @@ del %~dp0\..\..\Live\WebHub\Apps\whRubicon.exe
 if NOT "%compilehtru%"=="no" cd %droot%\Third Party Examples\whRubicon
 if NOT "%compilehtru%"=="no" call %cbat% whRubicon
 
-echo .
-echo ***
-echo ipc is %whipc%
-echo ***
-echo .
-
 ::whDPrefix uses NexusDB not BDE
 :: NexusDB v4.004 as of 28-Apr-2014
 :: Use same version of WebHub as CodeNewsFast so libraries are shared
@@ -150,6 +89,52 @@ if "%compiledpr%"=="no" goto END
 cd %droot%\Third Party Examples\whDPrefix
 set compilerdigits=22
 call d:\projects\webhubdemos\Source\_Control\compile-1demo_win64.bat whDPrefix
+
+
+:::::::::::::::::::::::::::::::
+:: Now BDE which requires win32
+:::::::::::::::::::::::::::::::
+
+set compilerdigits=%bdecomp%
+set bits=32
+set cbat=d:\projects\webhubdemos\Source\_Control\compile-1demo_win32.bat
+
+:: whQuery1 uses BDE
+del %~dp0\..\..\Live\WebHub\Apps\whQuery1.exe /q
+if "%compilehtq1%"=="" %CSSend% /error "compilehtq1 is blank. Start from Database-step00-compile-win32."
+if "%compilehtq1%"=="" pause
+if NOT "%compilehtq1%"=="no" cd %droot%\DB Examples\whQuery1
+if NOT "%compilehtq1%"=="no" call %cbat% whQuery1
+
+:: whQuery3 uses BDE
+del %~dp0\..\..\Live\WebHub\Apps\whQuery3.exe /q
+if "%compilehtq3%"=="yes" cd %droot%\DB Examples\whQuery3
+if "%compilehtq3%"=="yes" call %cbat% whQuery3
+
+:: whQuery4 uses BDE
+del %~dp0\..\..\Live\WebHub\Apps\whQuery4.exe /q
+if NOT "%compilehtq4%"=="no" cd %droot%\DB Examples\whQuery4
+if NOT "%compilehtq4%"=="no" call %cbat% whQuery4
+
+:: whClone uses BDE
+del %~dp0\..\..\Live\WebHub\Apps\whClone.exe /q
+if NOT "%compilehtcl%"=="no" cd %droot%\DB Examples\whClone
+if NOT "%compilehtcl%"=="no" call %cbat% whClone
+
+:: whLoadFromDB relies on some properties on the BDE webapp object
+del %~dp0\..\..\Live\WebHub\Apps\whLoadFromDB.exe
+if NOT "%compiledbhtml%"=="no" cd %droot%\DB Examples\whLoadFromDB
+if NOT "%compiledbhtml%"=="no" call %cbat% whLoadFromDB
+
+:: whInstantForm requires BDE
+del %~dp0\..\..\Live\WebHub\Apps\whInstantForm.exe
+if NOT "%compilehtfm%"=="no" cd %droot%\DB Examples\whInstantForm
+if NOT "%compilehtfm%"=="no" call %cbat% whInstantForm
+
+:: whScanTable uses bde
+del %~dp0\..\..\Live\WebHub\Apps\whScanTable.exe
+if NOT "%compilescan%"=="no" cd %droot%\DB Examples\whScanTable
+if NOT "%compilescan%"=="no" call %cbat% whScanTable
 
 goto end
 
