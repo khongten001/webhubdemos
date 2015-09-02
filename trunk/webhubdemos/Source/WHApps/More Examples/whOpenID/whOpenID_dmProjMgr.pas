dfb5f4a70d48c466de0747f99bc9963f11ea3985
annlynnworth <annlynnworth@88ca0d87-9d1d-4ddd-a6d8-5c97b49fb611>
2015-09-02 13:31:01 +0000

    janrain intf corrected

--- trunk/webhubdemos/Source/WHApps/More Examples/whOpenID/whOpenID_dmProjMgr.pas
+++ trunk/webhubdemos/Source/WHApps/More Examples/whOpenID/whOpenID_dmProjMgr.pas
@@ -39,14 +39,22 @@ end;
 
 procedure TDMProjForWHOpenID.ProjMgrDataModulesInit(Sender: TtpProject;
   var ErrorText: string; var Continue: Boolean);
+var
+  AFilespec: string;
 begin
   inherited;
   Continue := DMWHOpenIDviaJanrain.Init(ErrorText);
   if Continue then
   begin
+    AFilespec := getHtDemoCodeRoot +
+      'More Examples\whOpenID\janrain_api_key.txt';
     DMWHOpenIDviaJanrain.APIKey :=
-      Trim(StringLoadFromFile(getHtDemoCodeRoot +
-      'More Examples\whOpenID\janrain_api_key.txt'));
+      Trim(StringLoadFromFile(AFilespec));
+    if DMWHOpenIDviaJanrain.APIKey = '' then
+    begin
+      ErrorText := 'Unable to load valid JANRAIN API Secret Key from ' + AFilespec;
+      Continue := False;
+    end;
   end;
 end;
 
