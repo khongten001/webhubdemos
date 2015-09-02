2b41bedc7d84c98d7b491c9cd239fbf4ba7090c6
annlynnworth <annlynnworth@88ca0d87-9d1d-4ddd-a6d8-5c97b49fb611>
2015-09-02 13:29:48 +0000

    D23 adjustments

--- webhubdemos/Source/WHApps/Common/whdemo_About.pas
+++ webhubdemos/Source/WHApps/Common/whdemo_About.pas
@@ -1,9 +1,9 @@
 unit whdemo_About;  {panel to help self-document all WebHub demos}
 
 { ---------------------------------------------------------------------------- }
-{ * Copyright (c) 1996-2014 HREF Tools Corp.  All Rights Reserved Worldwide. * }
+{ * Copyright (c) 1996-2015 HREF Tools Corp.  All Rights Reserved Worldwide. * }
 { *                                                                          * }
-{ * This source code file is part of WebHub v3.1x.  Please obtain a WebHub   * }
+{ * This source code file is part of WebHub v3.2x.  Please obtain a WebHub   * }
 { * development license from HREF Tools Corp. before using this file, and    * }
 { * refer friends and colleagues to http://www.href.com/webhub. Thanks!      * }
 { ---------------------------------------------------------------------------- }
