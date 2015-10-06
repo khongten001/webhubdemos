713e472cb05a2bba2533a2ce98241f45ae18e74b
annlynnworth <annlynnworth@88ca0d87-9d1d-4ddd-a6d8-5c97b49fb611>
2015-10-06 00:27:09 +0000

    multipart/alternative

--- trunk/webhubdemos/Source/_Control/compile-standalone.bat
+++ trunk/webhubdemos/Source/_Control/compile-standalone.bat
@@ -7,12 +7,12 @@ cd %~dp0\..\StandaloneDemos\EMail_with_StartTLS
 
 del %~dp0\..\..\Live\WebHub\Apps\Test_Indy*.exe
 
-set compilerdigits=18
+set compilerdigits=22
 call %~dp0\compile-1demo_win32_nopackages.bat Test_Indy_Smtp
 ren %~dp0\..\..\Live\WebHub\Apps\Test_Indy_Smtp.exe Test_Indy_Smtp_D%compilerdigits%.exe
 if errorlevel 1 pause
 
-set compilerdigits=22
+set compilerdigits=23
 call %~dp0\compile-1demo_win32_nopackages.bat Test_Indy_Smtp
 ren %~dp0\..\..\Live\WebHub\Apps\Test_Indy_Smtp.exe Test_Indy_Smtp_D%compilerdigits%.exe
 if errorlevel 1 pause
