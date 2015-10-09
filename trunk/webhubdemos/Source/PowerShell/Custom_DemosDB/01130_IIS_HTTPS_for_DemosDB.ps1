dc61f477c21a0355be0cbff44c1507b89a90ae25
annlynnworth <annlynnworth@88ca0d87-9d1d-4ddd-a6d8-5c97b49fb611>
2015-10-09 08:43:13 +0000

    first draft for secure HTTP certificate creation and binding

---
+++ trunk/webhubdemos/Source/PowerShell/Custom_DemosDB/01130_IIS_HTTPS_for_DemosDB.ps1
@@ -0,0 +1,53 @@
+# Use CERTREQ to make a new HTTPS certificate request
+
+# call a separate script to init global variables.
+Invoke-Expression ($PSScriptRoot + '\..\WebHub_Appliance_PS\Initialize.ps1')  # loops back and uses custom values
+
+# include EZUtil functions
+."$Global:EzUtil"
+
+$RequestINFFilespec = "D:\Projects\webhubdemos\Live\ssl\Cert_Request_db.demos.href.com.inf"
+$REQFilespec = "D:\Projects\webhubdemos\Live\ssl\Cert_Request_db.demos.href.com.req.txt"
+$CERFilespec
+Start-Process "CERTREQ" -ArgumentList ('-New "' + $RequestINFFilespec + '" "' + $REQFilespec + '"') -NoNewWindow -Wait
+
+Start-Process $Global:CSConsole -ArgumentList '-Note "You should pass the REQUEST data to your secure certificate provider"' -NoNewWindow -Wait
+Start-Process $Global:CSConsole -ArgumentList '-Note "Recommended providers are DigiCert, CACert.org, LetsEncrypt.org"' -NoNewWindow -Wait
+
+Start-Process $Global:CSConsole -ArgumentList ('"Opening the REQUEST file in Notepad for your convenience" "' + $REQFilespec + '"') -NoNewWindow -Wait
+Start-Process "Notepad" -ArgumentList $REQFilespec -NoNewWindow -Wait
+
+echo .
+echo .
+echo "Press any key to continue -- best to do this"
+echo "AFTER you have submitted the REQUEST..."
+echo $REQFilespec
+$x = $host.UI.RawUI.ReadKey("NoEcho,IncludeKeyDown")
+
+# http://technet.microsoft.com/en-us/library/ee692792.aspx
+# This is called a "HERE" file.
+# No Leading Space.
+$x = @"
+// Placeholder.
+// When you have the response from the secure certificate provider,
+// replace all this with your secure certificate code.
+"@
+$x | out-file -filepath $CERFilespec -encoding ASCII 
+
+Start-Process $Global:CSConsole -ArgumentList ('"Opening the RESPONSE CERTIFICATE file in Notepad for your convenience" "' + $CERFilespec + '"') -NoNewWindow -Wait
+Start-Process "Notepad" -ArgumentList $CERFilespec -NoNewWindow -Wait
+
+echo .
+echo .
+echo "Press any key to continue -- "
+echo "AFTER you have saved the CERTIFICATE file"
+echo $CERFilespec
+echo "(from Notepad)..."
+$x = $host.UI.RawUI.ReadKey("NoEcho,IncludeKeyDown")
+
+Start-Process "CERTREQ" -ArgumentList ('-Accept "' + $CERFilespec + '"') -NoNewWindow -Wait
+
+$webSiteName = "WebHub Demos"
+$certDomain = 'db.demos.href.com'
+
+$ezutil.bindCertToHostHeader($webSiteName, $certDomain)
