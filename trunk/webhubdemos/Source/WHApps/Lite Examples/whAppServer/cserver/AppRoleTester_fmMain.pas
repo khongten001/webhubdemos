93a3afe157500d715a93a917280a807a625d96da
annlynnworth <annlynnworth@88ca0d87-9d1d-4ddd-a6d8-5c97b49fb611>
2016-01-02 05:40:36 +0000

    memo to show history of instance number

--- trunk/webhubdemos/Source/WHApps/Lite Examples/whAppServer/cserver/AppRoleTester_fmMain.pas
+++ trunk/webhubdemos/Source/WHApps/Lite Examples/whAppServer/cserver/AppRoleTester_fmMain.pas
@@ -11,7 +11,12 @@ type
     tpAppRole2: TtpAppRole;
     Button1: TButton;
     Label1: TLabel;
+    Memo1: TMemo;
+    Button2: TButton;
     procedure Button1Click(Sender: TObject);
+    procedure FormCreate(Sender: TObject);
+    procedure FormActivate(Sender: TObject);
+    procedure Button2Click(Sender: TObject);
   private
     { Private declarations }
   public
@@ -26,10 +31,29 @@ implementation
 {$R *.dfm}
 
 procedure TForm3.Button1Click(Sender: TObject);
+var
+  S1: string;
 begin
-  Label1.Caption :=
+  S1 :=
     Format('"%s" is #%d of %d',
     [tpAppRole2.AppID, tpAppRole2.InstanceSequence, tpAppRole2.InstanceCount]);
+  Memo1.Lines.Add(S1);
+  Label1.Caption := S1;
+end;
+
+procedure TForm3.Button2Click(Sender: TObject);
+begin
+  Form3.Close;
+end;
+
+procedure TForm3.FormActivate(Sender: TObject);
+begin
+  Button1Click(Sender);
+end;
+
+procedure TForm3.FormCreate(Sender: TObject);
+begin
+  Button1Click(Sender);
 end;
 
 end.
