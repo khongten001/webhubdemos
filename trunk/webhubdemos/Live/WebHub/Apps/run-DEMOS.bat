75beff5f65007b292a7e069be63e26cfc20b0ee1
annlynnworth <annlynnworth@88ca0d87-9d1d-4ddd-a6d8-5c97b49fb611>
2016-01-05 00:37:50 +0000

    support run as admin

--- trunk/webhubdemos/Live/WebHub/Apps/run-DEMOS.bat
+++ trunk/webhubdemos/Live/WebHub/Apps/run-DEMOS.bat
@@ -1,12 +1,8 @@
 setlocal
 
-:: set zmcontext variable
-call d:\AppsData\WinNT\bat\set-zmcontext.bat
-
 ::change to folder containing this bat file
-cd %~dp0
+cd /D %~dp0
 
 ::start the WebHub overview demo
-if     "%zmcontext%"=="NYX" start DServer_x_d18_win64.exe /ID=demos
-if NOT "%zmcontext%"=="NYX" start whLite.exe /ID=demos 
+start whLite.exe /ID=demos 
 
