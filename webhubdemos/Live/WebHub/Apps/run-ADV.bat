a9fb6baa6dc93ed9cf3f8b8a3be2a29cd1f349d1
annlynnworth <annlynnworth@88ca0d87-9d1d-4ddd-a6d8-5c97b49fb611>
2016-01-06 11:34:43 +0000

    win64 test

--- webhubdemos/Live/WebHub/Apps/run-ADV.bat
+++ webhubdemos/Live/WebHub/Apps/run-ADV.bat
@@ -5,8 +5,22 @@ call %ZaphodsMap%zmset.bat zmcontext AsDefaultContext
 set compilerdigits=23
 
 ::change to folder containing this bat file
-cd /d %~dp0
+cd /D %~dp0
 
 ::start the WebHub advertising demo
-if     "%zmcontext%"=="NYX" start DServer_x_d%compilerdigits%_win64.exe /ID=adv
-if NOT "%zmcontext%"=="NYX" start whLite.exe /ID=adv
+if     "%zmcontext%"=="NYX"		goto %zmcontext%
+if     "%zmcontext%"=="ultraann"	goto %zmcontext%
+goto others
+
+:NYX
+start DServer_x_d%compilerdigits%_win64.exe /ID=adv
+goto end
+
+:ultraann
+start whlite_x_d23_win64_src.exe /ID=adv
+goto end
+
+:others
+start whLite /ID=adv
+
+:end
