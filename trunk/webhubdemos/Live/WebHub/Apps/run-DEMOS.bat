bcb9f3ab75002a3db37980a4c096c9c556a25ff8
annlynnworth <annlynnworth@88ca0d87-9d1d-4ddd-a6d8-5c97b49fb611>
2016-01-06 18:00:31 +0000

    testing

--- trunk/webhubdemos/Live/WebHub/Apps/run-DEMOS.bat
+++ trunk/webhubdemos/Live/WebHub/Apps/run-DEMOS.bat
@@ -1,8 +1,26 @@
 setlocal
 
+:: set zmcontext variable
+call %ZaphodsMap%zmset.bat zmcontext AsDefaultContext
+set compilerdigits=23
+
 ::change to folder containing this bat file
 cd /D %~dp0
 
 ::start the WebHub overview demo
-start whLite.exe /ID=demos 
+if     "%zmcontext%"=="NYX"		goto %zmcontext%
+if     "%zmcontext%"=="ultraann"	goto %zmcontext%
+goto others
+:NYX
+start DServer_x_d%compilerdigits%_win64.exe /ID=demos
+goto end
+
+:ultraann
+start whlite_x_d23_win64_src.exe /ID=demos
+goto end
+
+:others
+start whLite /ID=demos
+
+:end
 
