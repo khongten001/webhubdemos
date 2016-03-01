4fe6c1fd381a816dc5f2616bd6f6479f4ed781f8
annlynnworth <annlynnworth@88ca0d87-9d1d-4ddd-a6d8-5c97b49fb611>
2016-03-01 08:59:01 +0000

    Http-WebHub-Runner-Workflow

--- trunk/webhubdemos/Source/PowerShell/Custom_DemosDB/09001_Parallel_Http_Test.ps1
+++ trunk/webhubdemos/Source/PowerShell/Custom_DemosDB/09001_Parallel_Http_Test.ps1
@@ -9,15 +9,23 @@ $InfoMsg = ('"parallel http" ok')
 echo $InfoMsg
 #Start-Process $Global:CSConsole -ArgumentList $InfoMsg -NoNewWindow 
 
-workflow Http-Google-Workflow {
-	$myArray = @("http://www.google.com/", "http://www.google.de/")
-	
-	ForEach -Parallel ($a in $myArray) {
-		#echo $a
-		$Content = Invoke-WebRequest -URI $a
-        $charlen = $Content.RawContentLength
-		echo $a "has length" $charlen
-	}
+workflow Http-WebHub-Runner-Workflow {
+    $NRepeat = 4
+	$myArray = @(
+        "http://delphiprefix.modulab.com/scripts/runisa_x_d23_win64.dll?r:echo", 
+        "http://delphiprefix.modulab.com/scripts/runisa_x_d23_win64.dll?r:pcv",
+        "http://delphiprefix.modulab.com/win64/runisa_x_d23_win64.dll?adv:pgTestLoremIpsum::1",
+        "http://delphiprefix.modulab.com/win64/runisa_x_d23_win64.dll?adv:pgTestLoremIpsum::3"
+       )
+
+    ForEach ($number in 1..$NRepeat ) {	
+	    ForEach -Parallel ($a in $myArray) {
+		    #echo ('Number: ' + $number)
+		    $Content = Invoke-WebRequest -URI $a
+            $charlen = $Content.RawContentLength
+		    echo ($a + " has length " + $charlen)
+	    }
+    }
 }
 
-Http-Google-Workflow
+Http-WebHub-Runner-Workflow
