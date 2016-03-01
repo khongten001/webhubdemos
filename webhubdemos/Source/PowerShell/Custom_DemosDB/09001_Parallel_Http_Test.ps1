0199eae864a714955f962c40b68b4811e0844bcd
annlynnworth <annlynnworth@88ca0d87-9d1d-4ddd-a6d8-5c97b49fb611>
2016-03-01 08:44:43 +0000

    Http-Google-Workflow works

--- webhubdemos/Source/PowerShell/Custom_DemosDB/09001_Parallel_Http_Test.ps1
+++ webhubdemos/Source/PowerShell/Custom_DemosDB/09001_Parallel_Http_Test.ps1
@@ -2,21 +2,22 @@
 # Copyright 2016 HREF Tools Corp. 
 # Creative Commons license - keep credits intact.
 
-# call a separate script to init global variables.
-# Invoke-Expression "$PSScriptRoot\Initialize.ps1"
+# call a separate script if you need to init global variables.
+#Invoke-Expression "$PSScriptRoot\Initialize.ps1"
 
 $InfoMsg = ('"parallel http" ok')
 echo $InfoMsg
 #Start-Process $Global:CSConsole -ArgumentList $InfoMsg -NoNewWindow 
 
-workflow Test-Workflow {
+workflow Http-Google-Workflow {
 	$myArray = @("http://www.google.com/", "http://www.google.de/")
 	
 	ForEach -Parallel ($a in $myArray) {
-		echo $a
-		#Start $a
+		#echo $a
 		$Content = Invoke-WebRequest -URI $a
-		$CharLen = $Content | measure-object -character | select -expandproperty characters
-		echo $charLen
+        $charlen = $Content.RawContentLength
+		echo $a "has length" $charlen
 	}
 }
+
+Http-Google-Workflow
