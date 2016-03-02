e20dc27c6703a18f2ca5083ae4b91faae3341c0d
annlynnworth <annlynnworth@88ca0d87-9d1d-4ddd-a6d8-5c97b49fb611>
2016-03-02 09:07:21 +0000

    work in progress - does not run correctly

--- trunk/webhubdemos/Source/PowerShell/Custom_DemosDB/09001_Parallel_Http_Test.ps1
+++ trunk/webhubdemos/Source/PowerShell/Custom_DemosDB/09001_Parallel_Http_Test.ps1
@@ -9,38 +9,58 @@ $InfoMsg = ('"parallel testing" "of http requests"')
 echo $InfoMsg
 #Start-Process $Global:CSConsole -ArgumentList $InfoMsg -NoNewWindow 
 
-function SizeCloseTo{ Param ([int]$actualSize, [int]$expectedSize) 
+<#function SizeCloseTo{ Param ([int]$actualSize, [int]$expectedSize) 
   $edge1 = $expectedSize - 8
   $edge2 = $expectedSize + 8
   if ( ($edge1 -le $actualSize) -and ($edge2 -ge $actualSize)) {
-    return $True
+    return 1
   } else {
-    echo $edge1 $edge2 $actualSize $expectedSize
-    return $False
+    # Write-Error ([string]$actualSize + " Fails for " + [string]$expectedSize)
+    return 0
   }
-}
+}#>
+
+# this should return $False
+# SizeCloseTo 633 2209
 
 workflow Http-WebHub-Runner-Workflow {
-    $NRepeat = 2
+    $NRepeat = 1
 	$myArray = @(
         "http://delphiprefix.modulab.com/scripts/runisa_x_d23_win64.dll?r:echo", 
         #"http://delphiprefix.modulab.com/scripts/runisa_x_d23_win64.dll?r:pcv",
         "http://delphiprefix.modulab.com/win64/runisa_x_d23_win64.dll?adv:pgTestLoremIpsum::2",
-        "http://delphiprefix.modulab.com/win64/runisa_x_d23_win64.dll?demos:pgTestLoremIpsum::2",
-        "http://delphiprefix.modulab.com/win64/runisa_x_d23_win64.dll?adv:pgTestLoremIpsum::3",
+        "http://delphiprefix.modulab.com/win64/runisa_x_d23_win64.dll?demos:pgTestLoremIpsum:888888:2",
+        "http://delphiprefix.modulab.com/win64/runisa_x_d23_win64.dll?invalid:invalid",
+        "http://delphiprefix.modulab.com/win64/runisa_x_d23_win64.dll?adv:pgTestLoremIpsum:888888:3",
         "http://delphiprefix.modulab.com/win64/runisa_x_d23_win64.dll?adv:pgTestLoremIpsum::4"
        )
+
+function SizeCloseTo{ Param ([int]$actualSize, [int]$expectedSize) 
+  $edge1 = $expectedSize - 8
+  $edge2 = $expectedSize + 8
+  if ( ($edge1 -le $actualSize) -and ($edge2 -ge $actualSize)) {
+    return 1
+  } else {
+    # Write-Error ([string]$actualSize + " Fails for " + [string]$expectedSize)
+    return 0
+  }
+}
 
     ForEach ($number in 1..$NRepeat ) {	
 	    ForEach -Parallel ($a in $myArray) {
 		    #echo ('Number: ' + $number)
-		    $Content = Invoke-WebRequest -URI $a
-            $charlen = [int]$Content.RawContentLength
-            if (SizeCloseTo $charlen 2208 -or SizeCloseTo $charlen 10321 -or SizeCloseTo $charlen 15244 -or SizeCloseTo -eq 20167) {
-		        #echo ($a + " has length " + $charlen)
+		    $Content = Invoke-WebRequest -URI $a -TimeoutSec 5;
+            $charlen = [int]$Content.RawContentLength;
+            $aok = SizeCloseTo $charlen 2208;
+            $aok = ($aok) -or (SizeCloseTo $charlen 10321);
+            $aok = ($aok) -or (SizeCloseTo $charlen 15244);
+            $aok = ($aok) -or (SizeCloseTo $charlen 20167);
+            if ($aok) {
+		        echo ($a + " has length " + $charlen)
                 echo ('ok ' + $charlen)
             } else {
-                Write-Error ($a + " wrong length! " + $charlen)
+                Write-Error ($a + " " + [string]$charLen + " Fails")
+            
             }
 	    }
     }
