92678ec51c9770803e38a14735be6c8965276708
annlynnworth <annlynnworth@88ca0d87-9d1d-4ddd-a6d8-5c97b49fb611>
2016-03-30 00:22:40 +0000

    cross-domain ajax example

---
+++ webhubdemos/Live/WebRoot/webhub/demos/js/zurb_ajax_demo.js
@@ -0,0 +1,49 @@
+/* zurb_ajax_demo */
+
+function sendDemoMessage() {
+
+	$('#divDataEntryFeedback').html('<img src="https://openclipart.org/download/195700/loader.svg" style="height: 50px;" alt="Please Wait" />');
+	$('#divDataEntryFeedback').show();
+
+	$.ajax( {
+		url:  'http://two.delphiprefix.modulab.com/scripts/runisa.dll?htfd:ajaxtest::002', 
+		data:  $('#formContact').serialize(),
+		type: "POST",
+		crossDomain: true, /* enable this */
+		headers: { "cache-control": "no-cache" },
+		dataType: "json",
+		//
+		success: function(json) {
+			if (json.responseStatus == 'OK') {
+				$('#divDataEntryFeedback').hide();
+				$('#messageThanksModal').foundation('reveal', 'open');
+			}
+			else
+			if (json.responseStatus == 'ERROR') {
+				/*alert('Error: ' + json.errorMsg);*/
+				$('#divDataEntryFeedback').html(json.errorMsg);
+				$('#divDataEntryFeedback').show();
+			}
+			else {
+				alert('Unexpected Error');
+				$('#dataEntryFeedback').html('Unexpected Error');
+				$('#divDataEntryFeedback').show();
+			}
+		},
+		//
+		error: function(jqXHR, exception) {
+			alert(exception);
+		}
+		}
+	);	
+}
+
+function addRevealEvents_FeedbackMessage() {
+	$('#messageInputModal').bind('opened.fndtn.reveal', function() {
+		$('#divDataEntryFeedback').hide();
+		document.getElementById("btnSendAnswers").addEventListener("click", sendDemoMessage);
+	});
+}
+
+$(document).ready(addRevealEvents_FeedbackMessage);
+
