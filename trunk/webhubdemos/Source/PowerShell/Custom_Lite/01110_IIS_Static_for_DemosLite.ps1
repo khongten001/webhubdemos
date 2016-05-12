94143bb802a5b6df55840cdf2518bceaaf21dce2
annlynnworth <annlynnworth@88ca0d87-9d1d-4ddd-a6d8-5c97b49fb611>
2016-05-12 08:43:41 +0000

    Live\WebRootDomains

--- trunk/webhubdemos/Source/PowerShell/Custom_Lite/01110_IIS_Static_for_DemosLite.ps1
+++ trunk/webhubdemos/Source/PowerShell/Custom_Lite/01110_IIS_Static_for_DemosLite.ps1
@@ -3,7 +3,7 @@
 # for use with the WebHub_Appliance_PS project
 # SVN checkout https://svn.code.sf.net/p/webhubapplianceps/code/trunk/PowerShell/WebHub_Appliance_PS
 
-if (! (Test-Path "D:\isites\demos.href.com")) { mkdir D:\isites\demos.href.com }
+##### placeholder #####
 
 # Let the site be created when the WebHub Demos are installed.
 
