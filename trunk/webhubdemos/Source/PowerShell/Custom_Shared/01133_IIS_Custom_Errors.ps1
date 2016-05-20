091a87be893af3427c9be5c01d74f060fca2ce6c
annlynnworth <annlynnworth@88ca0d87-9d1d-4ddd-a6d8-5c97b49fb611>
2016-05-20 05:27:27 +0000

    use shared functions to configure the custom error messages in IIS

--- trunk/webhubdemos/Source/PowerShell/Custom_Shared/01133_IIS_Custom_Errors.ps1
+++ trunk/webhubdemos/Source/PowerShell/Custom_Shared/01133_IIS_Custom_Errors.ps1
@@ -6,30 +6,38 @@
 # Creative Commons license - keep credits intact.
 
 # Thanks to http://forums.iis.net/t/1185503.aspx?creating+a+custom+http+error+for+single+site+in+powershell
+# Thanks to Joshua Gong for ideas and syntax for the PrepareEnableCustomErrors function and its usage.
 
 # call a separate script to init global variables.
-## Invoke-Expression ($PSScriptRoot + '\..\WebHub_Appliance_PS\Initialize.ps1')  # loops back and uses custom values
+Invoke-Expression ($PSScriptRoot + '\..\WebHub_Appliance_PS\Initialize.ps1')  # loops back and uses custom values
 
 # Alert! Using Response-Mode FILE rather than URL is necessary if you want the status code and headers to arrive at the http client
 
+
+# First, make latest version of shared functions available
 Import-Module WebAdministration
 
-$AWebSiteName = "WebHub Demos"
-$InfoMsg = '"CustomHttpErrors" "WebHubDemos 500.12"'
-## Start-Process $Global:CSConsole -ArgumentList $InfoMsg -NoNewWindow 
-echo $InfoMsg
+if (! (Test-Path ($env:USERPROFILE + '\Documents\WindowsPowerShell\Modules\Mod_IIS_Setup'))) { 
+	mkdir ($env:USERPROFILE + '\Documents\WindowsPowerShell\Modules\Mod_IIS_Setup')
+}
+copy ($PSScriptRoot + '\..\WebHub_Appliance_PS\Mod_IIS_Setup.psm1')   $env:USERPROFILE\Documents\WindowsPowerShell\Modules\Mod_IIS_Setup\Mod_IIS_Setup.psm1
+Import-Module Mod_IIS_Setup
 
-Add-WebConfiguration -Filter system.webserver/httpErrors -location $AWebSiteName -Value @{StatusCode=500; substatuscode=12;  Path="D:\Projects\webhubdemos\Live\WebHub\whmsg\whdemos_500_12_starting.html"; ResponseMode="File"} 
+New-Variable -Name AWebSiteName  -Value "WebHub Lite Demos" -Option private
+New-Variable -Name ErrorFilePath -Value "D:\Projects\webhubdemos\Live\WebHub\whmsg\" -Option private
 
-$InfoMsg = '"CustomHttpErrors" "WebHubDemos 500.13"'
-## Start-Process $Global:CSConsole -ArgumentList $InfoMsg -NoNewWindow 
-echo $InfoMsg
+# Enable then configure custom errors for a web site
+PrepareEnableCustomErrors $AWebSiteName        ($ErrorFilePath + "whdemos_IISDefault-Custom.html")
+ConfigureCustomError      $AWebSiteName 500 12 ($ErrorFilePath + "whdemos_500_12_starting.html")
+ConfigureCustomError      $AWebSiteName 500 13 ($ErrorFilePath + "whdemos_500_13_busy.html")
+ConfigureCustomError      $AWebSiteName 400 4  ($ErrorFilePath + "whdemos_404_4_appnotdefined.html")
 
-Add-WebConfiguration -Filter system.webserver/httpErrors -location $AWebSiteName -Value @{StatusCode=500; substatuscode=13;  Path="D:\Projects\webhubdemos\Live\WebHub\whmsg\whdemos_500_13_busy.html"; ResponseMode="File"} 
+$AWebSiteName = "WebHub More Demos"
+PrepareEnableCustomErrors $AWebSiteName        ($ErrorFilePath + "whdemos_IISDefault-Custom.html")
+ConfigureCustomError      $AWebSiteName 500 12 ($ErrorFilePath + "whdemos_500_12_starting.html")
+ConfigureCustomError      $AWebSiteName 500 13 ($ErrorFilePath + "whdemos_500_13_busy.html")
+ConfigureCustomError      $AWebSiteName 400 4  ($ErrorFilePath + "whdemos_404_4_appnotdefined.html")
 
-$InfoMsg = '"CustomHttpErrors" "WebHubDemos 400.4 for AppNotDefined"'
-## Start-Process $Global:CSConsole -ArgumentList $InfoMsg -NoNewWindow 
-echo $InfoMsg
-
-Add-WebConfiguration -Filter system.webserver/httpErrors -location $AWebSiteName -Value @{StatusCode=400; substatuscode=4;  Path="D:\Projects\webhubdemos\Live\WebHub\whmsg\whdemos_404_4_appnotdefined.html"; ResponseMode="File"} 
+Remove-Variable AWebSiteName
+Remove-Variable ErrorFilePath
 
