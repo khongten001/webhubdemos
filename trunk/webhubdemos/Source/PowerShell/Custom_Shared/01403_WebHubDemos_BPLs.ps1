11ec2b3f074372a75e028bc81398412ca4627908
annlynnworth <annlynnworth@88ca0d87-9d1d-4ddd-a6d8-5c97b49fb611>
2016-05-22 05:59:39 +0000

    Delphi Redist BPL files

---
+++ trunk/webhubdemos/Source/PowerShell/Custom_Shared/01403_WebHubDemos_BPLs.ps1
@@ -0,0 +1,80 @@
+# Delphi Redist files for WebHubDemos
+# Copyright 2014-2016 HREF Tools Corp. 
+# Creative Commons license - keep credits intact.
+
+# call a separate script to init global variables.
+Invoke-Expression ($PSScriptRoot + "\Initialize.ps1")
+
+$InfoMsg = ('/note "Delphi Redist Files for WebHubDemos"')
+Start-Process $Global:CSConsole -ArgumentList $InfoMsg -NoNewWindow 
+
+{
+
+# 64-bit files
+
+	$source = "https://archiveinstallers.s3.amazonaws.com/win64/D24_redist_win64.7z"
+	$destination = ($Global:FolderInstallers + "D24_redist_win64.7z")
+
+	$InfoMsg = ('Downloading ' + $source)
+	Start-Process $Global:CSConsole -ArgumentList $InfoMsg -NoNewWindow 
+	echo $InfoMsg
+	Invoke-WebRequest $source -OutFile $destination
+	if (! $?) { 
+		Start-Process $Global:CSConsole -ArgumentList ('/Error "Download D24_redist_win64.7z; Exit code ' + $LastExitCode.ToString + '"')  -NoNewWindow -Wait 
+	} 
+
+	$InfoMsg = ('Unzipping ' + $destination)
+	Start-Process $Global:CSConsole -ArgumentList $InfoMsg -NoNewWindow 
+	echo $InfoMsg
+
+	Set-Location "D:\Projects\WebHubDemos\Live\Library64"
+	Start-Process "d:\Apps\Utilities\7Zip\7z.exe" -ArgumentList ("x " + $destination) -NoNewWindow -Wait
+	ls "D:\Projects\WebHubDemos\Live\Library64\vcl*.bpl"
+
+	$source = "http://data.rubicon.href.com/FirebirdSQL_Client/fbclient_win64.dll"
+	$destination = "D:\Projects\WebHubDemos\Live\Library64\fbclient_win64.dll"
+	$InfoMsg = ('Downloading ' + $source)
+	Start-Process $Global:CSConsole -ArgumentList $InfoMsg -NoNewWindow 
+	echo $InfoMsg
+	Invoke-WebRequest $source -OutFile $destination
+	if (! $?) { 
+		Start-Process $Global:CSConsole -ArgumentList ('/Error "Download fbclient_win64.dll; Exit code ' + $LastExitCode.ToString + '"')  -NoNewWindow -Wait 
+	}
+
+
+
+# 32-bit files
+
+	$source = "http://archiveinstallers.s3.amazonaws.com/win32/D22_redist_upd1_win32.7z"
+	$destination = ($Global:FolderInstallers + "D22_redist_upd1_win32.7z")
+
+	$InfoMsg = ('Downloading ' + $source)
+	Start-Process $Global:CSConsole -ArgumentList $InfoMsg -NoNewWindow 
+	echo $InfoMsg
+	Invoke-WebRequest $source -OutFile $destination
+	if (! $?) { 
+		Start-Process $Global:CSConsole -ArgumentList ('/Error "Download D22_redist_upd1_win32.7z; Exit code ' + $LastExitCode.ToString + '"')  -NoNewWindow -Wait 
+	} 
+
+	$InfoMsg = ('Unzipping ' + $destination)
+	Start-Process $Global:CSConsole -ArgumentList $InfoMsg -NoNewWindow 
+	echo $InfoMsg
+
+	Set-Location "D:\Projects\WebHubDemos\Live\Library"
+	Start-Process "d:\Apps\Utilities\7Zip\7z.exe" -ArgumentList ("x " + $destination) -NoNewWindow -Wait
+	ls "D:\Projects\WebHubDemos\Live\Library\vcl*.bpl"
+
+	$source = "http://data.rubicon.href.com/FirebirdSQL_Client/fbclient_win32.dll"
+	$destination = "D:\Projects\WebHubDemos\Live\Library\fbclient_win32.dll"
+	$InfoMsg = ('Downloading ' + $source)
+	Start-Process $Global:CSConsole -ArgumentList $InfoMsg -NoNewWindow 
+	echo $InfoMsg
+	Invoke-WebRequest $source -OutFile $destination
+	if (! $?) { 
+		Start-Process $Global:CSConsole -ArgumentList ('/Error "Download fbclient_win32.dll; Exit code ' + $LastExitCode.ToString + '"')  -NoNewWindow -Wait 
+	} 
+	else {
+		Copy "D:\Projects\WebHubDemos\Live\Library\fbclient_win32.dll" "D:\Projects\WebHubDemos\Live\Library\fbclient.dll"
+	}
+
+}
