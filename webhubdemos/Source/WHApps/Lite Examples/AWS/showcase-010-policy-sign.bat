ce2fbc7f29c92e27d76b26d69183b1106b474160
annlynnworth <annlynnworth@88ca0d87-9d1d-4ddd-a6d8-5c97b49fb611>
2016-06-01 12:13:35 +0000

    steps

--- webhubdemos/Source/WHApps/Lite Examples/AWS/showcase-010-policy-sign.bat
+++ webhubdemos/Source/WHApps/Lite Examples/AWS/showcase-010-policy-sign.bat
@@ -4,18 +4,22 @@ set opensslpath=D:\Apps\OpenSSL\OpenSSL-Win64\bin\
 
 set   inputfilespec=showcase-003-upload.policy.viaopenssl.base64.txt
 set outputfilespec1=showcase-020-upload.policy.viaopenssl.signed.txt
-set outputfilespec2=showcase-021-upload.policy.viaopenssl.signed.base64.txt
 
 set /P secretkey=Enter secret key :
 
 CD /D %~dp0
 
-type %filespec% | %opensslpath%openssl.exe dgst -hmac %secretkey% -sha1 > %outputfilespec1% 
+del %outputfilespec1%
 
-type %filespec% | %opensslpath%openssl.exe dgst -hmac %secretkey% -sha1 | %opensslpath%openssl.exe base64 -A > %outputfilespec2% 
+type %inputfilespec% | %opensslpath%openssl.exe dgst -hmac %secretkey% -sha1 -out %outputfilespec1% 
 
-dir showcase-02?-upload.policy.viaopenssl.signed*
-
-type %outputfilespec2%
 pause
+type %outputfilespec1%
+
+copy %outputfilespec1% showcase-021-upload.policy.viaopenssl.signed-cleaned.txt
+@echo off
+echo Use Notepad to leave just the answer in the cleaned file...
+pause
+
+notepad showcase-021-upload.policy.viaopenssl.signed-cleaned.txt
 
