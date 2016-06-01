c12fccc7282bff1dd32a818dd3dd2b6cbbe52496
annlynnworth <annlynnworth@88ca0d87-9d1d-4ddd-a6d8-5c97b49fb611>
2016-06-01 13:57:10 +0000

    openssl syntax works to make AWS signature v2 on Windows

--- trunk/webhubdemos/Source/WHApps/Lite Examples/AWS/showcase-010-policy-sign.bat
+++ trunk/webhubdemos/Source/WHApps/Lite Examples/AWS/showcase-010-policy-sign.bat
@@ -2,21 +2,19 @@ setlocal
 
 set opensslpath=D:\Apps\OpenSSL\OpenSSL-Win64\bin\
 
-set   inputfilespec=showcase-003-upload.policy.viaopenssl.base64.txt
-
-set outputfilespec2=showcase-023-upload.policy.viaopenssl.signed.base64.txt
+set  inputfilespec=showcase-003-upload.policy.viaopenssl.base64.txt
+set outputfilespec=showcase-011-upload.policy.viaopenssl.signed.base64.txt
 
 set /P secretkey=Enter secret key :
 
 CD /D %~dp0
 
-@del %outputfilespec2%
+@del %outputfilespec%
 
-%opensslpath%openssl.exe dgst -sha1 -hmac "%secretkey%" -binary %inputfilespec% | %opensslpath%openssl.exe base64 -A > %outputfilespec2% 
+%opensslpath%openssl.exe dgst -sha1 -hmac "%secretkey%" -binary %inputfilespec% | %opensslpath%openssl.exe base64 -A > %outputfilespec% 
 
 if errorlevel 1 pause
-type %outputfilespec2%
+type %outputfilespec%
 
 pause
-
 
