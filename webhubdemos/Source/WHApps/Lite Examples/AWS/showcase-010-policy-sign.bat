87042ef86796e2216e4f29062a388f5e10009d1e
annlynnworth <annlynnworth@88ca0d87-9d1d-4ddd-a6d8-5c97b49fb611>
2016-06-01 11:41:52 +0000

    -A no line breaks in the base64 output

--- webhubdemos/Source/WHApps/Lite Examples/AWS/showcase-010-policy-sign.bat
+++ webhubdemos/Source/WHApps/Lite Examples/AWS/showcase-010-policy-sign.bat
@@ -3,8 +3,8 @@ setlocal
 set opensslpath=D:\Apps\OpenSSL\OpenSSL-Win64\bin\
 
 set   inputfilespec=showcase-003-upload.policy.viaopenssl.base64.txt
-set outputfilespec1=showcase-020.upload.policy.viaopenssl.signed.txt
-set outputfilespec2=showcase-021.upload.policy.viaopenssl.signed.base64.txt
+set outputfilespec1=showcase-020-upload.policy.viaopenssl.signed.txt
+set outputfilespec2=showcase-021-upload.policy.viaopenssl.signed.base64.txt
 
 set /P secretkey=Enter secret key :
 
@@ -12,7 +12,7 @@ CD /D %~dp0
 
 type %filespec% | %opensslpath%openssl.exe dgst -hmac %secretkey% -sha1 > %outputfilespec1% 
 
-type %filespec% | %opensslpath%openssl.exe dgst -hmac %secretkey% -sha1 | %opensslpath%openssl.exe base64 > %outputfilespec2% 
+type %filespec% | %opensslpath%openssl.exe dgst -hmac %secretkey% -sha1 | %opensslpath%openssl.exe base64 -A > %outputfilespec2% 
 
 dir showcase-02?-upload.policy.viaopenssl.signed*
 
