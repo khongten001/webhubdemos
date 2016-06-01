39313c0a1e1b96dc1971acb040026c5cbb245668
annlynnworth <annlynnworth@88ca0d87-9d1d-4ddd-a6d8-5c97b49fb611>
2016-06-01 13:52:00 +0000

    binary then base64

--- trunk/webhubdemos/Source/WHApps/Lite Examples/AWS/showcase-010-policy-sign.bat
+++ trunk/webhubdemos/Source/WHApps/Lite Examples/AWS/showcase-010-policy-sign.bat
@@ -3,23 +3,20 @@ setlocal
 set opensslpath=D:\Apps\OpenSSL\OpenSSL-Win64\bin\
 
 set   inputfilespec=showcase-003-upload.policy.viaopenssl.base64.txt
-set outputfilespec1=showcase-020-upload.policy.viaopenssl.signed.txt
+
+set outputfilespec2=showcase-023-upload.policy.viaopenssl.signed.base64.txt
 
 set /P secretkey=Enter secret key :
 
 CD /D %~dp0
 
-del %outputfilespec1%
+@del %outputfilespec2%
 
-type %inputfilespec% | %opensslpath%openssl.exe dgst -hmac %secretkey% -sha1 -out %outputfilespec1% 
+%opensslpath%openssl.exe dgst -sha1 -hmac "%secretkey%" -binary %inputfilespec% | %opensslpath%openssl.exe base64 -A > %outputfilespec2% 
+
+if errorlevel 1 pause
+type %outputfilespec2%
 
 pause
-type %outputfilespec1%
 
-copy %outputfilespec1% showcase-021-upload.policy.viaopenssl.signed-cleaned.txt
-@echo off
-echo Use Notepad to leave just the answer in the cleaned file...
-pause
-
-notepad showcase-021-upload.policy.viaopenssl.signed-cleaned.txt
 
