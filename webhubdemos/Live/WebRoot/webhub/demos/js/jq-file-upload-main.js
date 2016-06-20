75e90ab914179240ab4d2297e59ce3dfa9cf76f3
annlynnworth <annlynnworth@88ca0d87-9d1d-4ddd-a6d8-5c97b49fb611>
2016-06-20 05:28:13 +0000

    adjustments for use with WebHub showcase demo; work in progress (Harman)

--- webhubdemos/Live/WebRoot/webhub/demos/js/jq-file-upload-main.js
+++ webhubdemos/Live/WebRoot/webhub/demos/js/jq-file-upload-main.js
@@ -8,70 +8,51 @@
  * Licensed under the MIT license:
  * http://www.opensource.org/licenses/MIT
  */
-
 /* global $, window */
-
-$(function () {
-    'use strict';
-
-    // Initialize the jQuery File Upload widget:
-    $('#fileupload').fileupload({
-        // Uncomment the following to send cross-domain cookies:
-        //xhrFields: {withCredentials: true},
-        forceIframeTransport: true,
-        maxFileSize: 999000,
-        acceptFileTypes: /(\.|\/)(gif|jpe?g|png)$/i
-    });
-
-    // Enable iframe cross-domain access via redirect option:
-    $('#fileupload').fileupload(
-        'option',
-        'redirect',
-        window.location.href.replace(
-            /\/[^\/]*$/,
-            '/cors/result.html?%s'
-        )
-    );
-
-    if (window.location.hostname !== 'blueimp.github.io') {
-        // Demo settings:
-        $('#fileupload').fileupload('option', {
-            url: '//jquery-file-upload.appspot.com/',
-            // Enable image resizing, except for Android and Opera,
-            // which actually support image resizing, but fail to
-            // send Blob objects via XHR requests:
-            disableImageResize: /Android(?!.*Chrome)|Opera/
-                .test(window.navigator.userAgent),
-            maxFileSize: 999000,
-            acceptFileTypes: /(\.|\/)(gif|jpe?g|png)$/i
-        });
-        // Upload server status check for browsers with CORS support:
-        if ($.support.cors) {
-            $.ajax({
-                url: '//jquery-file-upload.appspot.com/',
-                type: 'HEAD'
-            }).fail(function () {
-                $('<div class="alert alert-danger"/>')
-                    .text('Upload server currently unavailable - ' +
-                            new Date())
-                    .appendTo('#fileupload');
-            });
-        }
-    } else {
-        // Load existing files:
-        $('#fileupload').addClass('fileupload-processing');
-        $.ajax({
-            // Uncomment the following to send cross-domain cookies:
-            //xhrFields: {withCredentials: true},
-            url: $('#fileupload').fileupload('option', 'url'),
-            dataType: 'json',
-            context: $('#fileupload')[0]
-        }).always(function () {
-            $(this).removeClass('fileupload-processing');
-        }).done(function (result) {
-            $(this).fileupload('option', 'done')
-                .call(this, $.Event('done'), {result: result});
-        });
-    }
-
+$(function() {
+        'use strict';
+        var e = $("#fileupload");
+        e.fileupload({
+            url: e.attr("action"),
+            type: "POST",
+            autoUpload: !0,
+            dataType: "json",
+            add: function(t, n) {
+                console.log(n);
+                $.ajax({
+                    url: file_upload_options.sign_url,
+                    type: "POST",
+                    dataType: "json",
+                    data: {
+                        fname: n.files[0].name,
+                        ftype: n.files[0].type,
+                        fsize: n.files[0].size,
+                        flastmodified: n.files[0].lastModified
+                    },
+                    success: function(t) {
+                        e.find("input[name=key]").val(t.key), e.find("input[name=policy]").val(t.policy), e.find("input[name=signature]").val(t.signature)
+                    }
+                }), n.submit()
+            },
+            send: function(e, t) {
+                
+                $(".progress").fadeIn()
+            },
+            progress: function(e, t) {
+                var n = Math.round(e.loaded / e.total * 100);
+                $(".bar").css("width", n + "%")
+            },
+            fail: function(e, t) {
+                console.log("fail")
+            },
+            success: function(e) {
+                var t = decodeURIComponent($(e).find("Location").text());
+                $("#photo_file").val(t)
+            },
+            done: function(e, t) {
+                $(".progress").fadeOut(300, function() {
+                    $(".bar").css("width", 0)
+                })
+            }
+        })
 });
