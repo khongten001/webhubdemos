3b0d7643056597a4c7e9e009873d9cbef9a6a94c
harman <harman@88ca0d87-9d1d-4ddd-a6d8-5c97b49fb611>
2016-06-29 13:11:49 +0000

    UI interface change , Validation Methods modification , Add new Separate Css file, Code cleanup.

--- trunk/webhubdemos/Live/WebRoot/webhub/demos/js/jq-file-upload-main.js
+++ trunk/webhubdemos/Live/WebRoot/webhub/demos/js/jq-file-upload-main.js
@@ -1,173 +1,155 @@
-/*
- * jQuery File Upload Plugin JS Example
- * https://github.com/blueimp/jQuery-File-Upload
- *
- * Copyright 2010, Sebastian Tschan
- * https://blueimp.net
- *
- * Licensed under the MIT license:
- * http://www.opensource.org/licenses/MIT
- */
-/* global $, window */
-
-/*$(function () {
-    'use strict';
-
-    // Initialize the jQuery File Upload widget:
-    $('#fileupload').fileupload({
-        // Uncomment the following to send cross-domain cookies:
-        //xhrFields: {withCredentials: true},
-        url: 'server/php/'
-    });
-
-    // Enable iframe cross-domain access via redirect option:
-    $('#fileupload').fileupload(
-        'option',
-        'redirect',
-        window.location.href.replace(
-            /\/[^\/]*$/,
-            '/cors/result.html?%s'
-        )
-    );
-
-        // Demo settings:
-        $('#fileupload').fileupload('option', {
-            url: '//jquery-file-upload.appspot.com/',
-            // Enable image resizing, except for Android and Opera,
-            // which actually support image resizing, but fail to
-            // send Blob objects via XHR requests:
-            disableImageResize: /Android(?!.*Chrome)|Opera/
-                .test(window.navigator.userAgent),
-            maxFileSize: 999000,
-            acceptFileTypes: /(\.|\/)(gif|jpe?g|png)$/i
-        });
-        // Upload server status check for browsers with CORS support:
-      
-    } else {
-        // Load existing files:
-        $('#fileupload').addClass('fileupload-processing');
-        $.ajax({
-            // Uncomment the following to send cross-domain cookies:
-            //xhrFields: {withCredentials: true},
-            url: $('#fileupload').fileupload('option', 'url'),
-            dataType: 'json',
-            context: $('#fileupload')[0]
-        }).always(function () {
-            $(this).removeClass('fileupload-processing');
-        }).done(function (result) {
-            $(this).fileupload('option', 'done')
-                .call(this, $.Event('done'), {result: result});
-        });
-    }
-
-});*/
-
-
 $(function() {
         'use strict';
         // Initialize the jQuery File Upload widget:
         var form= $("#fileupload");
+        var maxFileSizeCustom =  400*1024; //400kb
 
-        
          form.fileupload({
             dropZone: $('#dropzone'),
-            acceptFileTypes: /(\.|\/)(gif|jpe?g|png|mp3|mp4)$/i,
-            maxFileSize: 400000, //400kbytes //400000
+           /* acceptFileTypes: /(\.|\/)(gif|jpe?g|png|mp3|mp4)$/i,*/
+           /* maxFileSize: 4000, //400kbytes //400000 //*/
             url: form.attr("action"),
             type: "POST",
-            autoUpload: false,
             dataType: "XML",
-            sequentialUploads: true,
-/*            add: function(t, n) {
-                console.log(n.files);
-                var localFilesArr       = new Array();
-                var jsonArg1            = new Object();
-                jsonArg1.fname          = n.files[0].name,
-                jsonArg1.ftype          = n.files[0].type,
-                jsonArg1.fsize          = n.files[0].size,
-                jsonArg1.flastmodified  =n.files[0].lastModified
-
-                $.ajax({
-                    url: file_upload_options.sign_url,
-                    type: "POST",
-                    dataType: "json",
-                    data: {
-                       fileDetails: JSON.stringify(jsonArg1)
-                    },
-                    success: function(data) {
-                        console.log(data);
-                        var fileName = data.ShowcaseResponse.ConfirmedFname;
-                        var ResponseType = data.ShowcaseResponse.ResponseType;
-                        var SignData = data.ShowcaseResponse.SignData;
-                        if(ResponseType == "OK"){
-                            var key = SignData.awsResource;
-                            var policy = SignData.awsPolicy64;
-                            var signature = SignData.awsUploadSignature;
-                            var awsDownloadURL = SignData.awsDownloadURL;
-                            e.find("input[name=key]").val(key), e.find("input[name=policy]").val(policy), e.find("input[name=signature]").val(signature)
-                        }
-                        
-                    }
-                }),n.submit() 
-            },*/
-            send: function(e, t) {
-                
-                $(".progress").fadeIn()
-            },
-            progress: function(e, t) {
-                var n = Math.round(e.loaded / e.total * 100);
-                $(".bar").css("width", n + "%")
-            },
-            fail: function(e, t) {
-              /*  console.log(t.context);*/
-                var ErrMessage  = $(t.jqXHR.responseXML).find("Message").text();
-                if(ErrMessage){
-                    t.context.find('strong.error').html("Error : "+ ErrMessage);
+            uploadTemplateId: null,
+            downloadTemplateId: null,
+            add: function(e, data) {
+                $(".fileupload-progress").fadeIn();
+                 //console.log(data.files);
+                //Validations will be here 
+                if(data.files[0].size > maxFileSizeCustom){
+                    var ErrMessage = "File is too large";
+                    addShowErr(data.files[0].name+ ", " + ErrMessage +", "+ formatFileSize_jqupload(data.files[0].size));                  
                 }else{
-                   t.context.find('strong.error').html("Error : "+ file.error); 
-                }
-                 
-                 //t.context.html(ErrMessage); 
+                     if(file_upload_options.allow_start_action==true){
+                            var jsonArg1            = new Object();
+                            jsonArg1.fname          = data.files[0].name,
+                            jsonArg1.ftype          = data.files[0].type,
+                            jsonArg1.fsize          = data.files[0].size,
+                            jsonArg1.flastmodified  =data.files[0].lastModified
+                            $.ajax({
+                                url: file_upload_options.upload_start_action,
+                                type: "POST",
+                                dataType: "json",
+                                data: {
+                                   fileDetails: JSON.stringify(jsonArg1)
+                                },
+                                success: function(data) {
+                                    console.log(data);
+                                    var fileName = data.ShowcaseResponse.ConfirmedFname;
+                                    var ResponseType = data.ShowcaseResponse.ResponseType;
+                                    var SignData = data.ShowcaseResponse.SignData;
+                                    if(ResponseType == "OK"){
+                                        console.log("start Upload Action Ajax Complete");
+                                    }
+                                    
+                                }
+                            }),data.submit();
+                    }else{
+                        data.submit();
+                    }
+                }//End Validation 
+                
             },
-            success: function(e,t) {
-               
-               //if t=="nocontent" upload complete :) 
+            send: function(e, data) { },
+            success: function(e,t) { },
+            progressall: function(e, data) {
+                var n = Math.round(data.loaded / data.total * 100);
+                $(".fileupload-progress .progress").css("width", n + "%");
+               // console.log(n + "%");
+            },
+            fail: function(e, data) {
+                if(file_upload_options.allow_fail_action==true){
+                    var jsonArg1            = new Object();
+                    jsonArg1.fname          = data.files[0].name,
+                    jsonArg1.ftype          = data.files[0].type,
+                    jsonArg1.fsize          = data.files[0].size,
+                    jsonArg1.flastmodified  =data.files[0].lastModified
 
-                /*var t = decodeURIComponent($(e).find("Location").text());
-                   $.ajax({
-                        url: file_upload_options.sign_url,
+                    $.ajax({
+                        url: file_upload_options.upload_fail_action,
                         type: "POST",
                         dataType: "json",
                         data: {
                            fileDetails: JSON.stringify(jsonArg1)
                         },
                         success: function(data) {
+                            console.log(data);
                             var fileName = data.ShowcaseResponse.ConfirmedFname;
                             var ResponseType = data.ShowcaseResponse.ResponseType;
                             var SignData = data.ShowcaseResponse.SignData;
                             if(ResponseType == "OK"){
-                                var key = SignData.awsResource;
-                                var policy = SignData.awsPolicy64;
-                                var signature = SignData.awsUploadSignature;
-                                var awsDownloadURL = SignData.awsDownloadURL;
-                                e.find("input[name=key]").val(key), e.find("input[name=policy]").val(policy), e.find("input[name=signature]").val(signature)
+                                console.log("Upload Fail Action Ajax Complete");
                             }
                             
                         }
-                    });*/
+                    });
+                }
+
+                var ErrMessage  = $(data.jqXHR.responseXML).find("Message").text();
+                if(ErrMessage){
+                    addShowErr(data.files[0].name+ " " + ErrMessage);
+                }
             },
-            done: function(e, t) {
+            
+            done: function(e, data) {
 
-                $(".progress").fadeOut(300, function() {
-                    $(".bar").css("width", 0)
-                })
+                var that_data = data;
 
+                var jsonArg1            = new Object();
+                jsonArg1.fname          = data.files[0].name,
+                jsonArg1.ftype          = data.files[0].type,
+                jsonArg1.fsize          = data.files[0].size,
+                jsonArg1.flastmodified  =data.files[0].lastModified
+
+                if(file_upload_options.allow_complete_action==true){
+                    //Add Upload complete Call back 
+                    $.ajax({
+                        url: file_upload_options.upload_complete_action,
+                        type: "POST",
+                        dataType: "json",
+                        data: {
+                           fileDetails: JSON.stringify(jsonArg1)
+                        },
+                        success: function(dataWH) {
+                            console.log(dataWH);
+                            var ResponseType = dataWH.ShowcaseResponse.ResponseType;
+                            if(ResponseType == "OK"){
+                                //Build File List
+                                addFileToList(that_data);
+                            } //if(ResponseType == "OK")
+                        }, //success
+                        fail:function(e, t){
+                            addShowErr("WH Upload Complete CallBack Failed!");
+                        }
+                    }); //$.ajax
+                }else{ //file_upload_options.allow_complete_action
+                     addFileToList(that_data);
+                }
+                
+            },
+            stop: function(e,data){
+                console.log("All upload Complete");
+                 setTimeout(function(){
+                     $(".fileupload-progress").fadeOut(300, function() {
+                        $(".fileupload-progress .progress").css("width", 0)
+                    });
+                },1500);
+                setTimeout(function(){
+                    $('.fileUploadErrMsgs').html('');
+                    $('.fileUploadErrMsgs').hide('fast');
+                },4500);
             }
         });
 
-        form.bind('fileuploadsubmit', function (e, n) {
-               var file_type = n.files[0].type;
-               n.formData = {
+        /*  Additional Form Data  Required : Note if you trying to send
+            data by using this method you have to also 
+            add your input fields here , because this method ignore 
+            the input fields data inside the form - (harman)
+        */ 
+        form.bind('fileuploadsubmit', function (e, data) {
+               var file_type = data.files[0].type;
+               data.formData = {
                     'Content-Type': file_type,
                     'key': form.find("input[name=key]").val(),
                     'acl': form.find("input[name=acl]").val(),
@@ -176,28 +158,6 @@ $(function() {
                     'policy': form.find("input[name=policy]").val(),
                     'signature': form.find("input[name=signature]").val(),
                 };
-        });
-       form.bind('fileuploadadded', function (e, n) {
-              //var file_type = n.files[0].type;
-               //e.find("input[name=Content-Type]").val(file_type);
-             /*var localFilesArr       = new Array();
-                var jsonArg1            = new Object();
-                jsonArg1.fname          = n.files[0].name;
-                jsonArg1.ftype          = n.files[0].type;
-                jsonArg1.fsize          = n.files[0].size;
-                jsonArg1.flastmodified  =n.files[0].lastModified;
-
-                $.ajax({
-                    url: file_upload_options.sign_url,
-                    type: "POST",
-                    dataType: "json",
-                    data: {
-                       fileDetails: JSON.stringify(jsonArg1)
-                    },
-                    success: function(t) {
-                        form.find("input[name=key]").val(t.key), e.find("input[name=policy]").val(t.policy), e.find("input[name=signature]").val(t.signature)
-                    }
-                })*/
         });
 
         //DropZone setup 
@@ -231,4 +191,103 @@ $(function() {
         $(document).bind('drop dragover', function (e) {
             e.preventDefault();
         });
+
 });
+//Render Table row on success 
+function addFileToList(data){
+    if(typeof data.files[0] !=="undefined"){
+          var rows = $();
+          $.each(data.files, function (index, file) {
+           // console.log(file);
+            var row = $('<tr class="template-download fade">' +
+                            '<td class="nameTD"><p class="fname"></p></td>' +
+                            '<td class="extTD"><span class="fext"></span></td>' +
+                            '<td class="sizeTD"><span class="fsize"></span></td>' +
+                            '<td class="downloadTD"><a title="Test download of your file from AWS CloudFront" data-file="" class="fdownload" onClick="downloadAjaxFile(this);"><span>&#x1f4be;</span></a></td>' +
+                            '' +
+                         '</tr>');
+                row.find('.fsize').text(formatFileSize_jqupload(file.size));
+                row.find('.fname').append(file.name);
+                row.find('.fext').append("."+file.name.split('.').pop());
+                row.find('.fdownload').attr('data-file',file.name);
+                rows = rows.add(row);
+        });
+        $('table.fileUploadList tbody.files').append(rows);
+    }
+}
+function addShowErr(msg){
+    $('.fileUploadErrMsgs').append("<p>Error : " + msg +"</p>");
+    $('.fileUploadErrMsgs').fadeIn("slow");
+    setTimeout(function(){
+        $('.fileUploadErrMsgs').fadeOut("slow");
+    },10000);
+}
+ function downloadAjaxFile(aTagObj){
+    var filename = $(aTagObj).attr('data-file');
+    filename = (escapeJQFilename(filename));
+    var jsonArg1            = new Object();
+        jsonArg1.fname          = filename;
+     $.ajax({
+        url: file_upload_options.download_url,
+        type: "POST",
+        dataType: "json",
+        data: {
+           fileDetails: JSON.stringify(jsonArg1)
+        },
+        success: function(data) {
+            console.log("WH AJAX DATA (File Download Url) ");
+            console.log(data);
+            var fileName = data.ShowcaseResponse.ConfirmedFname;
+            var ResponseType = data.ShowcaseResponse.ResponseType;
+            var SignData = data.ShowcaseResponse.SignData;
+            if(ResponseType == "OK"){
+                var key = SignData.awsResource;
+                var policy = SignData.awsPolicy64;
+                var signature = SignData.awsUploadSignature;
+                var awsDownloadURL = SignData.awsDownloadURL;
+                console.log("Download Url : " + awsDownloadURL);
+               download_file_url(awsDownloadURL);
+            }
+            
+        }
+  });
+}
+
+
+function download_file_url(awsDownloadURL){
+    if(window.VBArray){
+        //alert('1');
+        var alink = document.createElement('a');
+        alink.href = awsDownloadURL;
+        alink.target = '_Blank';
+        document.body.appendChild(alink);
+        alink.click();
+    }else{
+        window.location.href = awsDownloadURL;
+    }
+}
+function escapeJQFilename(filename) {
+    var temp = filename;
+    temp = temp.replace('/','_');
+    temp = temp.replace('\\','_');
+    temp = temp.replace(' ','_');
+    temp = temp.replace('#','_');
+    temp = temp.replace('%','_');
+    temp = temp.replace('?','_');
+    temp = temp.replace('&','_');
+    temp = temp.replace('<','_');
+    temp = temp.replace('>','_');
+    return temp;
+}
+function formatFileSize_jqupload(bytes) {
+    if (typeof bytes !== 'number') {
+        return '';
+    }
+    if (bytes >= 1000000000) {
+        return (bytes / 1000000000).toFixed(2) + ' GB';
+    }
+    if (bytes >= 1000000) {
+        return (bytes / 1000000).toFixed(2) + ' MB';
+    }
+    return (bytes / 1000).toFixed(2) + ' KB';
+} 
