348e8c0736c46f63879387e0e14683ececb4bd7a
annlynnworth <annlynnworth@88ca0d87-9d1d-4ddd-a6d8-5c97b49fb611>
2016-07-08 01:55:38 +0000

    CloudFront delivery to EC2 machines

--- webhubdemos/Source/PowerShell/Custom_Shared/04000_WebHubRuntime_Upgrade.ps1
+++ webhubdemos/Source/PowerShell/Custom_Shared/04000_WebHubRuntime_Upgrade.ps1
@@ -43,13 +43,18 @@ if ($Global:FlagInstallWebHubRuntime) {
 	if (Test-Path $filespec) { Del $filespec }
 
 	if ($Global:ZMGlobalContext -eq 'DORIS') {
-	
 		# The Setup binary 
-		$source = "http://edeliver.href.com/WebHub/WebHub_Central/WebHub_X_Runtime_System_v3.259_Setup.exe?Policy=eyJTdGF0ZW1lbnQiOlt7IlJlc291cmNlIjoiaHR0cCo6Ly9lZGVsaXZlci5ocmVmLmNvbS9XZWJIdWIvKiIsIkNvbmRpdGlvbiI6eyJJcEFkZHJlc3MiOnsiQVdTOlNvdXJjZUlwIjoiNTIuOTAuODguMC8zMiJ9LCJEYXRlTGVzc1RoYW4iOnsiQVdTOkVwb2NoVGltZSI6MTU4NTcwMjkyM319fV19&Signature=s6JTUeGNRz01IJ1jSkDogU5uIEOb2U7j7ClxnIlnADi5mbH6bdKCmkb56XFALSzpLbSws3laijXFyN9smTd3Fy0LUl4AZqg98QQDXU8gAdCEyULfWuF0uIZ7S7~EMTvRACi57z-2wPoFJKpVnKB1pybi1DnskCauQyWJqoIqljvqEcUuvvwyVyclNToRaXAZB0M2~Wdj7g5mDr5Yih0hUdAPx6Nsp8BXtFI0xBELV3RqjUHp3mjsbQRWyCXG5vcEd-6aZ-pw-0cCEKr7Pd2tXO2jDjbwObuO7kYoEhHQH15rhZn~JTOR1M2v~gOKHqFiRo74Md2RcD21cTsbSbWFsA__&Key-Pair-Id=APKAIGAY3EJC77HVGRFQ"
+		$source = "https://edeliver.href.com/WebHub/WebHub_Central/WebHub_X_Runtime_System_v3.259_Setup.exe?Policy=eyJTdGF0ZW1lbnQiOlt7IlJlc291cmNlIjoiaHR0cHM6Ly9lZGVsaXZlci5ocmVmLmNvbS9XZWJIdWIvV2ViSHViX0NlbnRyYWwvV2ViSHViX1hfUnVudGltZV9TeXN0ZW1fdjMuMjU5X1NldHVwLmV4ZSIsIkNvbmRpdGlvbiI6eyJJcEFkZHJlc3MiOnsiQVdTOlNvdXJjZUlwIjoiNTIuOTAuODguMC8zMiJ9LCJEYXRlR3JlYXRlclRoYW4iOnsiQVdTOkVwb2NoVGltZSI6MTQ2Nzk0MjMyMH0sIkRhdGVMZXNzVGhhbiI6eyJBV1M6RXBvY2hUaW1lIjoxNDk5NDc4NjIwfX19XX0_&Signature=qHbYg-pBemIyYT9xRtnEctOR5kVvnYhxq7ZuhPeXwnYkpeKCL5KX5Ri0wgN9EfuSSkVWK3utzlUT6Fxv4rYX8x3yXnD7wusYk04JPRGrxqQr0CGS7Dg78fYk~CKR-ZTBEdl1malEKEQFeB95F4jtkF7treylArDfc20V8F4Drtjz6LOC-z4xahmMViLqfafGAdWTezR0JlqbFGezgVy9Xk2rXCukr2c84Wn-ePmpXH4MQOxTrK8AKpjDWFb8bCr7hNQeFeefMNNCUb8ed7AwDdlT2qd4xKJlRwWfAaTUM0SKtKdYZTyOw39zPRjXBDENRTEou4QZyw15AkoLG4nkwg__&Key-Pair-Id=APKAIGAY3EJC77HVGRFQ"
 		Start-Process $Global:CSConsole -ArgumentList ('Downloading ' + $whrunsetup ) -NoNewWindow -Wait
-		$destination = ($NcfPath + "\ncftp.exe")
 		Invoke-WebRequest $source -OutFile $filespec 
-		if (! $?) { Start-Process $Global:CSConsole -ArgumentList ('/Error "Download ncftp.exe; Exit code ' + $LastExitCode.ToString + '"')  -NoNewWindow -Wait }
+		if (! $?) { Start-Process $Global:CSConsole -ArgumentList ('/Error "Download Exit code ' + $LastExitCode.ToString + '"')  -NoNewWindow -Wait }
+	}
+	else if ($Global:ZMGlobalContext -eq 'DEMOS') {
+		# The Setup binary 
+		$source = "https://edeliver.href.com/WebHub/WebHub_Central/WebHub_X_Runtime_System_v3.259_Setup.exe?Policy=eyJTdGF0ZW1lbnQiOlt7IlJlc291cmNlIjoiaHR0cHM6Ly9lZGVsaXZlci5ocmVmLmNvbS9XZWJIdWIvV2ViSHViX0NlbnRyYWwvV2ViSHViX1hfUnVudGltZV9TeXN0ZW1fdjMuMjU5X1NldHVwLmV4ZSIsIkNvbmRpdGlvbiI6eyJJcEFkZHJlc3MiOnsiQVdTOlNvdXJjZUlwIjoiNTQuMTk3LjI1LjgzLzMyIn0sIkRhdGVHcmVhdGVyVGhhbiI6eyJBV1M6RXBvY2hUaW1lIjoxNDY3OTQyMzIwfSwiRGF0ZUxlc3NUaGFuIjp7IkFXUzpFcG9jaFRpbWUiOjE0OTk0Nzg2MjB9fX1dfQ__&Signature=aeZuME~C8tHNC-~k3T-hKEastf7V~XDdkVl~3WI4SGeE8KTMh9JET8-TaoWa5tcklgCM1jJERzrv-icJVfIVTG9g9fcxkGRho6LzzHg-ONAD-sCFwk9FtpHr8LEqZ626gwMaQYLQ3YGOOb6kYb377fOKjXNNKMzJL0XoM-aEJcd0ao5ix6tTX6cjbRTUOTfKGsVrlFARMFB5BccwBfYFI~n6XDT1Z-RUyaEM9Kt9Z9EPb2mwx24XnfSuowGcd-WS2U-cWosdM6uSNSM0Yz5aNJy6R6E1L6kNnblHWA1dYqnBYgvvwQFSUVGbFcgXuEuQuoWLX6E9bpaKokb55MBRWQ__&Key-Pair-Id=APKAIGAY3EJC77HVGRFQ"
+		Start-Process $Global:CSConsole -ArgumentList ('Downloading ' + $whrunsetup ) -NoNewWindow -Wait
+		Invoke-WebRequest $source -OutFile $filespec 
+		if (! $?) { Start-Process $Global:CSConsole -ArgumentList ('/Error "Download Exit code ' + $LastExitCode.ToString + '"')  -NoNewWindow -Wait }
 	}
 	else {
 
