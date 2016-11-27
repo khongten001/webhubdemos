25eee7daaad70d4fa7e9c299de4d9350788f1c67
hreftoolscorp <hreftoolscorp@88ca0d87-9d1d-4ddd-a6d8-5c97b49fb611>
2016-11-27 19:03:10 +0000

    testing CollabNet becauase svn.exe from TortoiseSVN requires a DLL and it does not load from a case-sensitive drive

--- webhubdemos/_ControlSVN/svn-update-webhubdemos.bat
+++ webhubdemos/_ControlSVN/svn-update-webhubdemos.bat
@@ -5,11 +5,23 @@
 @echo off
 setlocal
 
+:: set zmcontext variable
+call %ZaphodsMap%zmset.bat zmcontext AsDefaultContext
+
 set src=https://svn.code.sf.net/p/webhubdemos/code/trunk
 
+if "%zmcontext%"=="DORIS" goto Doris
+goto General
+
+:Doris
+set svndir=D:\Apps\Utilities\SVN\Collabnet\
+goto Continue01
+
+:General
 if exist D:\Apps\Utilities\SVN\Collabnet\svn.exe       set svndir=D:\Apps\Utilities\SVN\Collabnet\
 if exist D:\Apps\Utilities\SVN\TortoiseSVN\bin\svn.exe set svndir=D:\Apps\Utilities\SVN\TortoiseSVN\bin\
 
+:Continue01
 cd /d %~dp0
 cd ..
 
