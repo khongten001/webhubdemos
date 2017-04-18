1477266407d49e671f3940dd70da91089904538a
hreftoolscorp <hreftoolscorp@88ca0d87-9d1d-4ddd-a6d8-5c97b49fb611>
2017-04-18 00:17:59 +0000

    C:\Library64

--- webhubdemos/Live/_Control/LiteMore-receive-new.bat
+++ webhubdemos/Live/_Control/LiteMore-receive-new.bat
@@ -1,7 +1,5 @@
 setlocal
 
-set bits=64
-
 d:\Apps\HREFTools\WebHub\bin\whadmin.exe app instances stop all
 start TaskMgr
 rem wait until all demos are out of memory
@@ -9,15 +7,15 @@ pause
 
 d:\Apps\HREFTools\WebHub\bin\whadmin.exe app cover --appid=demos --minutes=10 "--reason=upgrading web application"
 
-cd /d %~dp0
-cd ..\WebHub\Apps
+cd /d %~dp0..\WebHub\Apps
 
 d:\Apps\Utilities\7Zip\7z.exe x LiteMore-bin.7z -aoa
 if errorlevel 1 pause
 
-if "%bits%"=="32" cd ..\..\Library
-if "%bits%"=="64" cd ..\..\Library64
-d:\Apps\Utilities\7Zip\7z.exe x LiteMore-Library%bits%-bin.7z -aoa
+cd /D C:\Library64
+if errorlevel 1 pause
+
+d:\Apps\Utilities\7Zip\7z.exe x %~dp0..\Library64\LiteMore-Library64-bin.7z -aoa
 if errorlevel 1 pause
 
 :: now call startup procedure
