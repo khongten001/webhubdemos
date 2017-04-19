3bf4afc8632b10b5e5f87b4d4ff8d59c49ba9383
hreftoolscorp <hreftoolscorp@88ca0d87-9d1d-4ddd-a6d8-5c97b49fb611>
2017-04-19 06:34:09 +0000

    single place to define current cuompiler code (D25) and the path to the Raize CodeSite units

---
+++ webhubdemos/Source/_Control/set-compilerdigits.bat
@@ -0,0 +1,5 @@
+:: for all WebHub Demos
+:: D25 = Delphi 10.2 Tokyo
+
+set compilerdigits=25
+
---
+++ webhubdemos/Source/_Control/set-raizepath.bat
@@ -0,0 +1,8 @@
+:: for all WebHub Demos
+
+if "%compilerdigits%"=="25" set raizepath=%cslibroot%\RX10.2\win%wbits%
+if "%compilerdigits%"=="24" set raizepath=%cslibroot%\RX10.1\win%wbits%
+if "%compilerdigits%"=="23" set raizepath=%cslibroot%\RX10\win%wbits%
+if "%compilerdigits%"=="22" set raizepath=%cslibroot%\RS-XE8\win%wbits%
+
+
