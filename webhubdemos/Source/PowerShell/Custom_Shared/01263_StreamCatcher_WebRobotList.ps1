32e9608411565703e45d15cb656dc08fc38a1af5
hreftoolscorp <hreftoolscorp@88ca0d87-9d1d-4ddd-a6d8-5c97b49fb611>
2017-05-13 21:56:53 +0000

    powershell to download latest copy of WebRobotList

---
+++ webhubdemos/Source/PowerShell/Custom_Shared/01263_StreamCatcher_WebRobotList.ps1
@@ -0,0 +1,44 @@
+# StreamCatcher WebRobotList via PowerShell
+# Copyright 2014-2017 HREF Tools Corp. 
+# Creative Commons license - keep credits intact.
+
+# New StreamCatcher users should install by running SCSetup.exe
+# This PowerShell script is intended only for users who already have configuration files.
+
+# Optional: run this as a daily scheduled task to obtain the latest WebRobotList regularly
+
+function DownloadHTTP($source, $destination) {
+	Start-Process $Global:CSConsole -ArgumentList ('Downloading "' + $source + '"') -NoNewWindow -Wait
+	Start-Process $Global:CSConsole -ArgumentList ('Destination "' + $destination + '"') -NoNewWindow -Wait
+	Invoke-WebRequest $source -OutFile $destination
+	if (! (Test-Path $destination)) {
+		Start-Process $Global:CSConsole -ArgumentList ('/error "Not downloaded: "' + $destination + '"') -NoNewWindow -Wait
+	}
+}
+
+# call a separate script to init global variables.
+Invoke-Expression ($PSScriptRoot + '\..\WebHub_Appliance_PS\Initialize.ps1')  # loops back and uses custom values
+
+New-Variable -Name TempFilespec -Value ($Global:FolderInstallers + 'HREFTools\webrobotlist.txt') -Option private
+
+DownloadHTTP ("http://www.streamcatcher.com/webrobotlist.txt") $TempFilespec
+
+if (Test-Path D:\AppsData\StreamCatcher\Administrator\config) {
+	Copy $TempFilespec D:\AppsData\StreamCatcher\Administrator\config\webrobotlist.txt
+}
+
+if (Test-Path D:\whAppliance\streamcatcher\Administrator\Config) {
+	Copy $TempFilespec D:\whAppliance\streamcatcher\Administrator\Config\webrobotlist.txt
+}
+
+if (Test-Path D:\whAppliance\scConfig) {
+	Copy $TempFilespec D:\whAppliance\scConfig\webrobotlist.txt
+}
+
+
+Delete $TempFilespec
+
+Start-Process 'iisreset' -NoNewWindow -Wait
+
+
+Remove-Variable TempFilespec
