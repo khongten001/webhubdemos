7ad96fd4302ae93163fa42587d35d4c7e40ed9a2
hreftoolscorp <hreftoolscorp@88ca0d87-9d1d-4ddd-a6d8-5c97b49fb611>
2017-05-31 06:07:26 +0000

    use svn to get these svn files onto the target server

---
+++ webhubdemos/Source/PowerShell/Custom_DemosDB/00285_Subversion_Clipboard_DemosDB.ps1
@@ -0,0 +1,35 @@
+
+
+<# ************************** #>
+<# run these manually by copy+paste into the powershell console on the target machine over remote desktop #>
+$Trg = "C:\Temp\PowerShell\Custom"
+if (!(Test-Path "$Trg\.svn")) {
+	Set-Location 'C:\'  # parent folder
+	$svnurl = 'https://svn.code.sf.net/p/webhubdemos/code/trunk/webhubdemos/Source/PowerShell/Custom_DemosDB'
+	Start-Process "svn.exe" -ArgumentList ("checkout " + $svnurl + " " + $TrgDir + " --username hreftechsupport --force ") -NoNewWindow -Wait
+} else {
+	Set-Location $Trg
+	Start-Process "svn.exe" -ArgumentList "update"  -NoNewWindow -Wait
+}
+
+$Trg = "C:\Temp\PowerShell\Custom_Shared"
+if (!(Test-Path "$Trg\.svn")) {
+	Set-Location 'C:\'  # parent folder
+	$svnurl = 'https://svn.code.sf.net/p/webhubdemos/code/trunk/webhubdemos/Source/PowerShell/Custom_Shared'
+	Start-Process "svn.exe" -ArgumentList ("checkout " + $svnurl + " " + $TrgDir + " --username hreftechsupport --force ") -NoNewWindow -Wait
+} else {
+	Set-Location $Trg
+	Start-Process "svn.exe" -ArgumentList "update"  -NoNewWindow -Wait
+}
+
+$svnurl = 'https://svn.riouxsvn.com/psmiscwebhub/trunk/Custom_StreamCatcher'
+Start-Process "svn.exe" -ArgumentList ("export " + $svnurl + " " + "C:\Temp\PowerShell\Custom" + " --force ") -NoNewWindow -Wait
+
+$svnurl = 'https://svn.riouxsvn.com/psmiscwebhub/trunk/Custom_PurgeByDate'
+Start-Process "svn.exe" -ArgumentList ("export " + $svnurl + " " + "C:\Temp\PowerShell\Custom" + " --force ") -NoNewWindow -Wait
+
+Remove-Variable -Name Trg
+Remove-Variable -Name svnurl
+
+<# ************************** #>
+
