240a6d313b87852a8c00935385878967c9e145b9
hreftoolscorp <hreftoolscorp@88ca0d87-9d1d-4ddd-a6d8-5c97b49fb611>
2017-06-02 01:14:01 +0000

    typo TrgDir

--- trunk/webhubdemos/Source/PowerShell/Custom_DemosDB/00285_Subversion_Clipboard_DemosDB.ps1
+++ trunk/webhubdemos/Source/PowerShell/Custom_DemosDB/00285_Subversion_Clipboard_DemosDB.ps1
@@ -3,20 +3,22 @@
 <# ************************** #>
 <# run these manually by copy+paste into the powershell console on the target machine over remote desktop #>
 $Trg = "C:\Temp\PowerShell\Custom"
+if (!(Test-Path $Trg)) { MKDIR $Trg }
 if (!(Test-Path "$Trg\.svn")) {
 	Set-Location 'C:\'  # parent folder
 	$svnurl = 'https://svn.code.sf.net/p/webhubdemos/code/trunk/webhubdemos/Source/PowerShell/Custom_DemosDB'
-	Start-Process "svn.exe" -ArgumentList ("checkout " + $svnurl + " " + $TrgDir + " --username hreftechsupport --force ") -NoNewWindow -Wait
+	Start-Process "svn.exe" -ArgumentList ("checkout " + $svnurl + " " + $Trg + " --username hreftechsupport --force ") -NoNewWindow -Wait
 } else {
 	Set-Location $Trg
 	Start-Process "svn.exe" -ArgumentList "update"  -NoNewWindow -Wait
 }
 
 $Trg = "C:\Temp\PowerShell\Custom_Shared"
+if (!(Test-Path $Trg)) { MKDIR $Trg }
 if (!(Test-Path "$Trg\.svn")) {
 	Set-Location 'C:\'  # parent folder
 	$svnurl = 'https://svn.code.sf.net/p/webhubdemos/code/trunk/webhubdemos/Source/PowerShell/Custom_Shared'
-	Start-Process "svn.exe" -ArgumentList ("checkout " + $svnurl + " " + $TrgDir + " --username hreftechsupport --force ") -NoNewWindow -Wait
+	Start-Process "svn.exe" -ArgumentList ("checkout " + $svnurl + " " + $Trg + " --force ") -NoNewWindow -Wait
 } else {
 	Set-Location $Trg
 	Start-Process "svn.exe" -ArgumentList "update"  -NoNewWindow -Wait
