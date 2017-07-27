3a2bf47cf3204c171d147785bb56474148f8244f
hreftoolscorp <hreftoolscorp@88ca0d87-9d1d-4ddd-a6d8-5c97b49fb611>
2017-07-27 03:04:59 +0000

    FireMonkey GUI that calls the Delphi function to set ACL public for selected files in an AWS S3 bucket

---
+++ trunk/webhubdemos/Source/StandaloneDemos/AmazonS3ACLPublic/S3ACLPublic_Gui_fmMain.pas
@@ -0,0 +1,109 @@
+unit S3ACLPublic_Gui_fmMain;
+
+interface
+
+(*
+Permission is hereby granted, on 26-Jul-2017, free of charge, to any person
+obtaining a copy of this file (the "Software"), to deal in the Software
+without restriction, including without limitation the rights to use, copy,
+modify, merge, publish, distribute, sublicense, and/or sell copies of the
+Software, and to permit persons to whom the Software is furnished to do so,
+subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+THE SOFTWARE.
+
+Author: Ann Lynnworth at HREF Tools Corp.
+*)
+
+uses
+  System.SysUtils, System.Types, System.UITypes, System.Classes, System.Variants,
+  FMX.Types, FMX.Controls, FMX.Forms, FMX.Graphics, FMX.Dialogs, FMX.Edit,
+  FMX.Controls.Presentation, FMX.StdCtrls, FMX.Layouts;
+
+type
+  TForm3 = class(TForm)
+    GridPanelLayout1: TGridPanelLayout;
+    Label1: TLabel;
+    EditBucketName: TEdit;
+    Label2: TLabel;
+    EditRegion: TEdit;
+    Label3: TLabel;
+    EditLeadingPath: TEdit;
+    Label4: TLabel;
+    EditMatchThis: TEdit;
+    Label5: TLabel;
+    EditAWSAccessKey: TEdit;
+    Label6: TLabel;
+    EditAWSSecretKey: TEdit;
+    Label7: TLabel;
+    cbJustRootFolder: TCheckBox;
+    Label8: TLabel;
+    EditMaxFilesToTouch: TEdit;
+    LabelScheme: TLabel;
+    GroupBox1: TGroupBox;
+    cbSchemeHttp: TRadioButton;
+    cbSchemeHttps: TRadioButton;
+    Label9: TLabel;
+    cbActionIt: TCheckBox;
+    Button1: TButton;
+    ButtonGo: TButton;
+    procedure Button1Click(Sender: TObject);
+    procedure ButtonGoClick(Sender: TObject);
+  private
+    { Private declarations }
+  public
+    { Public declarations }
+  end;
+
+var
+  Form3: TForm3;
+
+implementation
+
+{$R *.fmx}
+
+uses
+  S3ACLPublic_uMarkPublic;
+
+procedure TForm3.Button1Click(Sender: TObject);
+begin
+  Self.Close;  // close the form
+end;
+
+procedure TForm3.ButtonGoClick(Sender: TObject);
+var
+  bActionIt: Boolean;
+  bJustRootFolder: Boolean;
+  Scheme: string;
+begin
+  bActionIt := cbActionIt.IsChecked;
+  bJustRootFolder := cbJustRootFolder.IsChecked;
+
+  if cbSchemeHttp.IsChecked then
+  begin
+    if cbSchemeHttps.IsChecked then
+    begin
+      ShowMessage('select either http or https!');
+      Exit;
+      scheme := 'http';
+    end
+    else
+      scheme := 'https';
+  end;
+
+  TagPublic(bActionit, scheme, EditBucketName.Text, EditLeadingPath.Text,
+    EditMatchThis.Text, EditAWSAccessKey.Text, EditAWSSecretKey.Text,
+    bJustRootFolder, StrToIntDef(EditMaxFilesToTouch.Text, 1), EditRegion.Text);
+
+end;
+
+end.
